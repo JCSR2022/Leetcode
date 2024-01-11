879552781eff964007b89475cd028a0b034ce23c
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-01-11 12:36:22 -0600

    Time: 37 ms (63.32%), Space: 17.3 MB (32.12%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,13 +1,32 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-      #  cont = 0
-      #  i = 0
-      #  while  -1*(i-1)<=len(s) and s[i-1] == ' ':
-      #      i -= 1
-      #       
-      #  while -1*(i-1)<=len(s) and  s[i-1] != ' ':
-      #      cont +=1
-      #      i -=1
-      #  return cont
-        return len(s.split()[-1])
+        
+# opcion 1, lenta
+#         cont = 0
+#         i = 0
+#         while  -1*(i-1)<=len(s) and s[i-1] == ' ':
+#             i -= 1
+             
+#         while -1*(i-1)<=len(s) and  s[i-1] != ' ':
+#             cont +=1
+#             i -=1
+#         return cont
+
+
+# opcion 2, mas lenta pero elegante
+#         return len(s.split()[-1])
+        
+        empezado_conteo = False
+        conteo = 0 
+        for letter in s[::-1]:
+            if not empezado_conteo:
+                if letter != ' ':
+                    conteo = 1
+                    empezado_conteo = True
+            else:
+                if letter != ' ':
+                    conteo += 1
+                else:
+                    break
+        return conteo
         

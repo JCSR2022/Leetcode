c1a1559db0410f089003102eb3cc6253d7c08a4a
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-01-13 16:40:23 -0600

    Time: 41 ms (58.35%), Space: 17.5 MB (15.93%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -10,6 +10,7 @@ class Solution:
                     break
             return comun
         
+        #opcion 1
         #long_common_prefix = strs[0]
         #    
         #for string_i in strs:
@@ -18,6 +19,48 @@ class Solution:
         #        break
         #return long_common_prefix
             
-        strs.sort()
+        # opcion 2
+        #strs.sort()
+        #return common_prefix(strs[0],strs[-1])
         
-        return common_prefix(strs[0],strs[-1])
+        
+        
+        # Opcion 3 . divide (binary search)
+        if len(strs) == 0:
+            return ""
+        if len(strs) == 1:
+            return strs[0]
+        
+        
+        def common_prefix_ver2(strs, size):
+            str1 = strs[0][0:size]
+            i = 1
+            while (i < len(strs)):
+                if (not strs[i].startswith(str1)):
+                    return False
+                i +=1
+            return True
+        
+        
+        # 0 <= strs[i].length <= 200
+        min_len = 201
+        
+        
+        for string_i in strs:
+            min_len = min(min_len,len(string_i))
+        low = 1
+        high = min_len
+        while (low <= high):
+            mid = (low+high)//2
+            if common_prefix_ver2(strs, mid):
+                low = mid + 1
+            else:
+                high = mid - 1
+                
+        return strs[0][0:(low+high)//2]
+        
+            
+            
+            
+        
+        

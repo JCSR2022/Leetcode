985b2a3c856f7442e1aad5b0224feda6f927c0b1
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-01-14 06:26:53 -0600

    Time: 131 ms (71.65%), Space: 18.6 MB (14.44%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -37,13 +37,11 @@ class Solution:
         #condicion 1
         letras_word1 = set(dicc_letras_word1.keys())
         letras_word2 = set(dicc_letras_word2.keys())
-        if set(letras_word1) != set(letras_word2):
+        if letras_word1 != letras_word2 :
             return False
         
         #condicion 2
-        frecuencias_word1 = Counter(list(dicc_letras_word1.values()))
-        frecuencias_word2 = Counter(list(dicc_letras_word2.values()))
-        if frecuencias_word1 != frecuencias_word2:
+        if Counter(dicc_letras_word1.values()) != Counter(dicc_letras_word2.values()):
             return False   
             
         return True      

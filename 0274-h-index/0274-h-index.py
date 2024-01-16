4e255187ec881f0373516aa13a5460a294cd4f14
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-01-16 13:32:42 -0600

    Time: 34 ms (95.79%), Space: 17.7 MB (25.45%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -5,11 +5,8 @@ class Solution:
         
         citations.sort(reverse=True)
         for resarch_i,citation_resarch_i in enumerate(citations):
-            print(resarch_i,citation_resarch_i)
+           # print(resarch_i,citation_resarch_i)
             if citation_resarch_i <= resarch_i:
                 return resarch_i
         return len(citations)
     
-#[9, 7, 6, 2,1]
-#[3,1,1]
-#[0,0,0]        

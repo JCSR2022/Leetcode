32d82aec3734cde9c0f9d980cf53c853f87d412b
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-01-17 10:58:48 -0600

    Time: 164 ms (87.74%), Space: 24.9 MB (30.68%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -0,0 +1,28 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        
+        
+        # La forma mas facil seria multiplicar todo y despues 
+        # dividir por nums[i] pero no es valida
+        
+        # Esto no es O(n)
+        # def mult(lista):
+        #     return reduce(lambda x, y: x * y, lista, 1)
+        # return [mult(nums[:i] + nums[i+1:]) for i in range(len(nums))]
+        
+        size = len(nums)
+        mult_1 = [nums[0]]
+        for i in range(1,size-1):
+            mult_1.append(mult_1[i-1]*nums[i])
+        mult_1 = [1]+ mult_1
+        
+        mult_2 = [0]*(size-1)
+        mult_2[-1] = nums[-1]
+        for i in range(-2,-size,-1): 
+            mult_2[i] = mult_2[i+1]*nums[i]
+        mult_2.append(1)
+        
+        return [mult_1[i]*mult_2[i] for i in range(size)]
+        
+        

a0d601127824c996e611bd0b6b06c7abb2e946f9
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-01-17 20:38:16 -0600

    Time: 50 ms (36.39%), Space: 17.5 MB (48.52%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,14 +1,37 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         
-        map_chars = {}
-        for i, char_s in enumerate(s):
-            if char_s not in map_chars:
-                if t[i] not in map_chars.values():
-                    map_chars[char_s] = t[i]
-                else:
+       # map_chars = {}
+       # for i, char_s in enumerate(s):
+       #     if char_s not in map_chars:
+       #         if t[i] not in map_chars.values():
+       #             map_chars[char_s] = t[i]
+       #         else:
+       #             return False
+       #     else:
+       #         if map_chars[char_s] != t[i]:
+       #             return False
+       # return True
+    
+    
+        mapping_s_t = {}
+        mapping_t_s = {}
+
+        if len(s) != len(t):
+            return False
+
+        for char_s, char_t in zip(s, t):
+            if char_s in mapping_s_t:
+                if mapping_s_t[char_s] != char_t:
                     return False
             else:
-                if map_chars[char_s] != t[i]:
+                mapping_s_t[char_s] = char_t
+
+            if char_t in mapping_t_s:
+                if mapping_t_s[char_t] != char_s:
                     return False
+            else:
+                mapping_t_s[char_t] = char_s
+
         return True
+    

ee6ca2508464b2f56c556a435d75551bc034f6aa
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-01-19 12:52:40 -0600

    Time: 961 ms (75.19%), Space: 22.1 MB (92.67%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,5 +1,6 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        
         if sum(gas) < sum(cost):
             return -1
         
@@ -9,12 +10,15 @@ class Solution:
         
         for i in range(size):
             suma += gas[i] - cost[i]
+            #  print(i,gas[i],cost[i], suma,suma < 0,posicion)
             if suma < 0:
                 suma = 0
                 posicion = i+1
         
         return posicion
-                
-                
+
+# gas = [5,1,2,4,3,4,1,5,2]
+# cost = [1,3,4,2,5,1,5,2,4]
+# diff = [ gas_i-cost_i for gas_i,cost_i in zip(gas,cost) ]
         
         

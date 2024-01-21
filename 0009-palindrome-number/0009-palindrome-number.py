bddf178be5a9d5727701eeaa192ac0f768c201a7
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-01-21 16:37:04 -0600

    Time: 43 ms (96.21%), Space: 16.6 MB (55.23%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,11 +4,14 @@ class Solution:
             return False
             
         str_x = str(x)
-        size = len(str_x)
-        mitad = size//2
-        odd =  size%2
-        if odd:
-            return  str_x[:mitad] == str_x[-1:mitad:-1]
-        else:
-            return  str_x[:mitad] == str_x[-1:mitad-1:-1]
+        
+        return str_x[::-1] == str_x
+        
+        #size = len(str_x)
+        #mitad = size//2
+        #odd =  size%2
+        #if odd:
+        #    return  str_x[:mitad] == str_x[-1:mitad:-1]
+        #else:
+        #    return  str_x[:mitad] == str_x[-1:mitad-1:-1]
 

0588f07e1892749443247902964f65980daca3b8
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-01-21 20:00:48 -0600

    Time: 173 ms (50.26%), Space: 18.2 MB (71.38%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,17 +1,28 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        #for i,num in enumerate(nums):
-        #    if num != i+1:
-        #        return [num,i+1]
-        numeros = [n for n in range(1,len(nums)+1)]
-        faltante = 0
-        repetido = 0
-        for num in numeros:
-            if num not in nums:
-                faltante = num
-            else:
-                nums.pop(nums.index(num))
-        repetido = nums[0]
+
+       # numeros = [n for n in range(1,len(nums)+1)]
+       # faltante = 0
+       # repetido = 0
+       # for num in numeros:
+       #     if num not in nums:
+       #         faltante = num
+       #     else:
+       #         nums.pop(nums.index(num))
+       # repetido = nums[0]
+       # return [repetido,faltante]
+        
+        numeros = { n: 0 for n in range(1,len(nums)+1)} 
+
+        for num in nums:
+            numeros[num] +=1
+            if numeros[num] == 2:
+                repetido = num
+
+        for key, value in numeros.items():
+            if value == 0:
+                faltante = key
+                break
         return [repetido,faltante]
                 
         

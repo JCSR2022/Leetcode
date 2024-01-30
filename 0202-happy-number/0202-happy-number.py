d58f7b4fa7600f391c578471ca1d31c3944c2e6f
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-01-30 13:06:31 -0600

    Time: 40 ms (55.77%), Space: 16.5 MB (67.30%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,20 +1,16 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        
-        def calc_happy(n):
-            return  sum([ int(i)**2 for i in str(n)])
-        
-        cont = 0
+
         result = [n]
-        while cont < 1000000:
-            n = calc_happy(n)
+        while True:
+            n = sum([ int(i)**2 for i in str(n)])
             if n == 1:
                 return True
             if n in result:
                 return False
             else:
                 result.append(n)
-            cont +=1
+   
                 
             
   

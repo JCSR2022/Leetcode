6cdeb60b9b962a629bbbbd269f400f5f9e43c6c1
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-02-01 12:04:01 -0600

    Time: 62 ms (17.64%), Space: 16.5 MB (64.69%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,21 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        unidades = ['I', 'II', 'III', 'IV','V','VI','VII','VIII','IX']
+        decenas = ['X','XX','XXX','XL','L','LX','LXX','LXXX','XC']
+        centenas = ['C','CC','CCC','CD','D','DC','DCC','DCCC','CM']
+        mil = ['M','MM','MMM', 'I\u0056\u0305','\u0056\u0305','\u0056\u0305M',
+               '\u0056\u0305MM','\u0056\u0305MMM', 'I\u0058\u0305']
+        conv_4dig = [mil ,centenas,decenas,unidades]
+
+        str_num = str(num)
+        conv_dig = conv_4dig[-len(str_num):]
+
+        ans = ""
+        for i,digito in enumerate(str(num)):
+            #print(i,digito, conv_dig[i][int(digito)-1])
+            if int(digito) != 0:
+                ans = ans + conv_dig[i][int(digito)-1]
+
+        return ans
+
+        

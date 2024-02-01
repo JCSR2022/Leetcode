07377972aa95b6c4060d4775380050a08461c98d
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-02-01 12:06:14 -0600

    Time: 40 ms (91.49%), Space: 16.5 MB (64.69%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,9 +1,9 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        unidades = ['I', 'II', 'III', 'IV','V','VI','VII','VIII','IX']
-        decenas = ['X','XX','XXX','XL','L','LX','LXX','LXXX','XC']
-        centenas = ['C','CC','CCC','CD','D','DC','DCC','DCCC','CM']
-        mil = ['M','MM','MMM', 'I\u0056\u0305','\u0056\u0305','\u0056\u0305M',
+        unidades = ['','I', 'II', 'III', 'IV','V','VI','VII','VIII','IX']
+        decenas = ['','X','XX','XXX','XL','L','LX','LXX','LXXX','XC']
+        centenas = ['','C','CC','CCC','CD','D','DC','DCC','DCCC','CM']
+        mil = ['','M','MM','MMM', 'I\u0056\u0305','\u0056\u0305','\u0056\u0305M',
                '\u0056\u0305MM','\u0056\u0305MMM', 'I\u0058\u0305']
         conv_4dig = [mil ,centenas,decenas,unidades]
 
@@ -13,8 +13,8 @@ class Solution:
         ans = ""
         for i,digito in enumerate(str(num)):
             #print(i,digito, conv_dig[i][int(digito)-1])
-            if int(digito) != 0:
-                ans = ans + conv_dig[i][int(digito)-1]
+            #if int(digito) != 0:
+            ans = ans + conv_dig[i][int(digito)]
 
         return ans
 

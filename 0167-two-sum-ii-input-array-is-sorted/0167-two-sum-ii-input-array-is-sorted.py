bf17c786d569becd4beef359489b7e3b05b53d62
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-02-02 15:36:35 -0600

    Time: 105 ms (75.61%), Space: 17.3 MB (90.68%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -8,15 +8,16 @@ class Solution:
         #         elif num+numbers[j] == target:
         #             return [i+1,j+1]
         
-        i = 0
-        j = len(numbers)-1
-        while True:
-            if numbers[i] + numbers[j] > target:
-                j -= 1
-            elif numbers[i] + numbers[j] < target:
-                i += 1 
-            elif numbers[i] + numbers[j] == target:
-                return [i+1,j+1]
+        left = 0
+        right = len(numbers)-1 
+        
+        while left < right:
+            if numbers[left] + numbers[right] > target:
+                right -= 1
+            elif numbers[left] + numbers[right] < target:
+                left += 1 
+            elif numbers[left] + numbers[right] == target:
+                return [left+1,right+1]
             
         
         

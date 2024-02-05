f303937826452790a3b70965134a686d93d5d03f
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-02-05 09:17:28 -0600

    Time: 40 ms (99.91%), Space: 16.7 MB (66.48%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -1,13 +1,13 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        map_letter = {}
-        for letter in s:
-            map_letter[letter] = 1 + map_letter.get(letter,0)
+#         map_letter = {}
+#         for letter in s:
+#             map_letter[letter] = 1 + map_letter.get(letter,0)
         
-        for i,letter in enumerate(s):
-            if map_letter[letter] == 1:
-                return i
-        return -1
+#         for i,letter in enumerate(s):
+#             if map_letter[letter] == 1:
+#                 return i
+#         return -1
         
 #         map_letter = {}
 #         for i,letter in enumerate(s):
@@ -20,5 +20,16 @@ class Solution:
 #         return min_index if min_index != float("inf") else -1
         
         
-        
+        key = 'abcdefghijklmnopqrstuvwxyz'  # Pre-defined order of characters
+        idx = 10**5  # Set an initial large index
+
+        # Iterate through each character in the pre-defined order
+        for i in key:
+            x = s.find(i)  # Find the first occurrence of the character
+
+            if x != -1 and x == s.rfind(i):
+                idx = min(idx, x)  # Update the minimum index
+
+        # Return the minimum index if found, otherwise return -1
+        return idx if idx != 10**5 else -1        
         

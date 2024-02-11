5e9ea01937622e78b743c75da6c1f7cc287dc396
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-02-11 09:46:12 -0600

    Time: 83 ms (62.33%), Space: 16.5 MB (87.31%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -9,11 +9,40 @@ class Solution:
         
 #         return L_ans
 
-        ans = 0
+
+#   O(n**3) sol
+#         ans = 0
+#         for i in range(len(s)):
+#             for j in range(i):
+#                 if s[j:i+1] == s[j:i+1][::-1]:
+#                     ans += 1
+#         L_ans = len(list(s)) + ans
+#         return L_ans     
+    
+    
+#    O(n**2) sol:
+
+        res = 0
+    
+        def check_palindrom(l,r,res):
+            while l >=0 and r < len(s) and s[l] == s[r]:
+                res +=1
+                l -=1
+                r +=1
+            return res
+    
         for i in range(len(s)):
-            for j in range(i):
-                if s[j:i+1] == s[j:i+1][::-1]:
-                    ans += 1
-        L_ans = len(list(s)) + ans
+            #odd substrings
+            l = r = i
+            res = check_palindrom(l,r,res)
+            
+            #even substrings
+            l = i
+            r = i+1
+            res = check_palindrom(l,r,res)
         
-        return L_ans
+        return res
+            
+                
+    
+

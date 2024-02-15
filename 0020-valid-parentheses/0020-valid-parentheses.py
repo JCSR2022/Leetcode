4040704697bc94c71d774db84d577cfe07fed32b
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-02-15 12:26:45 -0600

    Time: 35 ms (64.91%), Space: 16.5 MB (83.62%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,27 +1,7 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-#         brackets = ['(' , ')' , '{' , '}', '[' , ']']
-#         brackets_close = { ')': '(' , '}': '{',  ']':'[' }
-        
-#         if len(s) < 2:
-#             return False
-        
-#         sequence = []
-        
-#         for elem in s:
-#             if elem in brackets:
-#                 if elem in brackets_close:
-#                     if brackets_close[elem] in sequence and  brackets_close[elem] == sequence[-1]:
-#                         sequence.pop()
-#                     else:
-#                         return False
-#                 else:
-#                     sequence.append(elem)
-     
-#         return len(sequence) == 0
-          
-        brackets_open =  ['(' , '{' , '[' ]
-        brackets_close = [')' , '}' , ']' ]
+        brackets = ['(' , ')' , '{' , '}', '[' , ']']
+        brackets_close = { ')': '(' , '}': '{',  ']':'[' }
         
         if len(s) < 2:
             return False
@@ -29,16 +9,36 @@ class Solution:
         sequence = []
         
         for elem in s:
-            if elem in brackets_open:
-                sequence.append(elem)
-                
-            if elem in brackets_close:
-                if  ( (len(sequence) > 0)  and
-                    (brackets_open[brackets_close.index(elem)] == sequence[-1])):
-                    sequence.pop()
+            if elem in brackets:
+                if elem in brackets_close:
+                    if brackets_close[elem] in sequence and  brackets_close[elem] == sequence[-1]:
+                        sequence.pop()
+                    else:
+                        return False
                 else:
-                    return False
+                    sequence.append(elem)
      
         return len(sequence) == 0
+          
+#         brackets_open =  ['(' , '{' , '[' ]
+#         brackets_close = [')' , '}' , ']' ]
+        
+#         if len(s) < 2:
+#             return False
+        
+#         sequence = []
+        
+#         for elem in s:
+#             if elem in brackets_open:
+#                 sequence.append(elem)
+                
+#             if elem in brackets_close:
+#                 if  ( (len(sequence) > 0)  and
+#                     (brackets_open[brackets_close.index(elem)] == sequence[-1])):
+#                     sequence.pop()
+#                 else:
+#                     return False
+     
+#         return len(sequence) == 0
                         
         

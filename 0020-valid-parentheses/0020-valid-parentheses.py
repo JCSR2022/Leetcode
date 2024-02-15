e43d861c58be9ce3a9ee17d9c7b65f7908f134bd
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-02-15 12:24:38 -0600

    Time: 34 ms (73.43%), Space: 16.5 MB (93.90%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,7 +1,27 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        brackets = ['(' , ')' , '{' , '}', '[' , ']']
-        brackets_close = { ')': '(' , '}': '{',  ']':'[' }
+#         brackets = ['(' , ')' , '{' , '}', '[' , ']']
+#         brackets_close = { ')': '(' , '}': '{',  ']':'[' }
+        
+#         if len(s) < 2:
+#             return False
+        
+#         sequence = []
+        
+#         for elem in s:
+#             if elem in brackets:
+#                 if elem in brackets_close:
+#                     if brackets_close[elem] in sequence and  brackets_close[elem] == sequence[-1]:
+#                         sequence.pop()
+#                     else:
+#                         return False
+#                 else:
+#                     sequence.append(elem)
+     
+#         return len(sequence) == 0
+          
+        brackets_open =  ['(' , '{' , '[' ]
+        brackets_close = [')' , '}' , ']' ]
         
         if len(s) < 2:
             return False
@@ -9,16 +29,16 @@ class Solution:
         sequence = []
         
         for elem in s:
-            if elem in brackets:
-                if elem in brackets_close:
-                    if brackets_close[elem] in sequence and  brackets_close[elem] == sequence[-1]:
-                        sequence.pop()
-                    else:
-                        return False
+            if elem in brackets_open:
+                sequence.append(elem)
+                
+            if (elem in brackets_close):
+                if  ( (len(sequence) > 0)  and
+                    (brackets_open[brackets_close.index(elem)] == sequence[-1])):
+                    sequence.pop()
                 else:
-                    sequence.append(elem)
-        ans = len(sequence) == 0
-        return ans
-                        
+                    return False
+     
+        return len(sequence) == 0
                         
         

2e743d7ac05bcf77d2a1f0a9fee56e0a94a43b83
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-02-19 21:12:02 -0600

    Time: 110 ms (72.62%), Space: 17.7 MB (88.15%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,7 +1,9 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        # brute force
-        lista = [ _ for _ in range(len(nums)+1)][::-1]
-        for num in nums:
-            lista.remove(num)
-        return lista[0]
+        # # brute brute, brute force
+        # lista = [ _ for _ in range(len(nums)+1)][::-1]
+        # for num in nums:
+        #     lista.remove(num)
+        # return lista[0]
+        n = len(nums)
+        return n*(n+1)//2 - sum(nums)

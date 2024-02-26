199d3f7bb968ab43b8119b580bff0a95c8c48db1
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-02-26 12:18:34 -0600

    Time: 39 ms (34.77%), Space: 16.5 MB (98.07%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,17 +6,28 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        # DFS
-        def compareTree(node_p, node_q):
-            if not node_p and not node_q:
-                return True
-            elif not node_p or not node_q:
+        
+        if not p and not q:
+            return True
+        elif not p or not q:
+            return False
+        else:
+            if p.val != q.val:
                 return False
-            else:
-                if node_p.val != node_q.val:
-                    return False
+            return self.isSameTree(p.left, q.left) and  self.isSameTree(p.right, q.right)
+        
+        
+#         # DFS
+#         def compareTree(node_p, node_q):
+#             if not node_p and not node_q:
+#                 return True
+#             elif not node_p or not node_q:
+#                 return False
+#             else:
+#                 if node_p.val != node_q.val:
+#                     return False
 
-                return compareTree(node_p.left, node_q.left) and compareTree(node_p.right, node_q.right)
+#                 return compareTree(node_p.left, node_q.left) and compareTree(node_p.right, node_q.right)
 
-        return compareTree(p, q)
+#         return compareTree(p, q)
             

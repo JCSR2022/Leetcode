4560e2aa211215d1873928e60c37043201695a49
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-03-03 09:35:23 -0600

    Time: 34 ms (76.64%), Space: 16.6 MB (61.49%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,25 +5,48 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+
+        #dfs? hashtable?  time : O(n), space: O(n) 
         
-        next_vector = [head]
+#         next_vector = [head]
         
-        def create_node_next_list(node):
-            while node.next:
-                next_vector.append(node.next)
-                node = node.next
+#         def create_node_next_list(node):
+#             while node.next:
+#                 next_vector.append(node.next)
+#                 node = node.next
 
             
-        create_node_next_list(head)
+#         create_node_next_list(head)
         
-        if len(next_vector) == 1:
-            head.val = ""
+#         if len(next_vector) == 1:
+#             head.val = ""
             
-        elif len(next_vector) == n:
-            head = head.next
-       
-        else:
-            next_vector[-n-1].next = next_vector[-n].next
+#         elif len(next_vector) == n:
+#             head = head.next
+            
+#         else:
+#             next_vector[-n-1].next = next_vector[-n].next
+        
+#         return head
+
+
+        # Two Pointer time O(n) space O(1)
+
+        dummy = ListNode(None,head)
+        left = dummy
+        right = head
         
         
-        return head
+        while n > 0 and right:
+            right = right.next
+            n -= 1
+            
+        while right:
+            left = left.next
+            right = right.next
+            
+        left.next = left.next.next
+        
+        return dummy.next
+    
+    

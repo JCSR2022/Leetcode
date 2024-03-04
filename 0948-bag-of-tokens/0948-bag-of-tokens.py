51b556c7d3ec465ae3624db8809618c7538b8456
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-03-04 16:58:30 -0600

    Time: 59 ms (20.92%), Space: 16.7 MB (94.14%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.py
+++ 0948-bag-of-tokens/0948-bag-of-tokens.py
@@ -24,25 +24,43 @@ class Solution:
         R = len(tokens)-1
         tokens.sort()
         
+#         while L<=R:
+#             print(L,R,tokens[L],tokens[R],power,score)
+#             if tokens[L] <= power:
+#                 #faceUp: 
+#                 score +=1
+#                 power -= tokens[L]
+#                 L +=1  
+#             else:
+#                 if score > 0 and L < R:
+#                     #faceDown:  
+#                     score -=1  
+#                     power += tokens[R] 
+#                     R -=1
+#                 else:
+#                     return score
+                
+#        return score
+         
+    
+        ans = 0
         while L<=R:
-            print(L,R,tokens[L],tokens[R],power,score)
+            #print(L,R,tokens[L],tokens[R],power,score)
             if tokens[L] <= power:
                 #faceUp: 
                 score +=1
                 power -= tokens[L]
                 L +=1  
-            else:
-                if score > 0 and L < R:
+                ans = max(ans,score)
+            elif score > 0:
                     #faceDown:  
                     score -=1  
                     power += tokens[R] 
                     R -=1
-                else:
-                    return score
-                
-        return score
-                
-        
+            else:
+                return ans
+
+        return ans 
                     
                 
                 

a80cc8eea0226e3084c1c2f5702ac4cc97d3f6a0
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-03-04 17:44:40 -0600

    Time: 52 ms (19.60%), Space: 19.2 MB (50.88%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -26,28 +26,46 @@ class Solution:
 
         # Two pointers :Floyd’s cycle finding or Hare-Tortoise algorithm
 
-        def FloydHareTortoise(nodeSlow, nodeFast):
-            if  (nodeSlow and 
-                nodeFast and 
-                nodeSlow.next and 
-                nodeFast.next and
-                nodeFast.next.next):
+#         def FloydHareTortoise(nodeSlow, nodeFast):
+#             if  (nodeSlow and 
+#                 nodeFast and 
+#                 nodeSlow.next and 
+#                 nodeFast.next and
+#                 nodeFast.next.next):
                     
-                if nodeSlow == nodeFast:
-                    return True
-                else:
-                    return FloydHareTortoise(nodeSlow.next, nodeFast.next.next)
-            else:
-                return False
+#                 if nodeSlow == nodeFast:
+#                     return True
+#                 else:
+#                     return FloydHareTortoise(nodeSlow.next, nodeFast.next.next)
+#             else:
+#                 return False
     
-        if head and head.next and head.next.next :
-            slow = head.next
-            fast = head.next.next
+#         if head and head.next and head.next.next :
+#             slow = head.next
+#             fast = head.next.next
 
-            return FloydHareTortoise(slow, fast)
+#             return FloydHareTortoise(slow, fast)
             
-        else:
-            return False      
+#         else:
+#             return False      
+
+
+
+        fast, slow = head, head
+    
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+            if slow == fast:
+                return True
+        
+        
+        return False
+            
+    
+    
+    
                 
                 
                 

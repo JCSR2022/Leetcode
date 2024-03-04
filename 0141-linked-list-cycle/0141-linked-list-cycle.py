037934d99c11d522c9b57b2019c332dbcf1d401d
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-03-04 17:40:34 -0600

    Time: 46 ms (53.99%), Space: 19.9 MB (20.00%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -7,22 +7,56 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         
-        # Brute force soluton
+#         # Brute force soluton Hash table
+#         list_nodes = []
+#         lisit_nodes_val =[]
         
-        list_nodes = []
-        lisit_nodes_val =[]
-        
-        def dfs(node):
-            if node:
-                if node in list_nodes:
+#         def dfs(node):
+#             if node:
+#                 if node in list_nodes:
+#                     return True
+#                 else:
+#                     lisit_nodes_val.append(node.val)
+#                     list_nodes.append(node)
+#                     return dfs(node.next)
+#             else:
+#                 return False
+#         return dfs(head)
+
+
+        # Two pointers :Floyd’s cycle finding or Hare-Tortoise algorithm
+
+        def FloydHareTortoise(nodeSlow, nodeFast):
+            if  (nodeSlow and 
+                nodeFast and 
+                nodeSlow.next and 
+                nodeFast.next and
+                nodeFast.next.next):
+                    
+                if nodeSlow == nodeFast:
                     return True
                 else:
-                    lisit_nodes_val.append(node.val)
-                    list_nodes.append(node)
-                    return dfs(node.next)
+                    return FloydHareTortoise(nodeSlow.next, nodeFast.next.next)
             else:
                 return False
+    
+        if head and head.next and head.next.next :
+            slow = head.next
+            fast = head.next.next
+
+            return FloydHareTortoise(slow, fast)
             
-                    
-        return dfs(head)
+        else:
+            return False      
+                
+                
+                
+            
+            
+    
+    
+    
+    
+    
+    
         

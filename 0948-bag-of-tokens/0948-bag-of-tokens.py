48d9dedfa60b38f706b5e3b6b169dffb0f7f9f55
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-03-04 16:42:59 -0600

    Time: 59 ms (20.92%), Space: 16.8 MB (41.42%) - LeetHub

---
+++ 0948-bag-of-tokens/0948-bag-of-tokens.py
@@ -0,0 +1,61 @@
+class Solution:
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        
+        # two pointers: Time O[nLog(n)] n = len(tokens) 
+        #  L = 0, R = len(tokens)-1 
+        #  order tokens
+        #  while L<=R:
+        #  if tokens[L]<power  faceUp: score +=1, L +=1 , power -= tokens[L] 
+        #  else:
+        #     if score > 0      faceDown:  score -=1  R -=1 ,  power += tokens[R] 
+        #       else End 
+        
+        #Trivial sol:
+        if len(tokens) == 0:
+            return 0
+        if len(tokens) == 1:
+            if tokens[0] < power:
+                return 1
+            else:
+                return 0
+        
+        score = 0
+        L = 0 
+        R = len(tokens)-1
+        tokens.sort()
+        
+        while L<=R:
+            print(L,R,tokens[L],tokens[R],power,score)
+            if tokens[L] <= power:
+                #faceUp: 
+                score +=1
+                power -= tokens[L]
+                L +=1  
+            else:
+                if score > 0 and L < R:
+                    #faceDown:  
+                    score -=1  
+                    power += tokens[R] 
+                    R -=1
+                else:
+                    return score
+                
+        return score
+                
+        
+                    
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+        

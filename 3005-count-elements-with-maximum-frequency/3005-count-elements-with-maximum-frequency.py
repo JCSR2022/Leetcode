bba29c754f69796b1d027ed7185fc66c75352854
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-03-08 11:45:54 -0600

    Time: 48 ms (25.91%), Space: 16.6 MB (83.74%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.py
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.py
@@ -3,17 +3,28 @@ from collections import defaultdict
 class Solution:
     def maxFrequencyElements(self, nums: List[int]) -> int:
 
-        hashMap = defaultdict(int)
+#         hashMap = defaultdict(int)
 
-        for n in nums:
-            hashMap[n] += 1
+#         for n in nums:
+#             hashMap[n] += 1
             
-        maxFrecuency = max(hashMap.values())
-        count_maxFrecuency = 0
+#         maxFrecuency = max(hashMap.values())
+#         count_maxFrecuency = 0
         
-        for frec in hashMap.values():
-            if frec == maxFrecuency:
-                count_maxFrecuency += 1
+#         for frec in hashMap.values():
+#             if frec == maxFrecuency:
+#                 count_maxFrecuency += 1
         
-        return count_maxFrecuency*maxFrecuency
+#         return count_maxFrecuency*maxFrecuency
+
+        freq=[0]*101
+        maxF=0
+        for x in nums:
+            freq[x]+=1
+            maxF=max(maxF, freq[x])
+        ans=0
+        for f in freq:
+            if f==maxF:
+                ans+=f
+        return ans
                 

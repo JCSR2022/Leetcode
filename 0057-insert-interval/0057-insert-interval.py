7766007d7d8e0d2bbed1cbc78ddf30f71bf95f5f
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-03-17 15:34:12 -0600

    Time: 79 ms (35.11%), Space: 19.8 MB (79.49%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -3,21 +3,26 @@ class Solution:
         res = []
         
         for i in range(len(intervals)):
+            
             # new interval is less than min in intervals
             if newInterval[1] < intervals[i][0]:
                 res.append(newInterval)
+                #print("finish breaking for cycle")
                 return res + intervals[i:]
-                #return [newInterval] + intervals
-        
+               
             # new interval begin after this interval
             elif newInterval[0] > intervals[i][1]:
                 res.append(intervals[i])
+            
+            # new interval is in this interval
             else: 
                 #merge intervals
                 left = min(newInterval[0],intervals[i][0])
                 right = max(newInterval[1],intervals[i][1])
                 newInterval = [left,right]
-        
+                
+            #print(res,newInterval)
+            
         res.append(newInterval)
-        
+        #print("finish after complete for cycle")
         return res

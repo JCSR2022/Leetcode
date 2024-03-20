ffd92fa84e0df4e62f68775162168a52f75cdf93
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-03-20 08:45:56 -0600

    Time: 191 ms (63.38%), Space: 21.1 MB (31.60%) - LeetHub

--- 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.py
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.py
@@ -12,9 +12,45 @@ class Solution:
         # change a_node.next to point list2
         # change last node in list2 to poitn b+1 node 
         
-        a_node = []
-        b_plus1_node = []
-        last_node_list2 =[]
+#         a_node = []
+#         b_plus1_node = []
+#         last_node_list2 =[]
+        
+#         # finding a_node and b+1 node
+#         node = list1
+#         cont = 1
+#         while node:
+#             #print(node.val,cont)
+#             if cont == a:
+#                 a_node.append(node)
+#             if cont == b+1:
+#                 b_plus1_node.append(node.next)
+#                 break
+#             node = node.next
+#             cont +=1
+   
+#         # find last node on list2
+#         node = list2
+#         while node:
+#             if not node.next:
+#                 last_node_list2.append(node)
+#             node = node.next
+        
+#         # Doing changes:
+        
+#         a_node[0].next = list2
+#         last_node_list2[0].next = b_plus1_node[0]
+        
+ 
+#         return list1    
+
+
+
+
+        a_node = None
+        b_plus1_node = None
+        last_node_list2 = None 
+        
         
         # finding a_node and b+1 node
         node = list1
@@ -22,25 +58,24 @@ class Solution:
         while node:
             #print(node.val,cont)
             if cont == a:
-                a_node.append(node)
+                a_node = node
             if cont == b+1:
-                b_plus1_node.append(node.next)
+                b_plus1_node =node.next
+                break
             node = node.next
             cont +=1
-        
-        #print(a_node,b_plus1_node)
-        
+   
         # find last node on list2
         node = list2
         while node:
             if not node.next:
-                last_node_list2.append(node)
+                last_node_list2 = node
             node = node.next
         
         # Doing changes:
         
-        a_node[0].next = list2
-        last_node_list2[0].next = b_plus1_node[0]
+        a_node.next = list2
+        last_node_list2.next = b_plus1_node
         
  
         return list1    

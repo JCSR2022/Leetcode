2ad4576b6da8f3cc2aa49a0dca0c8ffeed405206
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-03-20 20:08:19 -0600

    Time: 42 ms (23.46%), Space: 17.8 MB (33.99%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -6,28 +6,37 @@
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        # dfs prev_node and node, change next to previus
+#         # dfs prev_node and node, change next to previus
         
-        #specials case:
-        if not head:
-            return head
+#         #specials case:
+#         if not head:
+#             return head
         
-        if not head.next:
-            return head
+#         if not head.next:
+#             return head
         
-        node = head.next
-        prev = head
-        prev.next = None
+#         node = head.next
+#         prev = head
+#         prev.next = None
         
-        while node.next:
-            temp = node.next
-            node.next = prev
+#         while node.next:
+#             temp = node.next
+#             node.next = prev
             
-            prev = node
-            node = temp
+#             prev = node
+#             node = temp
             
-        node.next = prev
-        return node
+#         node.next = prev
+#         return node
+
+        prev = None
+        current = head
+        while current is not None:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        return prev
             
         
         

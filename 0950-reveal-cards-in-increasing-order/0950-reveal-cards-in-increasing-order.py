9d9ccbb15774d6971c28ddd63a95a5403084a6a2
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-04-09 19:06:51 -0600

    Time: 51 ms (22.19%), Space: 16.8 MB (36.98%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.py
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.py
@@ -1,15 +1,23 @@
 class Solution:
     def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
         
-        def rotate(arr):
-            if len(arr)>0:
-                arr[:] = [arr[-1]] + arr[:-1]
+#         def rotate(arr):
+#             if len(arr)>0:
+#                 arr[:] = [arr[-1]] + arr[:-1]
 
-        deck.sort(reverse=True)    
+#         deck.sort(reverse=True)    
 
-        arr = []
+#         arr = []
+#         for card in deck:
+#             rotate(arr)
+#             arr = [card]+arr
+
+#         return arr
+
+        deck.sort(reverse=True)   
+        arr = deque()
         for card in deck:
-            rotate(arr)
-            arr = [card]+arr
-
-        return arr
+            arr.rotate(1) 
+            arr.appendleft(card)
+            
+        return list(arr)

2e85bac270c643b4401f6b62f0877474f5e3b725
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-04-12 13:13:15 -0600

    Time: 8712 ms (5.00%), Space: 18.4 MB (72.62%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -20,91 +20,7 @@ class Solution:
                 
 #         return ans
                 
-    
-            
-        
-        
-        
-        
-        
-        
-        #aproach 2: two pointers
-        # p1 : find a peak if found move p2 until find a peak
-        # if p2 >= p1 calculate the water between p1 and p2:
-        #   calulateWater = for i in range(p1,p2)    sum( min(h[p1],h[p2]) - h[i])
-        #   p1 = p2 continue finding new p2
-        # if p2 > lengt(height): p1 +=1 and p2 = p1 
-        #   until p1 = lengt(height)
-        
-        
-#         def calulateWater(p1,p2):
-#             water = 0
-#             maxhwater = min(height[p1],height[p2])
-#             for i in range(p1+1,p2):
-#                 water += max(maxhwater - height[i],0)
-#             return water
-            
-        
-#         p1 = -1
-#         p2 = 0
-#         peak = False
-#         water = 0
-#         while p1 <= len(height): 
-#             if not peak:
-#                 p1 += 1 
-#                 print(0,(p1,height[p1]),(p2,height[p2]),water)
-#                 if p1 >=len(height):
-#                     break
-                
-#                 if height[p1] >= 0:
-#                     peak = True
-#                     p2 = p1
-#                     p1_peak = height[p1]
-#             else:
-#                 while peak:
-#                     #print(1,p1,p2,height[p2] >= height[p1])
-#                     p2 +=1 
-    
-#                     if p2 >= len(height):
-#                         peak = False
-#                         break
-                    
-#                     if height[p2] >= height[p1]:
-#                         water += calulateWater(p1,p2)
-#                         peak = False
-#                         p1 = p2-1
-#         return water
-                    
-# -------------------------------------------------------------------
-        # aproach 3  pointer and hash table
-        # create a hash_peak with {peak: position, } {0:0} ,   p = 0, height[p] =0   {0:0, 1:1, 2:3}  
-        #
-        #    if height[p] > 0 
-        #        if height[p] >= max(hash_peak.keys())
-        #               water = calulateWater(hash_peak[max(hash_peak.keys())],p)
-        #               del hash_peak[max(hash_peak.keys())]
-        #                hash_peak[height[p]] = p 
-    
-    
-        ##p = 0, height[p] =0   min(hash_peak.keys()) = 0
-                    
-        #           insert into hash_peak {}
-        #        else:
-        #
-        #
-        #    else:
-        #        p +=1
-        #     
-        #    
-        
-        
-#         def calulateWater(p1,p2):
-#             water = 0
-#             maxhwater = min(height[p1],height[p2])
-#             for i in range(p1+1,p2):
-#                 water += max(maxhwater - height[i],0)
-#             return water    
-            
+
 # -------------------------------------------------------------------
 
 
@@ -118,13 +34,57 @@ class Solution:
                 return 0
             return max(height[i+1:])
 
+        max_left_vect = []
+        for i in range(len(height)):
+            max_left_vect.append(max_left_height(i))
+        
+        max_right_vect = []
+        for i in range(len(height)):
+            max_right_vect.append(max_right_height(i))
+        
         water = 0
-        for i,h in enumerate(height):
-            #print(height,i,'max_left:',max_left_height(i) ,'max_right:',max_right_height(i), max(min(max_left_height(i) ,max_right_height(i))-h,0))  
-            water += max(min(max_left_height(i) ,max_right_height(i))-h,0)
+        for i,h in enumerate(height): 
+            water += max(min(max_left_vect[i] ,max_right_vect[i])-h,0)
         
         return water
         
         
         
         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         def max_left_height(i):
+#             if i < 1:
+#                 return 0
+#             return max(height[:i])
+
+#         def max_right_height(i):
+#             if i > len(height)-2:
+#                 return 0
+#             return max(height[i+1:])
+
+        
+#         water = 0
+#         for i,h in enumerate(height):
+#             #print(height,i,'max_left:',max_left_height(i) ,'max_right:',max_right_height(i), max(min(max_left_height(i) ,max_right_height(i))-h,0))  
+#             water += max(min(max_left_height(i) ,max_right_height(i))-h,0)
+        
+#         return water

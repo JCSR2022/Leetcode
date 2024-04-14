c8a6bc36667fe1c31c021e1572a611ab23649c11
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-04-14 05:52:20 -0600

    Time: 567 ms (16.78%), Space: 31.1 MB (38.06%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -14,20 +14,66 @@ class Solution:
         
         # aproach #1 Kadanes: 
         
+#         max_current = max_global = nums[0]
+#         print(0,nums[0],max_current,max_global)
+#         for i in range(1,len(nums)):
+#             max_current = max(nums[i], max_current+nums[i])
+#             max_global = max(max_global,max_current)
+#             print(i,nums[0:i+1],max_current,max_global)
+            
+#         return max_global
+            
+    
+    
+    
         max_current = max_global = nums[0]
         #print(0,nums[0],max_current,max_global)
+        j = k = 0
         for i in range(1,len(nums)):
             max_current = max(nums[i], max_current+nums[i])
-            max_global = max(max_global,max_current)
-            #print(i,nums[0:i+1],max_current,max_global)
+            new_max_global = max(max_global,max_current)
+            if new_max_global > max_global:
+                max_global = new_max_global
+                #j = k = i
+                #while sum(nums[j:i+1]) != max_global:
+                #    j -=1
+            #print(i,nums[i],nums[j:k+1],max_current,max_global)
+        #subarray_largest_sum = nums[j:k+1] 
+        #print("subarray_largest_sum:",subarray_largest_sum)
+        return max_global    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#         current_Left = 0
+#         global_left = 0
+#         max_current = nums[current_Left]
+#         max_global = nums[global_left]
+        
+#         for i in range(1,len(nums)):
+#             current_right = i
             
-        return max_global
+#             if  max_current + nums[current_right] > nums[current_right]:
+#                 max_current = max_current + nums[current_right]
+#             else:
+                
             
-        
-        
-        
-        return 0
-        
+            
+#             max_current = max(nums[i], max_current+nums[i])
+            
+            
+            
+#             max_global = max(max_global,max_current)
+#             print(i,nums[0:i+1],max_current,max_global)
+            
+#         return max_global
+            
         
             
         

1ab71f7d02b7a6fcb5f686519c489e32df420a59
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-04-21 16:40:37 -0600

    Time: 464 ms (5.53%), Space: 20.1 MB (6.53%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,29 +1,98 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
 
-        # construir un decision tree , dfs.
         
-        res = []
+        # brute force:
         
-        def dfs(i,cur,total):
-            #print(i,cur,total)
+        ans = []
+        
+        def DescTree(i,current,total):
+            #print(i,current,total,ans)
             if total == target:
-                res.append(cur.copy())
-                return 
-            
-            if i >= len(candidates) or total > target:
+                ans.append(current.copy())
                 return
             
-            cur.append(candidates[i])
-            dfs(i, cur, total+candidates[i])
-            cur.pop()
-            dfs(i+1, cur, total)
+            if  total > target:
+                return
             
-            
-        candidates.sort()    
-        dfs(0,[],0)
+            for n in candidates:
+                current.append(n)
+                DescTree(i+1,current,total+n)
+                current.pop()
+                
+                
+        candidates.sort()   
+        DescTree(0,[],0)
         
-        return res
+        
+        
+        
+        def filtrar_listas_sin_repetir(lista_de_listas):
+            # Usar un conjunto para almacenar listas únicas
+            # Ordenar la lista para que las listas con los mismos elementos, 
+            # pero en diferente orden, sean consideradas iguales
+            listas_unicas = set()
+            resultado = []
+
+            for lista in lista_de_listas:
+                # Convertir la lista actual en una tupla para poderla agregar al conjunto
+                lista_tupla = tuple(sorted(lista))  
+                if lista_tupla not in listas_unicas:
+                    # Agregar la lista al conjunto y al resultado si no está en el conjunto
+                    listas_unicas.add(lista_tupla)
+                    resultado.append(lista)
+
+            return resultado
+        
+        ans = filtrar_listas_sin_repetir(ans)
+        
+        return ans
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # construir un decision tree , dfs.
+        
+#         res = []
+        
+#         def dfs(i,cur,total):
+#             print(i,cur,total)
+#             if total == target:
+#                 res.append(cur.copy())
+#                 return 
+            
+#             if i >= len(candidates) or total > target:
+#                 return
+            
+#             cur.append(candidates[i])
+#             dfs(i, cur, total+candidates[i])
+#             cur.pop()
+#             dfs(i+1, cur, total)
+            
+#         candidates.sort()    
+#         dfs(0,[],0)
+        
+#         return res
             
             
                 

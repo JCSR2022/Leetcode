6d999c1548b21087ec3484a9f63463767c30b0fd
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-05-01 17:43:41 -0600

    Time: 111 ms (26.87%), Space: 16.7 MB (16.69%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -0,0 +1,59 @@
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        
+        #brute force
+        #dosent work on big numbers
+        
+#         def factorial(n):
+#             ans = 1
+#             for i in range(1,n+1):
+#                 # print(i,ans)
+#                 ans *= i
+                
+#             return ans
+        
+#         fact = str(factorial(n))[::-1]
+#         print(fact[::-1])
+        
+#         zeroes = 0
+#         i = 0
+#         while i <len(fact) and fact[i] == '0' :
+#             i += 1
+#             zeroes += 1
+        
+#         return zeroes
+
+    
+    
+
+        # Matematical aproach:
+        # numb of ceros is the number of 5 in n!
+        # example : 6! = 6*5*4*3*2*1 = 720       1
+        #           10! = 3628800                2
+        #           15! = 1307674368000          3
+        #           20! = 2432902008176640000    4
+        #           25! = 15511210043330985984000000  note here 25 = 5*5...... 6
+        #           30! = 265252859812191058636308480000000...                 7
+        
+        
+        count = 0
+        num = [ i for i in range(1,n+1) if i%5 == 0]
+        #print(num)
+        for elem in num:
+            count += 1
+            while (elem//5)%5 == 0:
+                count += 1
+                elem = elem//5
+                
+        return count
+        
+        
+        
+
+        
+
+
+
+
+        
+        

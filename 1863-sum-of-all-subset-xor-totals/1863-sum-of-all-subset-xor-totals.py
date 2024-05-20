597eba578cc761046f4b5f4f555b453065fa9593
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-05-20 05:43:40 -0600

    Time: 80 ms (23.73%), Space: 17 MB (17.17%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
@@ -1,21 +1,10 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        
-#         for i in range(len(nums)):
-#             for j in range(i+1,len(nums)+1):
-#                 print(nums[i:j])
-                
-#         return 0
-
-#        import itertools
 
         ans = 0 
 
         for i in range(1, len(nums)+1):
             elements = [list(x) for x in itertools.combinations(nums, i)]
-
             #print(elements)
             
             for els in elements:

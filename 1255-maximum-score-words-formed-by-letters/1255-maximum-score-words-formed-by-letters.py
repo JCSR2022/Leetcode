25dbf46e583478561be5299c518ebde84f431566
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-05-23 23:04:53 -0600

    Time: 62 ms (23.36%), Space: 16.6 MB (94.56%) - LeetHub

--- 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.py
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.py
@@ -1,39 +1,85 @@
 class Solution:
     def maxScoreWords(self, words: List[str], letters: List[str], score: List[int]) -> int:
+
+        def can_form_word(w,letter_cnt):
+            word_cnt = Counter(w)
+            for c in word_cnt:
+                if word_cnt[c] > letter_cnt[c]:
+                    return False
+            return True
+        
+        letter_score = {}
+        for i,letter in enumerate("abcdefghijklmnopqrstuvwxyz"):
+            letter_score[letter] = score[i]
+        def get_score(w):
+            return sum([letter_score[x] for x in w])
         
         
-        counter = Counter(letters)
-        n = len(words)
-        
-        def backTrack(start,letter_counter):
-            if start == n:
+        letter_cnt = Counter(letters)
+        def backtrack(i):
+
+            if i == len(words):
                 return 0
             
-            ans = 0
+            res = backtrack(i+1) # skip word
             
-            for i in range(start,n):
-                temp_counter = copy.deepcopy(letter_counter)
-                word = words[i]
-                word_score = 0
-                isValid = True
+            if can_form_word(words[i],letter_cnt):
                 
-                for c in word:
-                    if c in temp_counter and temp_counter[c] > 0:
-                        temp_counter[c] -= 1
-                        word_score += score[ord(c)-ord('a')]
-                    else:
-                        isValid = False
-                        break
-                   
-                if isValid:
-                    ans = max(ans,backTrack(i+1,temp_counter) + word_score)
+                for c in words[i]:
+                    letter_cnt[c] -= 1
+                
+                res = max(res,get_score(words[i])+ backtrack(i+1)  )
+                
+                for c in words[i]:
+                    letter_cnt[c] += 1
                     
-            return ans 
+            return res
+        
+        return backtrack(0)
+                
+                
+                
+                
+                
+                
         
         
-        return backTrack(0,counter)
         
         
+        
+#------------------------------------------------------           
+#         counter = Counter(letters)
+#         n = len(words)
+        
+#         def backTrack(start,letter_counter):
+#             if start == n:
+#                 return 0
+            
+#             ans = 0
+            
+#             for i in range(start,n):
+#                 temp_counter = copy.deepcopy(letter_counter)
+#                 word = words[i]
+#                 word_score = 0
+#                 isValid = True
+                
+#                 for c in word:
+#                     if c in temp_counter and temp_counter[c] > 0:
+#                         temp_counter[c] -= 1
+#                         word_score += score[ord(c)-ord('a')]
+#                     else:
+#                         isValid = False
+#                         break
+                   
+#                 if isValid:
+#                     ans = max(ans,backTrack(i+1,temp_counter) + word_score)
+                    
+#             return ans 
+        
+        
+#         return backTrack(0,counter)
+        
+#------------------------------------------------------    
         
 #         Letters_hash = {}
 #         for letter in letters:

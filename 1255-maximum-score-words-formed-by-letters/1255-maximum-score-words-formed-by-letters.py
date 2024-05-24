601fb2da90e354618e2a64ffffaf260d3a6ab433
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-05-23 21:39:16 -0600

    Time: 65 ms (22.45%), Space: 16.5 MB (94.56%) - LeetHub

---
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.py
@@ -0,0 +1,55 @@
+class Solution:
+    def maxScoreWords(self, words: List[str], letters: List[str], score: List[int]) -> int:
+        
+        
+        counter = Counter(letters)
+        n = len(words)
+        
+        def backTrack(start,letter_counter):
+            if start == n:
+                return 0
+            
+            ans = 0
+            
+            for i in range(start,n):
+                temp_counter = copy.deepcopy(letter_counter)
+                word = words[i]
+                word_score = 0
+                isValid = True
+                
+                for c in word:
+                    if c in temp_counter and temp_counter[c] > 0:
+                        temp_counter[c] -= 1
+                        word_score += score[ord(c)-ord('a')]
+                    else:
+                        isValid = False
+                        break
+                   
+                if isValid:
+                    ans = max(ans,backTrack(i+1,temp_counter) + word_score)
+                    
+            return ans 
+        
+        
+        return backTrack(0,counter)
+        
+        
+        
+#         Letters_hash = {}
+#         for letter in letters:
+#             if letter in Letters_hash:
+#                 Letters_hash[letter] += 1
+#             else:
+#                 Letters_hash[letter] = 1
+        
+        
+#         letter_score = {}
+#         for i,letter in enumerate("abcdefghijklmnopqrstuvwxyz"):
+#             letter_score[letter] = score[i]
+        
+        
+#         print(Letters_hash)
+#         print(letter_score)
+        
+        
+#         return 0 

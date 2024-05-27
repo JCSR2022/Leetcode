3abf51991eb348f783651e321fba073ef0f8e900
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-05-26 20:02:17 -0600

    Time: 37 ms (79.23%), Space: 16.6 MB (48.53%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
@@ -1,14 +1,28 @@
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
-        #maldita sea brute force
         
-        for i in range(len(nums)+1):
-            cont = 0
-            for n in nums:
-                if n >= i: cont +=1
-            #print(i,cont)
-            if i == cont:
-                return i
+#         #maldita sea brute force
+#         for i in range(len(nums)+1):
+#             cont = 0
+#             for n in nums:
+#                 if n >= i: cont +=1
+#             #print(i,cont)
+#             if i == cont:
+#                 return i
         
+#         return -1
+        
+    
+        n = len(nums)
+        l, r = 0, n
+        nums.sort()
+        while l <= r:
+            mid = (l + r) // 2
+            count = n - bisect.bisect_left(nums, mid)
+            if count == mid:
+                return mid
+            elif count > mid:
+                l = mid + 1
+            else:
+                r = mid - 1
         return -1
-        

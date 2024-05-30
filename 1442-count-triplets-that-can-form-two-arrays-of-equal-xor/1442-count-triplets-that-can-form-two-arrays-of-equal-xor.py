8ca670bd2f1d972fd07b6c9f1d1f668335a2ef31
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-05-30 11:18:12 -0600

    Time: 54 ms (48.85%), Space: 16.5 MB (66.09%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.py
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.py
@@ -13,19 +13,40 @@ class Solution:
         #                 cont += 1        
         # return cont
 
-        cont = 0 
-        for i in range(len(arr)-1):
-            a = 0
-            for j in range(i+1,len(arr)):
-                a ^= arr[j-1]
-                b = 0
-                for k in range(j,len(arr)):
-                    b ^= arr[k]
-                    #print(i,j,k,arr[i:j],arr[j:k+1],a,b,a==b)
-                    if a == b:
-                        cont += 1        
-        return cont        
+        
+        # # O(n**3) sol
+        # cont = 0 
+        # for i in range(len(arr)-1):
+        #     a = 0
+        #     for j in range(i+1,len(arr)):
+        #         a ^= arr[j-1]
+        #         b = 0
+        #         for k in range(j,len(arr)):
+        #             b ^= arr[k]
+        #             #print(i,j,k,arr[i:j],arr[j:k+1],a,b,a==b)
+        #             if a == b:
+        #                 cont += 1        
+        # return cont        
 
+        
+        # usign xor propeties (two pointers) n**2
+        
+        cont = 0
+        for x in range(len(arr)-1):
+            temp_xor = arr[x]
+            for y in range(x+1,len(arr)):
+                temp_xor ^= arr[y]
+                if temp_xor == 0:
+                    cont += y-x
+                    
+        return cont
+                    
+                    
+                
+        
+        
+        
+        
         
                     
         

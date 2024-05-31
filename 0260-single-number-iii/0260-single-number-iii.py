a4ce23917d52a145a1640f3f8350e91a4336ba56
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-05-31 05:48:24 -0600

    Time: 48 ms (95.40%), Space: 18.5 MB (76.83%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -1,16 +1,43 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
-        #brute force
         
-        hashMap = defaultdict(int)
+#         #brute force  Time O(n)  space O(n)
+#         hashMap = defaultdict(int)
+#         for n in nums:
+#             hashMap[n] += 1
+        
+#         ans = []
+#         for key,val in hashMap.items():
+#             if val == 1:
+#                 ans.append(key)
+#         return ans
+
+        #xor solution time O(n) sapace O(1) ??
+     
+        # XOR of list while return xor of only diff nums
+        xor =  0
+        for n in nums:
+            xor ^= n 
+        
+        
+        # Finding bits that differ
+        diff_bit = 1 
+        while not (xor & diff_bit):
+            diff_bit = diff_bit << 1
+            
+        a,b = 0,0
         
         for n in nums:
-            hashMap[n] += 1
+            if diff_bit & n:
+                a = a ^ n
+            else:
+                b = b ^ n
+                
+        return [a,b]
+                
+            
+            
+            
+            
+            
         
-        ans = []
-        
-        for key,val in hashMap.items():
-            if val == 1:
-                ans.append(key)
-        
-        return ans

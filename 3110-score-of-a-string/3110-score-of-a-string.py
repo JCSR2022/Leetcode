37de67b53bf9012eefc41fe00069111ad42a279d
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-06-01 05:37:25 -0600

    Time: 30 ms (90.60%), Space: 16.4 MB (98.12%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.py
+++ 3110-score-of-a-string/3110-score-of-a-string.py
@@ -5,8 +5,10 @@ class Solution:
         
         ans = 0
         
+        s_list = [ ord(k) for k in s]
+        
         for i in range(1,len(s)):
-            ans += abs(ord(s[i-1])-ord(s[i]))    
+            ans += abs(s_list[i-1]-s_list[i])    
     
         return ans
         

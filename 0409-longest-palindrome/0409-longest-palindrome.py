1d566a7fcc69b2143921234dbd8fb6aadde25a41
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-06-03 22:38:27 -0600

    Time: 31 ms (89.43%), Space: 16.5 MB (80.97%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,28 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        
+        #hashMap
+        
+        hashMap = {}
+        
+        for l in s:
+            if l in hashMap:
+                hashMap[l] +=1
+            else:
+                hashMap[l] = 1
+        
+
+        even_count = 0
+        count = 0
+        for k,x in hashMap.items():
+            #print(k,x,x%2 != 0,even_count)
+            count += x 
+            if x%2 != 0:
+                even_count += 1
+            #print(even_count)
+        return count - ( even_count -1  if even_count > 0 else even_count )  
+    
+    
+    
+    
+    

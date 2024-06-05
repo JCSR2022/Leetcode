7d6016b1012ee4e1b70d9c20be2622feca7f8bc1
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-06-04 23:24:17 -0600

    Time: 54 ms (32.67%), Space: 16.5 MB (93.72%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.py
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -3,34 +3,63 @@ class Solution:
         
         #brute aproach, as you, brute,  hashmap
         
-        def find_hash(word):
-            hash = {}
-            for l in word:
-                if l not in hash:
-                    hash[l] = 1
-                else: 
-                    hash[l] += 1
-            return hash 
+#         def find_hash(word):
+#             hash = {}
+#             for l in word:
+#                 if l not in hash:
+#                     hash[l] = 1
+#                 else: 
+#                     hash[l] += 1
+#             return hash 
         
-        hashWord = find_hash(words[0])
+#         hashWord = find_hash(words[0])
         
-        for i in range(1,len(words)):
-            Newhash = find_hash(words[i])
+#         for i in range(1,len(words)):
+#             Newhash = find_hash(words[i])
 
-            for key,val in hashWord.items():
-                if key in Newhash:
-                    hashWord[key] = min(hashWord[key],Newhash[key])
-                else:
-                    hashWord[key] = 0
+#             for key,val in hashWord.items():
+#                 if key in Newhash:
+#                     hashWord[key] = min(hashWord[key],Newhash[key])
+#                 else:
+#                     hashWord[key] = 0
                     
                 
-        ans = []        
+#         ans = []        
         
-        for key,val in hashWord.items():
-            if val > 0:
-                ans += [key]*val
+#         for key,val in hashWord.items():
+#             if val > 0:
+#                 ans += [key]*val
         
+#         return ans
+
+        
+        letters ={ l:ord(l)-97 for l in 'abcdefghijklmnopqrstuvwxyz' } 
+
+        def findHasword(word): 
+            hashWord = [0]*26
+            for l in word:
+                hashWord[letters[l]] += 1
+            return hashWord
+
+        hashW = [float(inf)]*26
+        
+        for word in words:
+            newhashW = findHasword(word)
+            
+            hashW = [min(x,y) for x,y in zip(hashW,newhashW) ]
+            
+        ans = []
+        for i in range(len(hashW)):
+            if hashW[i] > 0:
+                ans += [chr(i+97)]*hashW[i]
+            
         return ans
+            
+        
+            
+        
+        
+        
             
             
             

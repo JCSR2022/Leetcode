d40781ac3112ae85c8e7bc181b597eb6a9bc670c
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-06-05 22:00:12 -0600

    Time: 155 ms (83.86%), Space: 18.4 MB (63.87%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -1,47 +1,89 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        #brute brute force:
         
+        # https://www.youtube.com/watch?v=amnrMCVd2YI
         
-        
-        def isConsecutive(group):
-            #print('group',group)
-            
-            if len(group) <= 1:
-                return True
-    
-            for i in range(1,len(group)):
-                if group[i] != group[i-1] + 1:
-                    return False
-            return True
-                
-        #print(len(hand),groupSize,len(hand)%groupSize)
-        
-        if len(hand)%groupSize != 0:
+        if len(hand) % groupSize:
             return False
         
-        
-        hand.sort()
-        #print(hand)
-        
-        group_sortHand = [ [] for _ in range(len(hand)//groupSize) ]
-        
-        for card in hand:
-            for group in group_sortHand:
-                if card not in group and len(group)<groupSize:
-                    group.append(card)
-                    break
-        
-        #print(group_sortHand)
+        count = {}
+        for n in hand:
+            count[n] = 1 + count.get(n,0)
+            
+            
+        minH = list(count.keys())
+        heapq.heapify(minH)
         
         
-        for group in group_sortHand:
-            if len(group) != groupSize:
-                return False
-            if not isConsecutive(group):
-                return False
-
+        while minH:
+            first = minH[0]
+            
+            for i in range( first,first+groupSize ):
+                if i not in count:
+                    return False
+                count[i] -=1
+                if count[i] == 0:
+                    if i != minH[0]:
+                        return False
+                    heapq.heappop(minH)
+                    
         return True
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         #brute brute force:   
+#         def isConsecutive(group):
+#             #print('group',group)
+            
+#             if len(group) <= 1:
+#                 return True
+    
+#             for i in range(1,len(group)):
+#                 if group[i] != group[i-1] + 1:
+#                     return False
+#             return True
+                
+#         #print(len(hand),groupSize,len(hand)%groupSize)
+        
+#         if len(hand)%groupSize != 0:
+#             return False
+        
+        
+#         hand.sort()
+#         #print(hand)
+        
+#         group_sortHand = [ [] for _ in range(len(hand)//groupSize) ]
+        
+#         for card in hand:
+#             for group in group_sortHand:
+#                 if card not in group and len(group)<groupSize:
+#                     group.append(card)
+#                     break
+        
+#         #print(group_sortHand)
+        
+        
+#         for group in group_sortHand:
+#             if len(group) != groupSize:
+#                 return False
+#             if not isConsecutive(group):
+#                 return False
+
+#         return True
     
     
     

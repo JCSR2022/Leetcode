3eb1299e17eb1a6229e64a803cb83277574a26c1
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-06-09 19:09:46 -0600

    Time: 49 ms (7.50%), Space: 16.5 MB (77.64%) - LeetHub

--- 1051-height-checker/1051-height-checker.py
+++ 1051-height-checker/1051-height-checker.py
@@ -1,13 +1,37 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
         
-        # brute force oreder and compare
-        expected = heights.copy()
-        expected.sort()
+#         # brute force oreder and compare
+#         expected = heights.copy()
+#         expected.sort()
+#         res = 0
+#         for i in range(len(heights)):
+#             if heights[i] != expected[i]: res +=1
+            
+#         return res
+
+
+
+# 1 <= heights.length <= 100
+# 1 <= heights[i] <= 100
+        # using an sort method fast for small arr
+    
+        count = [0 for n in range(101)]
+
+        for h in heights:
+            count[h] += 1
+        
+        expected = []
+        for i,n in enumerate(count):
+            if n > 0:
+                expected += [i]*n
+                
+        #print(expected)
+        
         res = 0
         for i in range(len(heights)):
             if heights[i] != expected[i]: res +=1
             
         return res
-
-  
+        
+            

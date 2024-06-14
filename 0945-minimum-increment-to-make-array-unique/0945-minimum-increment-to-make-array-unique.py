e502b2dd871a2046383bfbd2635369fa7514c4af
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-06-13 20:43:29 -0600

    Time: 728 ms (7.66%), Space: 35.3 MB (7.21%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.py
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.py
@@ -15,18 +15,28 @@ class Solution:
         
         
         #https://www.youtube.com/watch?v=XPPs2Wj2YSk
+#         # sorted and to pointer sol nLog(n)
+#         nums.sort()
+#         res = 0
         
-        nums.sort()
-        res = 0
-        
-        for i in range(1,len(nums)):
-            if nums[i -1] >= nums[i]:
-                res += 1 + nums[i-1]-nums[i]
-                nums[i] = nums[i-1] + 1
+#         for i in range(1,len(nums)):
+#             if nums[i -1] >= nums[i]:
+#                 res += 1 + nums[i-1]-nums[i]
+#                 nums[i] = nums[i-1] + 1
                 
-        return res
+#         return res
+            
+        count = Counter(nums)
+        ans = 0
+        
+        for i in range(len(nums)+max(nums)):
+            if count[i] > 1:
+                extra = count[i] - 1
+                count[i+1] += extra
+                ans += extra
             
         
+        return ans
         
         
         

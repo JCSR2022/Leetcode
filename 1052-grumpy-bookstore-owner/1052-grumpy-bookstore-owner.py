7995e78a635f7e7a03e784793145d67d4725973a
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-06-20 23:05:37 -0600

    Time: 222 ms (33.33%), Space: 19 MB (14.93%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
@@ -1,17 +1,33 @@
 class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
         
-        SumSatCus = sum(c for c, g in zip(customers, grumpy) if g == 0)
-
-        UnSatCus = [c if g == 1 else 0 for c, g in zip(customers, grumpy)]
-
-        #print(SumSatCus,UnSatCus)
+        l = 0
+        window = 0
+        max_window = 0
+        statisfied = 0
+        for r in range(len(customers)):
+            if grumpy[r]:
+                window += customers[r]
+            else:
+                statisfied += customers[r]
+                
+            if r - l + 1 > minutes:
+                if grumpy[l]:
+                    window -= customers[l]
+                l += 1
+            max_window = max(max_window,window)
+            
+        return statisfied+max_window
+                
         
-        SumUnSatCus = 0
-        for i in range(len(UnSatCus)):
-            #print(UnSatCus[i:i+minutes], sum(UnSatCus[i:i+minutes]))
-            SumUnSatCus = max(SumUnSatCus,sum(UnSatCus[i:i+minutes]))
-
-      
         
-        return SumUnSatCus + SumSatCus
+        
+#         SumSatCus = sum(c for c, g in zip(customers, grumpy) if g == 0)
+#         UnSatCus = [c if g == 1 else 0 for c, g in zip(customers, grumpy)]
+
+#         #print(SumSatCus,UnSatCus)
+#         SumUnSatCus = 0
+#         for i in range(len(UnSatCus)):
+#             #print(UnSatCus[i:i+minutes], sum(UnSatCus[i:i+minutes]))
+#             SumUnSatCus = max(SumUnSatCus,sum(UnSatCus[i:i+minutes]))
+#         return SumUnSatCus + SumSatCus

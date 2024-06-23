92d06ae413bff9868b035b917d36bb0c999625b5
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-06-23 12:31:58 -0600

    Time: 436 ms (76.41%), Space: 26.5 MB (45.01%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,30 +1,76 @@
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
         
-        increase = deque()
-        decrease = deque()
-        max_length = 0
-        left = 0
-
-        for right in range(len(nums)):
-            while increase and nums[right] < increase[-1]:
-                increase.pop()
-            increase.append(nums[right])
-            
-            while decrease and nums[right] > decrease[-1]:
-                decrease.pop()
-            decrease.append(nums[right])
-            
-            while decrease[0] - increase[0] > limit:
-                if nums[left] == increase[0]:
-                    increase.popleft()
-                if nums[left] == decrease[0]:
-                    decrease.popleft()
-                left += 1
-                
-            max_length = max(max_length, right - left + 1)
         
-        return max_length
+        #No lo pude hacer funcionar porque la logica esta mal, necesito usar
+        # una cola para almacenar los maximos y minimos, cuando se mueve
+        # el puntero izquierdo necesito encontrar el maximo y minimo remanente 
+        # en la ventana
+        
+        # Monoctonic queue 
+        #https://www.youtube.com/watch?v=V-ecDfY5xEw
+        
+        min_q = deque() #monotonic increasing
+        max_q = deque() #monotonic decreasing
+        
+        l = 0
+        res = 0
+        
+        for r in range(len(nums)):
+        
+            while min_q and nums[r] < min_q[-1]:
+                min_q.pop()
+                
+            while max_q and nums[r] > max_q[-1]:
+                max_q.pop()
+                
+            min_q.append(nums[r])
+            max_q.append(nums[r])
+            
+            while max_q[0] - min_q[0] > limit:
+                if nums[l] == max_q[0]:
+                    max_q.popleft()
+                if nums[l] == min_q[0]:
+                    min_q.popleft()
+                l += 1
+            
+            
+            res = max(res,r - l + 1)
+        
+        
+        return res
+        
+        
+        
+        
+        
+        
+        
+#        
+#         increase = deque()
+#         decrease = deque()
+#         max_length = 0
+#         left = 0
+
+#         for right in range(len(nums)):
+#             while increase and nums[right] < increase[-1]:
+#                 increase.pop()
+#             increase.append(nums[right])
+            
+#             while decrease and nums[right] > decrease[-1]:
+#                 decrease.pop()
+#             decrease.append(nums[right])
+            
+#             while decrease[0] - increase[0] > limit:
+#                 if nums[left] == increase[0]:
+#                     increase.popleft()
+#                 if nums[left] == decrease[0]:
+#                     decrease.popleft()
+#                 left += 1
+                
+#             max_length = max(max_length, right - left + 1)
+        
+#         return max_length
         
         
         

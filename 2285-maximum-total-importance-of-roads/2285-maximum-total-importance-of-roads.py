a07d8edeaff482e2e6fa6dbc413b5c56321f762b
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-06-27 20:46:55 -0600

    Time: 1200 ms (81.97%), Space: 41.5 MB (81.15%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.py
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.py
@@ -2,24 +2,46 @@ class Solution:
     def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
         
         # approach: find num of age on nodes, order and give bigest valu to more_ages_nodes
+        # time nLog(n)  where n is amount of edges , Log because sort // SApce O(n)
         
+#         edges = defaultdict(int)
+#         for edge in roads:
+#             edges[edge[0]] +=1
+#             edges[edge[1]] +=1
+#         #print(edges)
         
-        edges = defaultdict(int)
-        for edge in roads:
-            edges[edge[0]] +=1
-            edges[edge[1]] +=1
-        #print(edges)
+#         sorted_edges = dict(sorted(edges.items(), key=lambda item: item[1], reverse=True))
+#         #print(sorted_edges)
         
-        sorted_edges = dict(sorted(edges.items(), key=lambda item: item[1], reverse=True))
-        #print(sorted_edges)
+#         nodes_val = defaultdict(int)
+#         for val,node in enumerate(sorted_edges.keys()):
+#             nodes_val[node] = n-val
+#         #print( nodes_val)
         
-        nodes_val = defaultdict(int)
-        for val,node in enumerate(sorted_edges.keys()):
-            nodes_val[node] = n-val
-        #print( nodes_val)
+#         ans = 0
+#         for n1,n2 in roads:
+#             ans += nodes_val[n1] + nodes_val[n2]
         
-        ans = 0
+#         return ans
+
+        #SAme aproach optimiztion:
+    
+        edge_cnt = [0]*n
+        
         for n1,n2 in roads:
-            ans += nodes_val[n1] + nodes_val[n2]
+            edge_cnt[n1] +=1
+            edge_cnt[n2] +=1    
+        #print(edge_cnt)
         
-        return ans
+        label = 1
+        res = 0
+        for count in sorted(edge_cnt):
+            res += label*count
+            label+=1
+        
+        return res
+        
+            
+            
+            
+            

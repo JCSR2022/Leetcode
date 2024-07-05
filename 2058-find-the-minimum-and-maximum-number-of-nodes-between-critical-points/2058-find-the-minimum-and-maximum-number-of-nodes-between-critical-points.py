0f5321c878a68c14315d776b2338803d644a0aca
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-07-04 18:54:14 -0600

    Time: 349 ms (63.12%), Space: 44.3 MB (58.17%) - LeetHub

---
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -0,0 +1,90 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
+        
+        
+        
+        #1. def is critical(node) find critical point return true or false
+        #  dfs if critical safe pos in arr
+        
+        def iscritical(pre_node,node):
+            if node.next:
+                # minimum
+                if pre_node.val > node.val and node.next.val > node.val:
+                    return True
+                # maximum
+                if pre_node.val < node.val and node.next.val < node.val:
+                    return True
+            return False
+            
+            
+        cont  =  0
+        criticals = []
+        pre_node = head
+        node = pre_node.next
+        
+        while node:
+            if iscritical(pre_node,node):
+                criticals.append(cont)
+            pre_node = node
+            node = node.next
+            cont +=1
+        
+        #print(criticals)
+        
+        if len(criticals) < 2:
+            return [-1,-1]
+        
+        min_dist = float('inf')
+        for i in range(len(criticals)-1):
+            min_dist = min(min_dist,criticals[i+1]-criticals[i])
+            
+        max_dist = criticals[-1]-criticals[0]
+        
+        return [min_dist,max_dist]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        

9281784442f751b558bce6bee8dcac3a3b8b9bae
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-07-07 19:49:56 -0600

    Time: 33 ms (84.79%), Space: 16.4 MB (84.30%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,16 +1,21 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        #simple non math way, brute, realy brute force
         
-        arr = [i for i in range(1,n+1)]
-        #print(arr)
+        winner = 0  # Josephus problem is 0-indexed in calculation
+        for i in range(1, n + 1):
+            winner = (winner + k) % i
+        return winner + 1
+        
+        
+#         #simple non math way, brute, realy brute force  
+#         arr = [i for i in range(1,n+1)]
+#         #print(arr)
      
-        
-        i = 0
-        while arr:
-            i = (i+ k-1)%(len(arr))
-            ans = arr.pop(i)
-            #print(arr)
+#         i = 0
+#         while arr:
+#             i = (i + k-1)%(len(arr))
+#             ans = arr.pop(i)
+#             #print(arr)
 
-        return ans
+#         return ans
         

d552acd80f3888f34bb8c430f1247ed6032e8b13
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-07-10 14:01:45 -0600

    Time: 41 ms (51.57%), Space: 16.6 MB (43.13%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,26 +1,61 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
-        #dinamic programin aproach, botom up aproach
-        def WordsInDict(i,s,wordDict):
-            posibles = []
+#         #dinamic programin aproach, botom up aproach
+
+
+
+
+        def isWordsInDict(i,s,wordDict):
             for word in wordDict:
                 if s[i] == word[0]:
                     if s[i:i+len(word)] == word:
-                        posibles.append(word)
-            return posibles
+                        return True ,word     
+            return False , -1
+        
         
         dp = [False]*len(s)
         dp.append(True)
         
         for i in range(len(s)-1,-1,-1):
-            words = WordsInDict(i,s,wordDict)
-            #print(i,s[i],words,dp)
-            if words:
-                for word in words:
-                    dp[i] = dp[i] | dp[i+len(word)] 
-        
+            for word in wordDict:
+                if s[i:i+len(word)] == word:
+                    dp[i] = dp[i+len(word)] 
+                if dp[i]:
+                    break
         #print(dp)
         
         return dp[0]
+
+
+
+
+
+
+
+
+
+
+
+#         def WordsInDict(i,s,wordDict):
+#             posibles = []
+#             for word in wordDict:
+#                 if s[i] == word[0]:
+#                     if s[i:i+len(word)] == word:
+#                         posibles.append(word)
+#             return posibles
+        
+#         dp = [False]*len(s)
+#         dp.append(True)
+        
+#         for i in range(len(s)-1,-1,-1):
+#             words = WordsInDict(i,s,wordDict)
+#             #print(i,s[i],words,dp)
+#             if words:
+#                 for word in words:
+#                     dp[i] = dp[i] | dp[i+len(word)] 
+        
+#         #print(dp)
+        
+#         return dp[0]
         

01c829b1ece529b3994fd114524ac13c1a8d95ce
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-07-10 11:13:53 -0600

    Time: 51 ms (87.11%), Space: 18.6 MB (64.77%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,15 +1,25 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         
-        hashMap = defaultdict(int)
+        ones = 0
+        twos = 0
+
+        for num in nums:
+          ones ^= (num & ~twos)
+          twos ^= (num & ~ones)
+
+        return ones
         
-        for n in nums:
-            hashMap[n] += 1
         
-        for k,v in hashMap.items():
-            if v == 1:
-                return k
+#         hashMap = defaultdict(int)
+        
+#         for n in nums:
+#             hashMap[n] += 1
+        
+#         for k,v in hashMap.items():
+#             if v == 1:
+#                 return k
             
-        return -1
+#         return -1
         
         

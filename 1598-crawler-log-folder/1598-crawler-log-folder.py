708768c69b9031a9e7b9aaae0f463fe9099ad3b5
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-07-09 18:20:31 -0600

    Time: 48 ms (63.33%), Space: 16.7 MB (82.14%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        
+        #brute force asume only options are as explain in problem
+        
+        ans = 0
+        
+        for move in logs:
+            m = move.strip('/')
+            #print(move,m)
+            if m == "..":
+                ans = max(0,ans-1)
+            elif m == '.':
+                pass
+            else:
+                ans += 1
+            
+        return ans
+        

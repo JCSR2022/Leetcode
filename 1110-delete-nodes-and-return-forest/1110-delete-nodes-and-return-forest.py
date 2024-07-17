551cdf2ea7868ddc1f72cb297b4d290756fc4915
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-07-17 16:04:43 -0600

    Time: 62 ms (21.58%), Space: 17.1 MB (7.44%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.py
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.py
@@ -7,41 +7,46 @@
 class Solution:
     def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
         
-        
+        s = set(to_delete)
+        res = []
+        def dfs(root, flag):
+            if not root: return None
+            toDelete = (root.val in s)
+            root.left = dfs(root.left, toDelete)
+            root.right = dfs(root.right, toDelete)
+            if not toDelete and flag: res.append(root)
+            return None if toDelete else root
+        dfs(root, True)
+        return res
 
         
-        # make dfs in reverse order so can delete nodes 
+#         # make post order traversal dfs  so can delete nodes 
         
-        deleted = []
+#         deleted = []
         
-        if root.val not in to_delete:
-            deleted.append(root)
+#         if root.val not in to_delete:
+#             deleted.append(root)
         
-        def reverse_DFS(node,prev_node,r):
-            if not node:
-                return
+#         def reverse_DFS(node,prev_node,r):
+#             if not node:
+#                 return
             
-            if node.left: reverse_DFS(node.left,node,False)
-            if node.right: reverse_DFS(node.right,node,True)
+#             if node.left: reverse_DFS(node.left,node,False)
+#             if node.right: reverse_DFS(node.right,node,True)
             
-            if node.val in to_delete:
+#             if node.val in to_delete:
                 
-                if node.left:   deleted.append(node.left)
-                if node.right:  deleted.append(node.right)   
+#                 if node.left:   deleted.append(node.left)
+#                 if node.right:  deleted.append(node.right)   
                 
-                if r: 
-                    prev_node.right = None
-                else:
-                    prev_node.left  =  None
-                
-                
+#                 if r: 
+#                     prev_node.right = None
+#                 else:
+#                     prev_node.left  =  None
             
-        dumy = TreeNode(None)   
-        dumy.right = root    
-        reverse_DFS(root,dumy,True)
-        
+#         dumy = TreeNode(None)   
+#         dumy.right = root    
+#         reverse_DFS(root,dumy,True)
 
-        
-
-        return deleted
+#         return deleted
         

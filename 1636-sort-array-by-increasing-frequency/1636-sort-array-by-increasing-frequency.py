19f194e93cea4dbc10098ab79bac192dd3f8f003
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-07-23 17:45:37 -0600

    Time: 57 ms (15.05%), Space: 16.7 MB (12.65%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
@@ -1,11 +1,11 @@
 class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
         
-        ctr = Counter(nums).items()                         # <-- 1)
+#         ctr = Counter(nums).items()                         # <-- 1)
 
-        arr = sorted(ctr, key = lambda x: (x[1], -x[0]))    # <-- 2)
+#         arr = sorted(ctr, key = lambda x: (x[1], -x[0]))    # <-- 2)
 
-        return chain(*[[n]* cnt for n,cnt in arr])          # <-- 3)        
+#         return chain(*[[n]* cnt for n,cnt in arr])          # <-- 3)        
         
         
         
@@ -37,23 +37,23 @@ class Solution:
 # My solution:
 #         #hashmap
 
-#         hasnums = defaultdict(int)
-#         for n in nums: hasnums[n] += 1
+        hasnums = defaultdict(int)
+        for n in nums: hasnums[n] += 1
         
-#         num_vect = [ [n,cant] for n,cant in hasnums.items()]
-#         print(num_vect)
+        num_vect = [ [n,cant] for n,cant in hasnums.items()]
+        print(num_vect)
         
-#         num_vect.sort(reverse = True)
-#         print(num_vect)
+        num_vect.sort(reverse = True)
+        print(num_vect)
         
-#         num_vect = sorted(num_vect, key=lambda x: x[1])
-#         print(num_vect)
+        num_vect = sorted(num_vect, key=lambda x: x[1])
+        print(num_vect)
         
-#         ans = []
-#         for n,cant in num_vect:
-#             ans += [n]*cant
+        ans = []
+        for n,cant in num_vect:
+            ans += [n]*cant
             
-#         return ans
+        return ans
         
         
         

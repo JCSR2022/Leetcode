b2b5d2dbfceb48f5a04c4a59424b1ef06a9cde02
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-07-23 17:44:18 -0600

    Time: 54 ms (29.51%), Space: 16.4 MB (77.81%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
@@ -1,12 +1,40 @@
 class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
         
-        freq = Counter(nums)
-        nums.sort(key=lambda x: (freq[x], -x))
-        return nums
+        ctr = Counter(nums).items()                         # <-- 1)
+
+        arr = sorted(ctr, key = lambda x: (x[1], -x[0]))    # <-- 2)
+
+        return chain(*[[n]* cnt for n,cnt in arr])          # <-- 3)        
         
         
         
+#         freq = Counter(nums)
+#         nums.sort(key=lambda x: (freq[x], -x))
+#         return nums
+        
+# This line sorts the nums array in place using a custom key. The key parameter of the sort method is a function that extracts a comparison key from each list element.
+# The lambda function lambda x: (freq[x], -x) returns a tuple for each element x in nums. This tuple contains two values:
+
+# freq[x]: The frequency of the element x.
+# -x: The negative value of x.
+# The sort method sorts primarily by the first element of the tuple (frequency), in ascending order (lower frequency comes first). If two elements have the same frequency, it uses the second element of the tuple to break the tie, sorting in descending order (higher value comes first) because -x will make higher values appear before lower values.
+
+# Example:
+# For nums = [4, 4, 2, 3, 2, 2]:
+
+# Frequencies: {2: 3, 4: 2, 3: 1}
+# The lambda function will generate the keys as follows:
+# For 4: (2, -4)
+# For 2: (3, -2)
+# For 3: (1, -3)
+# So, the sorted keys will be [(1, -3), (2, -4), (2, -4), (3, -2), (3, -2), (3, -2)], resulting in the sorted array [3, 4, 4, 2, 2, 2].
+        
+        
+        
+        
+        
+# My solution:
 #         #hashmap
 
 #         hasnums = defaultdict(int)

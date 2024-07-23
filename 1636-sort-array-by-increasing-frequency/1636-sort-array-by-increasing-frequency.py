cb20a9019d36607d65c1487c426e015a079aa055
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-07-23 17:34:12 -0600

    Time: 54 ms (29.51%), Space: 16.6 MB (38.76%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
@@ -1,24 +1,31 @@
 class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
-        #hashmap
+        
+        freq = Counter(nums)
+        nums.sort(key=lambda x: (freq[x], -x))
+        return nums
+        
+        
+        
+#         #hashmap
 
-        hasnums = defaultdict(int)
-        for n in nums: hasnums[n] += 1
+#         hasnums = defaultdict(int)
+#         for n in nums: hasnums[n] += 1
         
-        num_vect = [ [n,cant] for n,cant in hasnums.items()]
-        #print(num_vect)
+#         num_vect = [ [n,cant] for n,cant in hasnums.items()]
+#         print(num_vect)
         
-        num_vect.sort(reverse = True)
-        #print(num_vect)
+#         num_vect.sort(reverse = True)
+#         print(num_vect)
         
-        num_vect = sorted(num_vect, key=lambda x: x[1])
-        #print(num_vect)
+#         num_vect = sorted(num_vect, key=lambda x: x[1])
+#         print(num_vect)
         
-        ans = []
-        for n,cant in num_vect:
-            ans += [n]*cant
+#         ans = []
+#         for n,cant in num_vect:
+#             ans += [n]*cant
             
-        return ans
+#         return ans
         
         
         

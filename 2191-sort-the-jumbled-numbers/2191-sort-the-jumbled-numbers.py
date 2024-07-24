7fd1080451dd0423f4f72acb3174effecb3638f7
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-07-23 18:20:32 -0600

    Time: 1039 ms (65.76%), Space: 23.1 MB (75.00%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.py
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.py
@@ -2,9 +2,13 @@ class Solution:
     def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
         
         
+#         def mapingFunc(num,mapping):
+#             num = list(str(num))
+#             ans = [ str(mapping[int(n)]) for n in num ]
+#             return int("".join(ans))
+
         def mapingFunc(num,mapping):
-            num = list(str(num))
-            ans = [ str(mapping[int(n)]) for n in num ]
+            ans = [ str(mapping[int(n)]) for n in str(num) ]
             return int("".join(ans))
         
         return sorted(nums,key = lambda x: mapingFunc(x,mapping))

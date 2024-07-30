eed2ad691904c65bdbee04364512658ae639554b
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-07-30 17:20:09 -0600

    Time: 596 ms (23.93%), Space: 21.8 MB (18.03%) - LeetHub

---
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        
+        
+        #brute force:
+        #two pointers l, r:
+        # pointer l move from left to rigth, counts "b" to the left
+        # pointer r move from right to lefth, count "a" on the right
+        # create sum_ab with sum of count, return min
+        
+        sum_ab = [0]*len(s)
+        count_a = 0
+        count_b = 0
+        for l in range(len(s)):
+            r = len(s)-l-1
+            
+            if s[l] == 'b': count_b +=1
+            if s[r] == 'a': count_a +=1
+            
+            sum_ab[l] += count_b
+            sum_ab[r] += count_a
+            
+            #print(l,r,s[l],s[r],count_b,count_a)
+        
+        #print(sum_ab)
+            
+        return min(sum_ab)-1
+        
+        
+        

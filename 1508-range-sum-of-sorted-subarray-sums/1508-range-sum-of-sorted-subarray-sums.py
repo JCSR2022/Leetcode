04906f9f536787dd91ac7a152e207dbf24ee81d3
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-08-03 20:26:29 -0600

    Time: 205 ms (87.61%), Space: 39.9 MB (51.33%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.py
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.py
@@ -3,13 +3,23 @@ class Solution:
         
         mode = 10**9+7
         
-        #REEEEBrute force
+#         #REEEEBrute force
+#         arr = []
+#         for i in range(len(nums)):
+#             for j in range(i+1,len(nums)+1):
+#                 arr.append(sum(nums[i:j])%mode)
+#         arr.sort()
+        
+#         return sum(arr[left-1:right])%mode
+
+    
         arr = []
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)+1):
-                arr.append(sum(nums[i:j])%mode)
+        for i in range(n):
+            current_sum = 0
+            for j in range(i,n):
+                current_sum += nums[j]
+                arr.append(current_sum)
         arr.sort()
         
         return sum(arr[left-1:right])%mode
-        
          

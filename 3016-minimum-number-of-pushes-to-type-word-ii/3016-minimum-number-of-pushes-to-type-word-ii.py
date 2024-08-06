42fe6fd1464f76d5a6a35e39b27a12ac62ad99ac
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-08-05 18:50:04 -0600

    Time: 148 ms (71.84%), Space: 17.5 MB (73.03%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -5,22 +5,30 @@ class Solution:
         # sort the cont
         # mod the cosrt arr , mult by mode the repited letters count//
         
-        #cont_lett = Counter(word)
-        
-        letters = list('abcdefghijklmnopqrstuvwxyz')
-        cont_lett = [0]*len(letters)
-        
-        for l in word:
-            cont_lett[letters.index(l)] +=1
-        #print(letters)
-        #print(cont_lett)
-        
-        cont_lett.sort(reverse = True)
-        #print(cont_lett)
+        cont_lett = Counter(word)
         
         ans = 0
-        for i,cont in enumerate(cont_lett):
-            ans +=  cont*(i//8 +1)
-            #print(i,cont,i//8 +1 ,ans)
+        for i,cont_letter  in enumerate(sorted(cont_lett.values(),reverse = True)):
+            ans +=  cont_letter*(i//8 +1)  
         return ans
+        
+        
+        
+        
+#         letters = list('abcdefghijklmnopqrstuvwxyz')
+#         cont_lett = [0]*len(letters)
+        
+#         for l in word:
+#             cont_lett[letters.index(l)] +=1
+#         #print(letters)
+#         #print(cont_lett)
+        
+#         cont_lett.sort(reverse = True)
+#         #print(cont_lett)
+        
+#         ans = 0
+#         for i,cont in enumerate(cont_lett):
+#             ans +=  cont*(i//8 +1)
+#             #print(i,cont,i//8 +1 ,ans)
+#         return ans
         

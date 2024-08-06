f0b3648e498c1f593fb2494c99b5052751ebeae6
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-08-05 19:13:17 -0600

    Time: 146 ms (74.94%), Space: 17.8 MB (8.11%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -4,13 +4,31 @@ class Solution:
         # make a cont of letter
         # sort the cont
         # mod the cosrt arr , mult by mode the repited letters count//
+
         
         cont_lett = Counter(word)
-        
+        vect = [(k,v) for k,v in cont_lett.items()]
+        vect.sort(key = lambda x:x[1] , reverse =True)
+
+        digits = 8
         ans = 0
-        for i,cont_letter  in enumerate(sorted(cont_lett.values(),reverse = True)):
-            ans +=  cont_letter*(i//8 +1)  
+        #keypad = [[] for _ in range(digits)]
+
+        for i,(letter,cont) in enumerate(vect):
+            #keypad[i%digits].append(letter)
+            ans +=  cont*(i//digits +1)  
+
+        #print(ans)
+        #print(keypad)
         return ans
+        
+        
+#         cont_lett = Counter(word)
+#         ans = 0
+#         for i,cont_letter  in enumerate(sorted(cont_lett.values(),reverse = True)):
+#             ans +=  cont_letter*(i//8 +1)  
+#         return ans
+        
         
         
         

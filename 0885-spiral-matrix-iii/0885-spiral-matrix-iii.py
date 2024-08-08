a946764a8cbe54a6ae423b300de318e32936f7cb
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-08-07 19:12:10 -0600

    Time: 80 ms (71.09%), Space: 17.8 MB (74.61%) - LeetHub

--- 0885-spiral-matrix-iii/0885-spiral-matrix-iii.py
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.py
@@ -2,46 +2,63 @@ class Solution:
     def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
         
         
-        # aproach: 
-        # create matrix center in rstart and cStart
-        # move in espiral , every 2 move add 1 to the stepst to do
+        
+        directions = [0, 1, 0, -1, 0]
+        res = [[rStart, cStart]]
+        j = n = 0
+        while len(res) < rows * cols:
+            for i in range(n // 2 + 1):
+                rStart += directions[j]
+                cStart += directions[j + 1]
+                if 0 <= rStart < rows and 0 <= cStart < cols:
+                    res.append([rStart, cStart])
+            n += 1
+            j = (j + 1) % 4
+        return res
+        
+        # aproach, brute force?:  
+        # time complex  O(n), max  rStart = rows-1, cStart = cols-1, (2*rows)*(2*cols)
+        # move in espiral , every 2 move add 1 to the steps
+        # check if valid cell 
         
         
-        self.cont_visited = 1
-        self.visited = [[rStart,cStart]]
+#         self.cont_visited = 1
+#         self.visited = [[rStart,cStart]]
         
-        def checkValid(r,c):          
-            if r <= rows-1 and r >= 0 and c <= cols-1 and c >= 0:
-                self.cont_visited +=1
-                self.visited.append([r,c])
+#         def checkValid(r,c):          
+#             if r < rows and r >= 0 and c < cols and c >= 0:
+#                 self.cont_visited +=1
+#                 self.visited.append([r,c])
                 
         
-        size = rows*cols
-        steps = 0
-        r = rStart
-        c = cStart
+#         size = rows*cols
+#         steps = 0
+#         r = rStart
+#         c = cStart
     
-        while self.cont_visited < size:
+#         while self.cont_visited < size:
 
-            steps +=1
-            #move to right
-            for _ in range(steps):
-                c += 1
-                checkValid(r,c)
-            #move down
-            for _ in range(steps):
-                r += 1
-                checkValid(r,c)
+#             steps +=1
+#             #move to right
+#             for _ in range(steps):
+#                 c += 1
+#                 checkValid(r,c)
+#             #move down
+#             for _ in range(steps):
+#                 r += 1
+#                 checkValid(r,c)
                 
-            steps +=1
-            for _ in range(steps):
-                c -= 1
-                checkValid(r,c)
-            for _ in range(steps):
-                r -= 1
-                checkValid(r,c)
+#             steps +=1
+#             #move left
+#             for _ in range(steps):
+#                 c -= 1
+#                 checkValid(r,c)
+#             #move up
+#             for _ in range(steps):
+#                 r -= 1
+#                 checkValid(r,c)
             
-        return self.visited
+#         return self.visited
                 
             
             

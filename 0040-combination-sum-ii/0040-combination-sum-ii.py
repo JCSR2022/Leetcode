2a95225149f60d439a35c4de3c39dc156336e2c6
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-08-13 15:29:01 -0600

    Time: 72 ms (26.83%), Space: 16.6 MB (30.45%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,37 +1,149 @@
-from itertools import combinations
+#from itertools import combinations
+#from collections import Counter
 
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
        
-        #https://www.youtube.com/watch?v=FOyRpNUSFeA
-        
-        res = [] 
+        #aproach, make a dfs find al posible combinations
+        ans = []
         
         candidates.sort()
-        
-        def dfs(i, cur, total):
+
+        def dfs(i,cur,total):
             
             if total == target:
-                res.append(cur.copy())
+                ans.append(cur.copy())
                 return
-            
-            if total > target or i == len(candidates):
+
+            if i == len(candidates) or total > target:
                 return
-            
-            #include candidates[i]
+
+            #include element
             cur.append(candidates[i])
-            dfs(i+1,cur, total + candidates[i])            
+            dfs(i+1,cur,total+candidates[i])
             cur.pop()
-            
-            #skip candidates[i]
-            while i +1 < len(candidates) and candidates[i] == candidates[i+1]:
-                i += 1
-            dfs(i+1,cur, total)     
-            
-            
+
+            #not include element:
+            while i+1 < len(candidates) and candidates[i] == candidates[i+1]:
+                i +=1
+            dfs(i+1,cur,total)
+
+
         dfs(0,[],0)
+
+        return ans
+    
+    
+    
+    
+    
+    
+#         #aproach, make a dfs find al posible combinations
+#         ans = []
+# #        record = {}
+#         def dfs(i,cur,total):
+# #             if i in record:
+# #                 record[i].append([cur.copy(),total])
+# #             else:
+# #                 record[i] = [cur.copy(),total]
             
-        return res
+            
+#             if total == target:
+#                 #print(cur,total,ans)
+#                 for prev in ans:
+#                     if Counter(prev) == Counter(cur):
+#                         return
+#                 ans.append(cur.copy())
+#                 return
+
+#             if i == len(candidates) or total > target:
+#                 return
+
+#             #include element
+#             cur.append(candidates[i])
+#             dfs(i+1,cur,total+candidates[i])
+#             cur.pop()
+
+#             #not include element:
+#             dfs(i+1,cur,total)
+
+
+#         dfs(0,[],0)
+        
+#         # for k,v in record.items():
+#         #     if k == 10:
+#         #         for elem in v:
+#         #             print(elem)
+        
+        
+#         return ans
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#         #https://www.youtube.com/watch?v=FOyRpNUSFeA
+        
+#         res = [] 
+        
+#         candidates.sort()
+#         print(candidates)
+#         tree = {}
+#         def dfs(i, cur, total):
+#             if i in tree:
+#                 tree[i].append(cur.copy())
+#             else:
+#                 tree[i] = [cur.copy()]
+            
+#             #print(i, cur,total)
+            
+#             if total == target:
+#                 res.append(cur.copy())
+#                 return
+            
+#             if total > target or i == len(candidates):
+#                 return
+            
+#             #include candidates[i]
+#             cur.append(candidates[i])
+#             dfs(i+1,cur, total + candidates[i])            
+#             cur.pop()
+            
+#             #skip candidates[i]
+#             while i +1 < len(candidates) and candidates[i] == candidates[i+1]:
+#                 i += 1
+#             dfs(i+1,cur, total)     
+            
+            
+#         dfs(0,[],0)
+        
+#         for k,v in tree.items():
+#             print(k,v)
+        
+#         return res
     
     
     

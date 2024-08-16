f52b90b7b6031a200c55a4c1dd6938e73e9c6def
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-08-16 17:53:30 -0600

    Time: 1359 ms (39.42%), Space: 42.5 MB (51.30%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.py
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.py
@@ -1,42 +1,48 @@
 class Solution:
     def maxDistance(self, arrays: List[List[int]]) -> int:
         
+         
+        cur_min = arrays[0][0]
+        cur_max = arrays[0][-1]
         
+        ans = float("-inf")
         
-        min_val = arrays[0][0]
-        max_val = arrays[0][-1]
-        max_distance = 0
-
-        for i in range(1, len(arrays)):
-            # Calculate the distance using the current min and max with the current array
-            max_distance = max(max_distance, abs(arrays[i][-1] - min_val), abs(max_val - arrays[i][0]))
+        for arr in arrays[1:]:
+            ans = max(ans, max(arr[-1]-cur_min,cur_max-arr[0]))            
+            cur_min = min(cur_min,arr[0])
+            cur_max = max(cur_max,arr[-1])
             
-            # Update the global min and max values
-            min_val = min(min_val, arrays[i][0])
-            max_val = max(max_val, arrays[i][-1])
-        
-        return max_distance
-    
-
-        #aproach:
-        # create 4 var, 
-       
-        min_val_1 = arrays[1][0]
-        max_val_1 = arrays[0][-1]
-        
-        min_val_2 = arrays[0][0]
-        max_val_2 = arrays[1][-1]
-        
-        #print((min_val_1,max_val_1),(min_val_2,max_val_2),max_val_2-min_val_2 )        
-        for i in range(2,len(arrays)):
-            #print((min_val_1,max_val_1),(min_val_2,max_val_2),max_val_2-min_val_2 )
+        return ans     
             
-            if arrays[i][0] < min_val_2:
-                min_val_1 = min_val_2 
-                min_val_2 = arrays[i][0]
+            
+#         #!@@%^#$^#Y%$&$%^&%*^&*%^*(%^(%&#%!@#R!@$!@!))
+#         min_val_1 = [arrays[1][0],1]
+#         max_val_1 = [arrays[0][-1],0]
+        
+#         min_val_2 = [arrays[0][0],0]
+#         max_val_2 = [arrays[1][-1],1]
+        
+#         print(max_val_2, min_val_2,max_val_1 ,min_val_1)
+#         if len(arrays) == 2:
+#             return max(abs(max_val_2[0]-min_val_1[0]), abs(max_val_1[0]-min_val_2[0])) 
+        
+#         for i in range(2,len(arrays)):
+            
+#             if arrays[i][0] < min_val_2[0]:
+#                 min_val_1 = min_val_2 
+#                 min_val_2 = [arrays[i][0],i]
                 
-            if arrays[i][-1] > max_val_2:
-                max_val_1 = max_val_2
-                max_val_2 = arrays[i][-1]
+#             if arrays[i][-1] > max_val_2[0]:
+#                 max_val_1 = max_val_2
+#                 max_val_2 = [arrays[i][-1],i]
    
-        return max_val_2-min_val_2
+
+
+#         if min_val_2[1] != max_val_2[1]:
+#             return abs(max_val_2[0]-min_val_2[0])
+        
+#         else:
+#             return max(abs(max_val_2[0]-min_val_1[0]), abs(max_val_1[0]-min_val_2[0]))
+
+        
+      

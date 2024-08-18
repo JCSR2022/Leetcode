cf3b3d1adbc8d7700e5a6427f602d0ff86930ae3
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-08-17 22:30:06 -0600

    Time: 128 ms (25.83%), Space: 16.7 MB (18.38%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -3,22 +3,55 @@ import heapq
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
         
-        #using minheap and add all num that can be created untill reach n
+#         #using minheap and add all num that can be created untill reach n
+#         ans = [1]
+#         nums = [2,3,5]
+#         heapq.heapify(nums)
         
+#         while len(ans) < n: 
+#             #print("nums:",nums)
+#             x = heapq.heappop(nums)
+#             ans.append(x)
+#             for i in [2,3,5]:
+#                 if i*x not in nums:
+#                     heapq.heappush(nums,i*x)
 
-        ans = [1]
-        nums = [2,3,5]
-        heapq.heapify(nums)
+#         return ans[-1]
+
+
+        #improve:
+    
+    
+        nums = [1]
+        visit = set()
         
-        while len(ans) < n: 
-            #print("nums:",nums)
+        for i in range(n):
             x = heapq.heappop(nums)
-            ans.append(x)
+            if i == n - 1:
+                return x
+            
             for i in [2,3,5]:
-                if i*x not in nums:
-                    heapq.heappush(nums,i*x)
-
-        return ans[-1]
+                if x*i not in visit:
+                    visit.add(x*i)
+                    heapq.heappush(nums, x*i)
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+    
 
         
         

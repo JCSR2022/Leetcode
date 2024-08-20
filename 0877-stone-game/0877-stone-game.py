6f34aa84ba23e57ec68660e99eaecf7f521b2461
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-08-19 19:17:43 -0600

    Time: 436 ms (16.85%), Space: 128.2 MB (6.10%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -2,7 +2,47 @@ from collections import deque
 
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+        
+        #JAJAJAJAJA
+        #because even number of piles
+        # alice can always choose all even num or all odds
+        # and always one of the summ, all odd or all even is going to be grater 
+        
+        # return True
+        
+        
+        
+        # sol using cashing on dp an two painters O(n**2)
+        
+        dp = {}
+        
+        # return max of alice choices.
+        def dfs(l,r):
+            if l>r:
+                return 0
+            
+            #cashing so no work is repited:
+            if (l,r) in dp:
+                return dp[(l,r)]
+            
+            #confirm is alice choice:
+            alice = True if (r-l)%2 == 0 else False
+            
+            left = piles[l] if alice else 0
+            right = piles[r] if alice else 0
+            
+            dp[(l,r)] =   max(dfs(l+1,r)+left,dfs(l,r-1)+right)          
+            
+            return dp[(l,r)]
+            
+        max_alice = dfs(0,len(piles)-1)
+        
+        return max_alice > sum(piles)//2
+        
+        
+        
+        
+        
         
         
         

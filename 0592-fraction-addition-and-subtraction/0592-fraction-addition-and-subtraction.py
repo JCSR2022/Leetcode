95cbb180b8806c45b5c49d77b752c6d9b88e6818
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-08-22 19:04:45 -0600

    Time: 27 ms (95.35%), Space: 16.5 MB (59.69%) - LeetHub

---
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.py
@@ -0,0 +1,46 @@
+class Solution:
+    def fractionAddition(self, expression: str) -> str:
+        
+        #divide:
+        #  1.create a function to read all num, save it as [[num1,den1],[num2,den2]..]
+        #  2 crete a function that add all num and return [ans_num,ans_den]
+        #  3. creat func to present result
+        
+        import re
+        def readExp(exp:str):
+            # Expresión regular para capturar numeradores y denominadores
+            split_both = re.findall(r'([+-]?\d+)(?:/(\d+))?', exp)
+            
+            # Convertir a la forma deseada (numerador, denominador)
+            result = []
+            for num, denom in split_both:
+                result.append((int(num),int(denom) if denom else 1))
+            
+            return result
+        
+        
+        
+        from math import gcd
+        from functools import reduce
+        def suma_fracciones(fracciones):
+            # Función para sumar dos fracciones (num1/den1) + (num2/den2)
+            def suma_dos_fracciones(frac1, frac2):
+                num1, den1 = frac1
+                num2, den2 = frac2
+                num = num1 * den2 + num2 * den1
+                den = den1 * den2
+                return (num, den)
+
+            # Suma todas las fracciones en la lista
+            suma = reduce(suma_dos_fracciones, fracciones)
+
+            # Simplificar la fracción resultante
+            num, den = suma
+            divisor_comun = gcd(num, den)
+            return (num // divisor_comun, den // divisor_comun)
+        
+        
+        fracciones = readExp(expression)
+        ans = suma_fracciones(fracciones)
+            
+        return str(ans[0])+"/"+str(ans[1]) 

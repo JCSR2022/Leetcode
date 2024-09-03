e75b05c5d31df6ac5ed5a7eebc5c1c902d83e2f6
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-09-02 18:38:39 -0600

    Time: 46 ms (8.45%), Space: 16.5 MB (37.00%) - LeetHub

---
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.py
@@ -0,0 +1,21 @@
+class Solution:
+    def getLucky(self, s: str, k: int) -> int:
+        
+        import string
+
+        # Obtener las letras del alfabeto inglés en minúsculas
+        letters = string.ascii_lowercase
+        
+        # string num
+        num = int("".join([str(letters.index(x)+1) for x in s]))
+            
+            
+        while k > 0:
+            k -= 1
+            num = sum([int(n) for n in str(num)])
+        
+        return num
+    
+    
+    
+    

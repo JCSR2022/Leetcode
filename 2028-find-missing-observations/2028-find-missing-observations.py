16a259e67a3363201776ce7ee29481dfb784b114
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-09-04 18:58:35 -0600

    Time: 1044 ms (24.33%), Space: 26.3 MB (80.48%) - LeetHub

---
+++ 2028-find-missing-observations/2028-find-missing-observations.py
@@ -0,0 +1,24 @@
+class Solution:
+    def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+        #Aproach, :
+        # mean = suma / k  , where k = len(rolls)+n , suma = mean*(len(rolls)+n) 
+        # but suma = suma_rolls + suma_n  => suma_n = mean*(len(rolls)+ n ) - suma_rolls
+        # if suma_n < n or suma_n > n*6 return []
+        # create arr full 1, add each cell unitill get to 6 and untill reach suma_n
+        
+        
+        suma_n = mean*(len(rolls)+n) - sum(rolls)
+        
+        if suma_n < n or suma_n > n*6:
+            return []
+        
+        n_arr = [1]*n
+        suma_n -= n
+        i = 0
+        while suma_n != 0:
+            n_arr[i] += 1
+            suma_n -= 1
+            if n_arr[i] == 6:
+                i += 1
+                
+        return n_arr 

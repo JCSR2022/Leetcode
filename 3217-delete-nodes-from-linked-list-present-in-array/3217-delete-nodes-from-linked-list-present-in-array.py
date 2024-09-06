b9485e3a68fbae929b44ff6e3081376022b47cd8
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-09-05 18:51:20 -0600

    Time: 797 ms (15.58%), Space: 68.4 MB (6.28%) - LeetHub

---
+++ 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        #create dummyhead use as head
+        #dfs remove nodes if is list
+        
+        nums = set(nums)
+    
+        def dfs(prev_node,node):
+            if not node:
+                return
+            
+            if node.val in nums:
+                prev_node.next = node.next
+                dfs(prev_node,node.next)
+            else:
+                prev_node = node
+                dfs(prev_node,node.next)
+    
+    
+        dummy = ListNode(None,head)
+        dfs(dummy ,head)
+        
+        return dummy.next

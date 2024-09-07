2351853227996497c4c737edb6e6002c4a197677
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-09-07 16:43:43 -0600

    Time: 88 ms (20.20%), Space: 17.8 MB (18.78%) - LeetHub

--- 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.py
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.py
@@ -12,50 +12,74 @@
 class Solution:
     def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
         
-        # me dfs moving with head also, save list of nodes visited and check if new node match
         
-        
-        def dual_dfs(head_node,tree_node,new_review=True):
+        def helper(list_node, tree_node):
             
-            if  new_review: 
-                if head_node.val == tree_node.val:
-                    new_review = False
-                    next_head_node = head_node.next
-                else:
-                    next_head_node = head_node
-                
-            else:
-                if head_node.val == tree_node.val:
-                    next_head_node = head_node.next
-                else:
-                    if tree_node.val == head.val:
-                        new_review = False
-                        next_head_node = head.next
-                    else:
-                        new_review = True
-                        next_head_node = head
- 
-            if next_head_node == None:
+            if not list_node:
                 return True
             
-            
-            if tree_node.left:
-                if dual_dfs(next_head_node,tree_node.left,new_review):
-                    return True
-                
-            if tree_node.right:
-                if dual_dfs(next_head_node,tree_node.right,new_review):
-                    return True
-            
-        if dual_dfs(head,root):
+            if not tree_node or list_node.val != tree_node.val:
+                return False
+
+            return ( helper(list_node.next, tree_node.left) or
+                    helper(list_node.next, tree_node.right) )
+
+                    
+        if helper(head,root):
             return True
-        else:
-            if root.left:
-                if self.isSubPath(head,root.left): return True
-            if root.right:
-                if self.isSubPath(head,root.right): return True
+
+        if not root: 
+            return False
+
+        return ( self.isSubPath(head, root.left) or
+                self.isSubPath(head, root.right) )
+
+
         
-        return False
+        
+        # me dfs moving with head also, save list of nodes visited and check if new node match
+               
+#         def dual_dfs(head_node,tree_node,new_review=True):
+            
+#             if  new_review: 
+#                 if head_node.val == tree_node.val:
+#                     new_review = False
+#                     next_head_node = head_node.next
+#                 else:
+#                     next_head_node = head_node
+                
+#             else:
+#                 if head_node.val == tree_node.val:
+#                     next_head_node = head_node.next
+#                 else:
+#                     if tree_node.val == head.val:
+#                         new_review = False
+#                         next_head_node = head.next
+#                     else:
+#                         new_review = True
+#                         next_head_node = head
+ 
+#             if next_head_node == None:
+#                 return True
+            
+            
+#             if tree_node.left:
+#                 if dual_dfs(next_head_node,tree_node.left,new_review):
+#                     return True
+                
+#             if tree_node.right:
+#                 if dual_dfs(next_head_node,tree_node.right,new_review):
+#                     return True
+            
+#         if dual_dfs(head,root):
+#             return True
+#         else:
+#             if root.left:
+#                 if self.isSubPath(head,root.left): return True
+#             if root.right:
+#                 if self.isSubPath(head,root.right): return True
+        
+#         return False
                 
                       
             

9a25304ca751508735fc054e2728361177997b0c
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-09-10 19:23:16 -0600

    Time: 35 ms (76.58%), Space: 16.7 MB (99.51%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.py
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.py
@@ -5,34 +5,91 @@
 #         self.next = next
 class Solution:
     def splitListToParts(self, head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
-        ans = [None] * k
 
-        # Calculate total size of the linked list
-        size = 0
-        current = head
-        while current:
-            size += 1
-            current = current.next
+    
+        node = head
+        cont = 0
+        while node:
+            cont += 1
+            node = node.next    
+        pre_ans = [cont//k]*k 
+        for i in range(cont%k):
+            pre_ans[i] += 1
+            
+            
+            
+        print(pre_ans)
 
-        # Minimum size of each part
-        split_size = size // k
-        remainder = size % k  # Remaining nodes to distribute
-
-        current = head
-        for i in range(k):
-            ans[i] = current
-            current_size = split_size + (1 if remainder > 0 else 0)
-            remainder -= 1
-
-            # Traverse to the end of the current part
-            for _ in range(current_size - 1):
-                if current:
-                    current = current.next
-
-            # Cut the list
-            if current:
-                next_part = current.next
-                current.next = None
-                current = next_part
-
+        
+        def create_chunk(chunk,size):
+            
+            while size > 1:
+                size -= 1
+                if chunk.next:
+                    chunk = chunk.next
+                
+            next_chunk = chunk.next
+            chunk.next = None
+            return next_chunk
+        
+        node = head
+        ans =[None]*k
+        for i,size in enumerate(pre_ans):
+            if size != [] and node:
+                ans[i] = node
+                node = create_chunk(node,size)
         return ans
+        
+        
+        
+        
+            
+        # ans = []
+        # inic = 0 
+        # for num in pre_ans:
+        #     ans.append(list_head[inic:inic+num])
+        #     inic += num
+
+        return head
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         ans = [None] * k
+
+#         # Calculate total size of the linked list
+#         size = 0
+#         current = head
+#         while current:
+#             size += 1
+#             current = current.next
+
+#         # Minimum size of each part
+#         split_size = size // k
+#         remainder = size % k  # Remaining nodes to distribute
+
+#         current = head
+#         for i in range(k):
+#             ans[i] = current
+#             current_size = split_size + (1 if remainder > 0 else 0)
+#             remainder -= 1
+
+#             # Traverse to the end of the current part
+#             for _ in range(current_size - 1):
+#                 if current:
+#                     current = current.next
+
+#             # Cut the list
+#             if current:
+#                 next_part = current.next
+#                 current.next = None
+#                 current = next_part
+
+#         return ans

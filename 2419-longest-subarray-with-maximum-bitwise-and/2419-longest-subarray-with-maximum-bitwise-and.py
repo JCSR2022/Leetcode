0468b5fccf12de2f49465ab956bdb5e03cd44842
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-09-13 20:01:15 -0600

    Time: 659 ms (50.55%), Space: 31.4 MB (21.82%) - LeetHub

---
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.py
@@ -0,0 +1,65 @@
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+
+#brute force O(n**2):
+#         result = 0
+#         max_and = 0
+#         for i in range(len(nums)):
+#             for j in range(i,len(nums)):
+#                 actual = nums[i:j+1]
+#                 actual_and = functools.reduce(lambda x, y: x & y, actual )
+#                 if actual_and ==  max_and:
+#                     result = max(len(actual),result)
+#                 elif actual_and >  max_and:
+#                     max_and = actual_and
+#                     result = len(actual)
+#                 #print(actual,actual_and,max_and,result)   
+#         return result
+
+
+
+
+        #liniar aproach:
+        #find max bit rep num and index
+        #count elements continuos iqual to max
+        
+        
+#         max_bits = 0
+#         pre_ans = (0,0) # number with max bits, index
+#         for i,elem in enumerate(nums):
+#             print(i,elem,bin(elem).count('1'),max_bits,pre_ans )
+#             num_bits = bin(elem).count('1')
+#             if num_bits > max_bits:
+#                 max_bits = num_bits
+#                 pre_ans = (elem,i)
+        
+#         print(i,elem,bin(elem).count('1'),max_bits,pre_ans )
+#         print(max_bits,pre_ans)
+        
+#         ans = 1
+#         i = pre_ans[1]+1
+#         num = pre_ans[0]
+#         while i < len(nums) and nums[i] == num:
+#                 i += 1
+#                 ans +=1
+                
+#         return ans
+                
+        
+
+        ans = 0
+        cont = 0
+        max_num = max(nums)
+        
+        for num in nums:
+            if num == max_num:
+                cont += 1
+                ans = max(ans,cont)
+            else:
+                cont = 0
+        
+        return ans
+                
+        
+        
+            

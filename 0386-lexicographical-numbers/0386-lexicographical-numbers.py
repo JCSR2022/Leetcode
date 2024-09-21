e9dd42f6dac74204994098a5b4f194a75633984e
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-09-20 18:26:32 -0600

    Time: 85 ms (35.17%), Space: 24.9 MB (20.22%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.py
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -1,11 +1,27 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
         
+             
+#         ans = [ str(x) for x in range(1,n+1)]
+#         ans.sort()
         
-        #n_str = list(str(n))
-        #print(n_str)
+#         return [int(x) for x in ans]
+    
+    #---------------------------------------------------
+    
+    
+        #1 always comes first, and we fill in all the ones that exist,
+        #if it gets greater than n, then we finish
+        ans = []
         
-        ans = [ str(x) for x in range(1,n+1)]
-        ans.sort()
-        
-        return [ int(x) for x in ans]
+        def recursive(value):
+            if len(ans) >= n:
+                return
+            ans.append(value)
+            if (value * 10) <= n:
+                recursive(value * 10)
+            if value + 1 <= n and int(str(value)[-1]) < 9:
+                recursive(value + 1)
+        recursive(1)
+
+        return ans

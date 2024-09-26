5d492e7bb908491127cc832d6cd684bf50ecf4a9
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-09-25 20:32:02 -0600

    Time: 313 ms (37.53%), Space: 17.2 MB (89.44%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -1,24 +1,33 @@
 class MyCalendar:
 
-    def __init__(self):
-        self.events = [(-2,-1),(float("inf"),float("inf"))]
+#     def __init__(self):
+#         self.events = [(-2,-1),(float("inf"),float("inf"))]
         
 
-    def book(self, start: int, end: int) -> bool:
+#     def book(self, start: int, end: int) -> bool:
         
-        index = bisect.bisect_left(self.events , (start, end))
+#         index = bisect.bisect_left(self.events , (start, end))
         
+#         if end <= self.events[index][0] and start >= self.events[index-1][1]:
+#             self.events.insert(index, (start, end))
+#             return True
         
-        if end <= self.events[index][0] and start >= self.events[index-1][1]:
-            self.events.insert(index, (start, end))
-            return True
-        
-        else:
-            return False
+#         else:
+#             return False
     
         
         
 
+    def __init__(self):
+        self.calendar = []
+
+    def book(self, start, end):
+        for s, e in self.calendar:
+            if s < end and start < e:
+                return False
+        self.calendar.append((start, end))
+        return True
+    
 
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()

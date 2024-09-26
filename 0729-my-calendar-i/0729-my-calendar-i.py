57fdff40d240c36f310437ffdc9915b20ffe2454
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-09-25 20:21:32 -0600

    Time: 144 ms (98.83%), Space: 17.4 MB (34.88%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -1,45 +1,24 @@
 class MyCalendar:
 
     def __init__(self):
-        self.events = []
-
-    def start_bin_search(self, start):
-        if start <= self.events[0][0]:
-            return True, -1
-        l = 0
-        r = len(self.events)-1
-
-        while l <= r:
-            mid = (l+r)//2
-            # print(l,r)
-
-            if (mid+1 < len(self.events) and self.events[mid+1][0] == start):
-                return False, -1
-            if self.events[mid][1] <= start and (mid+1 >= len(self.events) or self.events[mid+1][0] > start):
-                return True, mid
-            elif self.events[mid][1] > start:
-                r = mid-1
-            else:
-                l = mid+1
+        self.events = [(-2,-1),(float("inf"),float("inf"))]
         
-        return False, -1
 
     def book(self, start: int, end: int) -> bool:
-        if not self.events:
-            self.events.append([start, end])
-            # print(self.events, True, start, end)
-            return True
-        start_possible, index = self.start_bin_search(start)
-
-        if start_possible:
-            if index+1 >= len(self.events) or self.events[index+1][0] >= end:
-                self.events.insert(index+1, [start,end])
-                # print(self.events, True, start, end)
-                return True
-        # print(self.events, False, start, end)
-        return False
-
         
+        index = bisect.bisect_left(self.events , (start, end))
+        
+        
+        if end <= self.events[index][0] and start >= self.events[index-1][1]:
+            self.events.insert(index, (start, end))
+            return True
+        
+        else:
+            return False
+    
+        
+        
+
 
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()

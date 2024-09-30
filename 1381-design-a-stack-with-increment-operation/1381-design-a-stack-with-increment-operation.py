774c3d29c6aea2d0d0d680cc843c1540e684a267
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-09-30 14:34:02 -0600

    Time: 108 ms (14.03%), Space: 17.7 MB (18.91%) - LeetHub

---
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.py
@@ -0,0 +1,53 @@
+class MyNode:
+    def __init__(self,val= None):
+        self.val = val
+        self.next = None
+
+        
+class CustomStack:
+    def __init__(self, maxSize: int):
+        self.maxSize = maxSize
+        self.root = MyNode()
+        self.nod_cont = 0
+        
+    def push(self, x: int) -> None:
+        if self.nod_cont == self.maxSize:
+            return None
+        self.nod_cont +=1
+        
+        root = self.root
+        node = MyNode(x)
+        node.next = root.next
+        root.next = node
+        
+        
+
+    def pop(self) -> int:
+        root = self.root
+        if root.next == None:
+            return -1
+        self.nod_cont -= 1
+        
+        val = root.next.val
+        root.next = root.next.next
+        return val
+        
+
+    def increment(self, k: int, val: int) -> None:
+        node = self.root.next
+        skip_nodes = max(0,self.nod_cont-k)
+        while node :
+            if skip_nodes != 0:
+                skip_nodes -= 1 
+            else:
+                node.val += val
+            node = node.next
+            
+        
+
+
+# Your CustomStack object will be instantiated and called as such:
+# obj = CustomStack(maxSize)
+# obj.push(x)
+# param_2 = obj.pop()
+# obj.increment(k,val)

0002de1eb720947f6368470476e31fa145b79281
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-09-30 14:38:52 -0600

    Time: 75 ms (81.85%), Space: 17.6 MB (59.50%) - LeetHub

--- 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.py
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.py
@@ -1,46 +1,67 @@
-class MyNode:
-    def __init__(self,val= None):
-        self.val = val
-        self.next = None
-
-        
 class CustomStack:
-    def __init__(self, maxSize: int):
-        self.maxSize = maxSize
-        self.root = MyNode()
-        self.nod_cont = 0
-        
+    def __init__(self, n: int):
+        self.n, self.stack, self.inc = n, [], []
+
     def push(self, x: int) -> None:
-        if self.nod_cont == self.maxSize:
-            return None
-        self.nod_cont +=1
-        
-        node = MyNode(x)
-        node.next = self.root.next
-        self.root.next = node
-        
+        if len(self.stack) < self.n: self.stack.append(x), self.inc.append(0)
 
     def pop(self) -> int:
-  
-        if self.root.next == None:
-            return -1
-        self.nod_cont -= 1
+        if not self.stack: return -1
+        if len(self.inc) > 1: self.inc[-2] += self.inc[-1]
+        return self.stack.pop() + self.inc.pop()
+
+    def increment(self, k: int, val: int) -> None:
+        if self.inc: self.inc[min(k, len(self.inc)) - 1] += val
+
+
+
+
+
+
+
+
+# class MyNode:
+#     def __init__(self,val= None):
+#         self.val = val
+#         self.next = None
+
         
-        val = self.root.next.val
-        self.root.next = self.root.next.next
-        return val
+# class CustomStack:
+#     def __init__(self, maxSize: int):
+#         self.maxSize = maxSize
+#         self.root = MyNode()
+#         self.nod_cont = 0
+        
+#     def push(self, x: int) -> None:
+#         if self.nod_cont == self.maxSize:
+#             return None
+#         self.nod_cont +=1
+        
+#         node = MyNode(x)
+#         node.next = self.root.next
+#         self.root.next = node
+        
+
+#     def pop(self) -> int:
+#         if self.root.next == None:
+#             return -1
+#         self.nod_cont -= 1
+        
+#         val = self.root.next.val
+#         self.root.next = self.root.next.next
+#         return val
         
         
 
-    def increment(self, k: int, val: int) -> None:
-        node = self.root.next
-        skip_nodes = max(0,self.nod_cont-k)
-        while node :
-            if skip_nodes != 0:
-                skip_nodes -= 1 
-            else:
-                node.val += val
-            node = node.next
+#     def increment(self, k: int, val: int) -> None:
+#         node = self.root.next
+#         skip_nodes = max(0,self.nod_cont-k)
+#         while node :
+#             if skip_nodes != 0:
+#                 skip_nodes -= 1 
+#             else:
+#                 node.val += val
+#             node = node.next
             
         
 

d4b8a5fcda9c03aba03f133a346929829821bfe1
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-10-01 21:17:46 -0600

    Time: 259 ms (23.46%), Space: 33.3 MB (75.10%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -2,7 +2,30 @@ class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
         #brute force
         
-        hash_arr = {num:i+1 for i,num in enumerate(sorted(set(arr)))}
-        return [hash_arr[n] for n in arr]
+        #hash_arr = {num:i+1 for i,num in enumerate(sorted(set(arr)))}
+        #return [hash_arr[n] for n in arr]
         
-        #return [{num:i+1 for i,num in enumerate(sorted(set(arr)))}[n] for n in arr]
+        
+        if not arr: return []
+            
+        new_arr = arr.copy()
+        heapq.heapify(new_arr)
+        
+        cont = 1
+        val = heapq.heappop(new_arr)
+        hash_arr = {val:cont}
+        while new_arr:
+            new_val = heapq.heappop(new_arr)
+            if new_val != val:
+                cont +=1
+                hash_arr[new_val] = cont
+                val = new_val
+            
+        return [hash_arr[n] for n in arr]
+    
+    
+    
+    
+    
+    
+        

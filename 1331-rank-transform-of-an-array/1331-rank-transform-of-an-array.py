c2199edb76997a584b5d4f1c2ad703e704fd4ec2
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-10-01 20:07:10 -0600

    Time: 238 ms (72.23%), Space: 35.8 MB (26.82%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -2,6 +2,7 @@ class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
         #brute force
         
-        hash_arr ={ num:i+1 for i,num in enumerate(sorted(list(set(arr))))}
-        
+        hash_arr = {num:i+1 for i,num in enumerate(sorted(set(arr)))}
         return [hash_arr[n] for n in arr]
+        
+        #return [{num:i+1 for i,num in enumerate(sorted(set(arr)))}[n] for n in arr]

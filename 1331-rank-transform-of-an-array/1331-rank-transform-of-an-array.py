84b55bc97309b5cefe19dc5e4fd98ae7936a4b87
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-10-01 21:29:46 -0600

    Time: 279 ms (12.29%), Space: 33.3 MB (75.10%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -10,14 +10,16 @@ class Solution:
         
         if not arr: return []
 
-        new_arr = list(set(arr))
+        new_arr = arr.copy()
         heapq.heapify(new_arr)
         
         cont = 0
         hash_arr = {}
         while new_arr:
-            cont += 1
-            hash_arr[heapq.heappop(new_arr)] = cont
+            val = heapq.heappop(new_arr)
+            if val not in hash_arr:
+                cont += 1
+                hash_arr[val] = cont
               
         return [hash_arr[n] for n in arr]
     

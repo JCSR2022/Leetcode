1f8df617783e15ff5abdc681e071ad84cbda9de5
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-10-03 21:17:27 -0600

    Time: 428 ms (19.47%), Space: 29.5 MB (31.15%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
@@ -1,22 +1,52 @@
 class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
 
+        total = sum(skill)
+        target = (2 * total)//len(skill)
         
+        if  (2 * total) % len(skill):
+            return -1
+        
+        count = Counter(skill)
+        res = 0
+        
+        for s in skill:
+            if not count[s]:
+                continue
+            
+            diff = target - s
+            if not count[diff]:
+                return -1
+            
+            res += s * diff
+            count[s] -=1
+            count[diff] -=1
+        
+        
+        
+        return res
+            
+        
+        
+        
+        
+        
+        
+#-----------------------------------------------------------
 #       Brute force nlog(n) - sort
 
-        target = sum(skill)//(len(skill)//2)
-        skill.sort()
-        ans = 0
-        for i in range(len(skill)//2):
-            #print(i,skill[i],skill[len(skill)-i-1])
-            if skill[i] + skill[len(skill)-i-1] != target:
-                return -1
-            ans +=  skill[i]*skill[len(skill)-i-1]
-        return ans
+#         target = sum(skill)//(len(skill)//2)
+#         skill.sort()
+#         ans = 0
+#         for i in range(len(skill)//2):
+#             if skill[i] + skill[len(skill)-i-1] != target:
+#                 return -1
+#             ans +=  skill[i]*skill[len(skill)-i-1]
+#         return ans
         
 
 
-
+#-----------------------------------------------------------
         
         
 #         #realy brute force aproach:

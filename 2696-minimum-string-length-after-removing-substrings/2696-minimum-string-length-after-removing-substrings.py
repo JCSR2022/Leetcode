b8ce55eeaf773291128abf5dcbd9fb022eb93eeb
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-10-06 19:31:28 -0600

    Time: 52 ms (16.07%), Space: 16.6 MB (41.77%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.py
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.py
@@ -1,13 +1,43 @@
 class Solution:
     def minLength(self, s: str) -> int:
+        # O(n) using a queue
+        
+        if len(s) == 1:
+            return 1
+        
+        ans = ""
+        for ch in s:
+            ans += ch
+            if ans[-2:] == 'AB':
+                ans = ans[:-2]
+            if ans[-2:] == 'CD':
+                ans = ans[:-2]
+                
+        return len(ans)
+            
+            
+            
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         #brute force:
+#         while 'AB' in s or 'CD' in s:
+#             while 'AB' in s:
+#                 s = s.replace("AB", "")
+#             while 'CD' in s:
+#                 s = s.replace("CD", "")
         
-        while 'AB' in s or 'CD' in s:
-            while 'AB' in s:
-                s = s.replace("AB", "")
-            while 'CD' in s:
-                s = s.replace("CD", "")
-        
-        return len(s)
+#         return len(s)
         

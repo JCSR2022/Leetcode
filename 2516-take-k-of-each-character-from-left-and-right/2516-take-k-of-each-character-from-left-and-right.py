597f5fd3409ffc1ef75f72968565d86a4957d8a7
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-11-20 13:25:07 -0600

    Time: 260 ms (40.55%), Space: 17 MB (95.90%) - LeetHub

--- 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.py
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.py
@@ -4,58 +4,55 @@ class Solution:
         #aproach: make a hash_map, find the max window that leave at least k of each letter out:
         
         
-        if k == 0:
-            return 0
+#         if k == 0:
+#             return 0
         
-        current_count = Counter(s)
-        if len(current_count.keys()) < 3: return -1
-        for v in current_count.values(): 
-            if v < k: return -1
+#         current_count = Counter(s)
+#         if len(current_count.keys()) < 3: return -1
+#         for v in current_count.values(): 
+#             if v < k: return -1
             
             
-        current_window_size = 0
-        max_size = 0
-        L = 0 
-        R = 0
-        while R < len(s):
-            current_count[s[R]] -=1
-            current_window_size += 1
-            while current_count[s[R]] < k:
-                current_count[s[L]] += 1
-                current_window_size -= 1
-                L += 1
+#         current_window_size = 0
+#         max_size = 0
+#         L = 0 
+#         R = 0
+#         while R < len(s):
+#             current_count[s[R]] -=1
+#             current_window_size += 1
+#             while current_count[s[R]] < k:
+#                 current_count[s[L]] += 1
+#                 current_window_size -= 1
+#                 L += 1
         
-            max_size =max(max_size,current_window_size)
-            R += 1 
+#             max_size =max(max_size,current_window_size)
+#             R += 1 
             
-        return len(s)-max_size
+#         return len(s)-max_size
     
     
     
     #----------------------------------------------------------------
         
+
         
-#         if k == 0:
-#             return 0
+        # Total counts
+        count = [0, 0, 0]
+        for c in s:
+            count[ord(c) - ord('a')] += 1
+
+        if min(count) < k:
+            return -1
+
+        # Sliding Window
+        res = float("inf")
+        l = 0
+        for r in range(len(s)):
+            count[ord(s[r]) - ord('a')] -= 1
+
+            while min(count) < k:
+                count[ord(s[l]) - ord('a')] += 1
+                l += 1
+            res = min(res, len(s) - (r - l + 1))
+        return res
         
-#         s_count = Counter(s)
-#         if len(s_count.keys()) < 3: return -1
-#         for v in s_count.values(): 
-#             if v < k: return -1
-            
-            
-#         current_count = s_count.copy()
-#         print("#",current_count)
-#         current_window_size = 0
-#         max_size = 0
-#         for i,letter in enumerate(s):
-#             current_count[letter] -=1
-#             print(i,current_count,max_size,current_window_size)
-#             if current_count[letter] < k:
-#                 current_count = s_count.copy()
-#                 current_window_size = 0
-#             else:
-#                 current_window_size += 1
-#                 max_size =max(max_size,current_window_size)
-                
-#         return len(s)-max_size

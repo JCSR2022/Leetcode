5fa01ca24f7815da530b38a74fdaa502c097c1c3
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-11-20 21:45:43 -0600

    Time: 176 ms (99.00%), Space: 38.2 MB (77.78%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.py
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.py
@@ -1,61 +1,59 @@
 class Solution:
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
         #brute force build matrix, for each guard modify matrix count values 0 
+
         
-        #matrix = [[ 0 if [i, j] not in guards and [i, j] not in walls else -1 for j in range(n)]
-        #            for i in range(m) ]
+#         matrix = [ [0]*n for _ in range(m)]
+#         for i,j in guards:
+#             matrix[i][j] = -1
+#         for i,j in walls:
+#             matrix[i][j] = -1
         
-        matrix = [ [0]*n for _ in range(m)]
-        for i,j in guards:
-            matrix[i][j] = -1
-        for i,j in walls:
-            matrix[i][j] = -1
+#         def fill_view(i,j):
+#             #north
+#             for k in range(i-1,-1,-1):
+#                 if matrix[k][j] >= 0:
+#                     matrix[k][j] = 1
+#                 else:
+#                     break
         
-        def fill_view(i,j):
-            #north
-            for k in range(i-1,-1,-1):
-                if matrix[k][j] >= 0:
-                    matrix[k][j] = 1
-                else:
-                    break
-        
-            #south
-            for k in range(i+1,m):
-                if matrix[k][j] >= 0:
-                    matrix[k][j] = 1
-                else:
-                    break
+#             #south
+#             for k in range(i+1,m):
+#                 if matrix[k][j] >= 0:
+#                     matrix[k][j] = 1
+#                 else:
+#                     break
                     
-            #east
-            for k in range(j-1,-1,-1):
-                if matrix[i][k] >= 0:
-                    matrix[i][k] = 1
-                else:
-                    break
+#             #east
+#             for k in range(j-1,-1,-1):
+#                 if matrix[i][k] >= 0:
+#                     matrix[i][k] = 1
+#                 else:
+#                     break
             
-            #west
-            for k in range(j+1,n):
-                if matrix[i][k] >= 0:
-                    matrix[i][k] = 1
-                else:
-                    break
+#             #west
+#             for k in range(j+1,n):
+#                 if matrix[i][k] >= 0:
+#                     matrix[i][k] = 1
+#                 else:
+#                     break
         
-        # def print_matrix():
-        #     for row in matrix:
-        #         print(row)
-        #     print()
+#         # def print_matrix():
+#         #     for row in matrix:
+#         #         print(row)
+#         #     print()
         
-        for i,j in guards:
-            fill_view(i,j)
-            # print(i,j)
-            # print_matrix()
+#         for i,j in guards:
+#             fill_view(i,j)
+#             # print(i,j)
+#             # print_matrix()
         
         
         
-        ans = sum([row.count(0) for row in matrix])
+#         ans = sum([row.count(0) for row in matrix])
 
 
-        return ans
+#         return ans
         
         
   #---------------------------------------------------------
@@ -64,50 +62,58 @@ class Solution:
 #         matrix = [[ 0 if [i, j] not in guards and [i, j] not in walls else -1 for j in range(n)]
 #                     for i in range(m) ]
         
-#         def fill_view(i,j):
-#             #north
-#             k = i-1
-#             while k >= 0:
-#                 if matrix[k][j] >= 0:
-#                     matrix[k][j] = 1
-#                     k -= 1
-#                 else:
-#                     break
+    
+        matrix = [ [0]*n for _ in range(m)]
+        for i,j in guards:
+            matrix[i][j] = -1
+        for i,j in walls:
+            matrix[i][j] = -1
             
-#             #south
-#             k = i+1
-#             while k < m:
-#                 if matrix[k][j] >= 0:
-#                     matrix[k][j] = 1
-#                     k += 1
-#                 else:
-#                     break
+            
+        def fill_view(i,j):
+            #north
+            k = i-1
+            while k >= 0:
+                if matrix[k][j] >= 0:
+                    matrix[k][j] = 1
+                    k -= 1
+                else:
+                    break
+            
+            #south
+            k = i+1
+            while k < m:
+                if matrix[k][j] >= 0:
+                    matrix[k][j] = 1
+                    k += 1
+                else:
+                    break
                     
-#             #east
-#             k = j-1
-#             while k >= 0:
-#                 if matrix[i][k] >= 0:
-#                     matrix[i][k] = 1
-#                     k -= 1
-#                 else:
-#                     break
+            #east
+            k = j-1
+            while k >= 0:
+                if matrix[i][k] >= 0:
+                    matrix[i][k] = 1
+                    k -= 1
+                else:
+                    break
             
-#             #west
-#             k = j+1
-#             while k < n:
-#                 if matrix[i][k] >= 0:
-#                     matrix[i][k] = 1
-#                     k += 1
-#                 else:
-#                     break
+            #west
+            k = j+1
+            while k < n:
+                if matrix[i][k] >= 0:
+                    matrix[i][k] = 1
+                    k += 1
+                else:
+                    break
                         
-#         for i,j in guards:
-#             fill_view(i,j)
+        for i,j in guards:
+            fill_view(i,j)
             
-#         ans = sum([row.count(0) for row in matrix])
+        ans = sum([row.count(0) for row in matrix])
 
 
-#         return ans
+        return ans
         
         
         

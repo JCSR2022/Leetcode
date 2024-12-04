e2f57bfdec6be0f50fe9c9a6104a4a13c1e2eada
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-12-03 21:12:59 -0600

    Time: 43 ms (97.92%), Space: 17.5 MB (56.40%) - LeetHub

---
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.py
@@ -0,0 +1,51 @@
+class Solution:
+    def canMakeSubsequence(self, str1: str, str2: str) -> bool:
+        
+        #two pointers hashtable
+        
+        hash_table = {
+    'a': ['a', 'b'],
+    'b': ['b', 'c'],
+    'c': ['c', 'd'],
+    'd': ['d', 'e'],
+    'e': ['e', 'f'],
+    'f': ['f', 'g'],
+    'g': ['g', 'h'],
+    'h': ['h', 'i'],
+    'i': ['i', 'j'],
+    'j': ['j', 'k'],
+    'k': ['k', 'l'],
+    'l': ['l', 'm'],
+    'm': ['m', 'n'],
+    'n': ['n', 'o'],
+    'o': ['o', 'p'],
+    'p': ['p', 'q'],
+    'q': ['q', 'r'],
+    'r': ['r', 's'],
+    's': ['s', 't'],
+    't': ['t', 'u'],
+    'u': ['u', 'v'],
+    'v': ['v', 'w'],
+    'w': ['w', 'x'],
+    'x': ['x', 'y'],
+    'y': ['y', 'z'],
+    'z': ['z', 'a']
+}
+
+        
+        
+        p2 = 0
+        for p1 in str1:
+            if str2[p2] in hash_table[p1]:
+                p2 += 1
+                if p2 == len(str2):
+                    return True
+        
+        return False
+        
+        
+        
+        
+        
+        
+        

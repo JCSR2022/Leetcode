7eb23f19c9c2c99b1b643e94a9f01f405b495a45
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-12-03 22:07:04 -0600

    Time: 60 ms (90.28%), Space: 17.8 MB (13.74%) - LeetHub

--- 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.py
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.py
@@ -43,17 +43,32 @@ class Solution:
 
 #--------------------------------------------------
 
-        hash_table = ['b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a']
+#         hash_table = ['b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a']
        
         
+#         p2 = 0
+#         for p1 in str1:
+#             if str2[p2] == p1 or str2[p2] ==  hash_table[(ord(p1)-97)]:
+#                 p2 += 1
+#                 if p2 == len(str2):
+#                     return True
+        
+#         return False
+        
+#--------------------------------------------------
+
+       
         p2 = 0
         for p1 in str1:
-            if str2[p2] == p1 or str2[p2] ==  hash_table[(ord(p1)-97)]:
+            #print(p2,str2[p2],ord(str2[p2]), p1,ord(p1)+1,chr(ord(p1)+1) )
+            #change_char = ord(p1) + 1 if ord(p1) + 1 <= 122 else 97
+            if str2[p2] == p1 or ord(str2[p2]) == ord(p1) + 1 if ord(p1) + 1 <= 122 else 97:
                 p2 += 1
                 if p2 == len(str2):
                     return True
         
         return False
-        
+
+
         
         

fd2b171578e36dc73cb6b7092109204f5ccaf15a
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-12-14 21:48:56 -0600

    Time: 1415 ms (12.00%), Space: 54.3 MB (37.04%) - LeetHub

---
+++ 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.py
@@ -0,0 +1,62 @@
+class Solution:
+    def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
+        # aproach:
+        # 1. calculate actual %
+        # 2 use a heap, add extra estudent always to min heap
+        
+        
+        # heap order by pass_ratio first and later for total studendts
+        # so have better impact adding a student
+        
+#         heap = [ (act_pass/total,act_pass,total) for act_pass, total in classes   ]
+        
+#         heapq.heapify(heap)
+        
+#         while extraStudents > 0:
+#             print(heap)
+#             _,act_pass,total =  heapq.heappop(heap)
+            
+#             heapq.heappush(heap,((act_pass+1)/(total+1), act_pass+1, total+1))
+            
+#             extraStudents -= 1
+            
+#         print(heap)
+            
+#         return sum( [pass_ratio for pass_ratio,_,_ in heap ]  )/len(heap)
+
+
+#buena idea mal aplicada
+#-----------------------------------------------------------------------------
+
+
+        def find_ratio_increment(index):
+            actual_ratio = classes[index][0]/classes[index][1]
+            new_ratio = (classes[index][0]+1)/(classes[index][1]+1)
+            #print(actual_ratio,new_ratio)
+            return new_ratio-actual_ratio
+        
+        heap = [(-find_ratio_increment(i),i) for i in range(len(classes)) ]
+        heapq.heapify(heap)
+    
+        while extraStudents > 0:
+            
+            
+            _,index =  heapq.heappop(heap)
+            
+            classes[index][0] +=1
+            classes[index][1] +=1
+            
+            heapq.heappush(heap,(-find_ratio_increment(index),index))
+            
+            extraStudents -= 1
+            
+        
+        return sum([act_pass/total for act_pass,total in classes])/len(classes)
+        
+        
+        
+        
+
+
+
+

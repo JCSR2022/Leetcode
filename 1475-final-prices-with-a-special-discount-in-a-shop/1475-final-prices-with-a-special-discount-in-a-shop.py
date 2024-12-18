f93a35f74ba6a4d8c0c102cc30a53e609c914430
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-12-18 11:36:38 -0600

    Time: 4 ms (21.42%), Space: 17.8 MB (10.22%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -2,17 +2,47 @@ class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
         #brute force:
         
-        ans = []
-        for i,price_i in enumerate(prices):
-            j = i+1
-            while j < len(prices):
-                if prices[j] <= price_i:
-                    ans.append(price_i-prices[j])
-                    break
-                else:
-                    j +=1
-            if j == len(prices):
-                ans.append(price_i)
+#         ans = []
+#         for i,price_i in enumerate(prices):
+#             j = i+1
+#             while j < len(prices):
+#                 if prices[j] <= price_i:
+#                     ans.append(price_i-prices[j])
+#                     break
+#                 else:
+#                     j +=1
+#             if j == len(prices):
+#                 ans.append(price_i)
                 
-        return ans
+#         return ans
+
+#---------------------------------------------------
+
+
+#         price = [8,7,4,2,8,1,7,7,10,1]
+#         arr = [ (val,i) for i,val in enumerate(prices) ]
+#         #arr.sort(key=lambda x: (x[1], x[0]))
+#         #ordena por valor de forma decreciente
+#         arr.sort(key=lambda x: x[0])
+#         #ordena por indice de forma creciente
+#         arr.sort(key=lambda x: x[1], reverse=True) 
+#         print(arr)
+        
+#         return prices
+
+
+
+        stack = [prices[-1]]
+
+        for i in range(len(prices) - 2, -1, -1):
+            while stack and prices[i] < stack[-1]:
+                stack.pop()
+
+            tmp = prices[i]
+            if stack:
+                prices[i] -= stack[-1]
+
+            stack.append(tmp)
+
+        return prices
             

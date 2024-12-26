4c071103971e50b72d3434153a1f43034e4c2226
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-12-25 21:19:41 -0600

    Time: 122 ms (75.79%), Space: 18.1 MB (61.85%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -33,18 +33,41 @@ class Solution:
 
 # backtrack with memorization 
 
-        memory = {}
+#         memory = {}
 
     
-        def backtrack(i,cur_sum):
+#         def backtrack(i,cur_sum):
             
-            if (i,cur_sum) in memory:
-                return memory[(i,cur_sum)]
+#             if (i,cur_sum) in memory:
+#                 return memory[(i,cur_sum)]
     
-            if i == len(nums):
-                return 1 if cur_sum == target else 0
+#             if i == len(nums):
+#                 return 1 if cur_sum == target else 0
             
-            memory[(i,cur_sum)] = backtrack(i+1,cur_sum + nums[i] ) + backtrack(i+1,cur_sum - nums[i] )
-            return memory[(i,cur_sum)] 
+#             memory[(i,cur_sum)] = backtrack(i+1,cur_sum + nums[i] ) + backtrack(i+1,cur_sum - nums[i] )
+#             return memory[(i,cur_sum)] 
 
-        return backtrack(0,0)
+#         return backtrack(0,0)
+
+#--------------------------------------------------
+
+#https://www.youtube.com/watch?v=dwMOrl85Xes
+#dp solution
+
+        dp = [defaultdict(int) for _ in range(len(nums)+1)]
+        #dp[ elements of nums, sum of elements]
+        
+        dp[0][0]  =1 #  1 way to sum to zero with 0 elements
+        
+        for i in range(len(nums)):
+            for cur_sum, count in dp[i].items():
+                dp[i+1][cur_sum + nums[i]] += count
+                dp[i+1][cur_sum - nums[i]] += count
+        
+        # for i,level in enumerate(dp):
+        #     print(i,{k: level[k] for k in sorted(level)}    )
+        
+        
+        #dp[len(nums)][target] number of ways sum target with all nums
+        
+        return dp[len(nums)][target]

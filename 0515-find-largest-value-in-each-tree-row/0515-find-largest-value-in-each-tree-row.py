f818ca19bfb62f0037f34104be146f11494ed047
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2024-12-25 19:37:14 -0600

    Time: 0 ms (100.00%), Space: 19.9 MB (7.75%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.py
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.py
@@ -8,30 +8,52 @@ class Solution:
     def largestValues(self, root: Optional[TreeNode]) -> List[int]:
         
         
-        max_level_val = {}
-        def dfs(node,level):
-            if level in max_level_val:
-                max_level_val[level] = max(max_level_val[level],node.val)
-            else:
-                max_level_val[level] = node.val
+#         max_level_val = {}
+#         def dfs(node,level):
+#             if level in max_level_val:
+#                 max_level_val[level] = max(max_level_val[level],node.val)
+#             else:
+#                 max_level_val[level] = node.val
 
-            if node.left: dfs(node.left,level+1)
-            if node.right: dfs(node.right,level+1)
+#             if node.left: dfs(node.left,level+1)
+#             if node.right: dfs(node.right,level+1)
         
         
-        if root:
-            dfs(root,0)
-            return list( max_level_val.values())
+#         if root:
+#             dfs(root,0)
+#             return list( max_level_val.values())
         
-        return []
+#         return []
     
-        #print(max_level_val)
-        #return [ max_level_val[k]  k for sorted(max_level_val.keys())]
+#         #print(max_level_val)
+#         #return [ max_level_val[k]  k for sorted(max_level_val.keys())]
         
         
         
         
-            
+        res = []
+
+        if not root:
+            return res
+        
+        q = deque()
+        q.append(root)
+
+        while q:
+            max_val = float("-inf")
+            for _ in range(len(q)):
+                node = q.popleft()
+                max_val = max(max_val, node.val)
+
+                if node.left:
+                    q.append(node.left)
+
+                if node.right:
+                    q.append(node.right)                    
+
+            res.append(max_val)
+
+        return res   
                 
                 
                 

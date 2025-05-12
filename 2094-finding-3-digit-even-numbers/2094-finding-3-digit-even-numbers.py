5f4a337eb77f136d77ce08c4e79c6c301f8d4313
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-05-11 19:41:51 -0600

    Time: 1763 ms (41.62%), Space: 17.9 MB (65.57%) - LeetHub

---
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.py
@@ -0,0 +1,57 @@
+class Solution:
+    def findEvenNumbers(self, digits: List[int]) -> List[int]:
+
+        #aproach n**3
+
+        # N = len(digits)
+
+        # ans = []
+        # for i in range(N):
+        #     for j in range(i+1,N):
+        #         for k in range(j+1,N):
+        #             if digits[i] != 0 and digits[k]%2 == 0:
+        #                 ans.append( digits[i]*100+digits[j]*10+digits[k])
+        
+        # return ans
+        
+        #nooooooooooooooooooooooooo
+#--------------------------------------------------------------------
+
+
+        # ans = []
+        # for num in set(itertools.permutations(digits, 3)):
+        #     if num[0] !=0  and num[2]%2 == 0:
+        #         ans.append(num[0]*100+num[1]*10+num[0]  )
+        # ans.sort()
+        # return ans
+
+        #noooooooooooooooooooooooooooooooooooooo
+#-----------------------------------------------------------
+
+
+
+        N = len(digits)
+
+        ans = set()
+        for i in range(N):
+            for j in range(i+1,N):
+                for k in range(j+1,N):
+                    if digits[i] != 0: 
+                        if digits[j]%2 == 0:
+                            ans.add( digits[i]*100+digits[k]*10+digits[j])
+                        if digits[k]%2 == 0:
+                            ans.add(digits[i]*100+digits[j]*10+digits[k])
+                    if digits[j] != 0:
+                        if digits[k]%2 == 0:
+                            ans.add(digits[j]*100+digits[i]*10+digits[k])
+                        if digits[i]%2 == 0:
+                            ans.add(digits[j]*100+digits[k]*10+digits[i])
+                    if digits[k] != 0:
+                        if digits[i]%2 == 0:
+                            ans.add(digits[k]*100+digits[j]*10+digits[i])
+                        if digits[j]%2 == 0:
+                            ans.add(digits[k]*100+digits[i]*10+digits[j])
+        ans =list(ans)
+        ans.sort()
+                
+        return ans

c2d0922184bddc6cbd6506143c13311ac8b46111
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-05-11 20:05:08 -0600

    Time: 2054 ms (35.18%), Space: 17.7 MB (89.91%) - LeetHub

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.py
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.py
@@ -28,30 +28,43 @@ class Solution:
         #noooooooooooooooooooooooooooooooooooooo
 #-----------------------------------------------------------
 
+        #aproach n**3
 
+        # N = len(digits)
+
+        # ans = set()
+        # for i in range(N):
+        #     for j in range(i+1,N):
+        #         for k in range(j+1,N):
+        #             if digits[i] != 0: 
+        #                 if digits[j]%2 == 0:
+        #                     ans.add( digits[i]*100+digits[k]*10+digits[j])
+        #                 if digits[k]%2 == 0:
+        #                     ans.add(digits[i]*100+digits[j]*10+digits[k])
+        #             if digits[j] != 0:
+        #                 if digits[k]%2 == 0:
+        #                     ans.add(digits[j]*100+digits[i]*10+digits[k])
+        #                 if digits[i]%2 == 0:
+        #                     ans.add(digits[j]*100+digits[k]*10+digits[i])
+        #             if digits[k] != 0:
+        #                 if digits[i]%2 == 0:
+        #                     ans.add(digits[k]*100+digits[j]*10+digits[i])
+        #                 if digits[j]%2 == 0:
+        #                     ans.add(digits[k]*100+digits[i]*10+digits[j])
+        # ans =list(ans)
+        # ans.sort()
+                
+        # return ans
+# 1763 ms,  Beats 41.62%
+#-------------------------------------------------------
 
         N = len(digits)
-
         ans = set()
         for i in range(N):
-            for j in range(i+1,N):
-                for k in range(j+1,N):
-                    if digits[i] != 0: 
-                        if digits[j]%2 == 0:
-                            ans.add( digits[i]*100+digits[k]*10+digits[j])
-                        if digits[k]%2 == 0:
-                            ans.add(digits[i]*100+digits[j]*10+digits[k])
-                    if digits[j] != 0:
-                        if digits[k]%2 == 0:
-                            ans.add(digits[j]*100+digits[i]*10+digits[k])
-                        if digits[i]%2 == 0:
-                            ans.add(digits[j]*100+digits[k]*10+digits[i])
-                    if digits[k] != 0:
-                        if digits[i]%2 == 0:
-                            ans.add(digits[k]*100+digits[j]*10+digits[i])
-                        if digits[j]%2 == 0:
-                            ans.add(digits[k]*100+digits[i]*10+digits[j])
-        ans =list(ans)
-        ans.sort()
-                
-        return ans
+            if digits[i] != 0:
+                for j in range(N):
+                    if j != i:
+                        for k in range(N):
+                            if k != i and k != j and digits[k]%2 == 0:
+                                ans.add( digits[i]*100+digits[j]*10+digits[k] )
+        return sorted(list(ans))

95bb1c6954bbe690c3357fb198d1d2234f32c1fa
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-05-20 22:03:38 -0600

    Time: 3 ms (73.14%), Space: 18.1 MB (99.59%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,21 +3,97 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rowsCeros = []
-        columnsCeros = []
+
+        # #sol O(n)
+        # m = len(matrix)
+        # n = len(matrix[0])
+
+        # rows = set()
+        # col = set()        
+        # for i in range(m):
+        #     for j in range(n):
+        #         if matrix[i][j] == 0:
+        #             if i not in rows:
+        #                 rows.add(i)
+        #                 for y in range(n):
+        #                     matrix[i][y] = 0
+        #             if j not in col:    
+        #                 col.add(j)
+        #                 for x in range(m):
+        #                     matrix[x][j] = 0
+#nooooo
+#----------------------------------------------------
         
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
+
+        #sol O(n)
+        m = len(matrix)
+        n = len(matrix[0])
+
+        rows = set()
+        col = set()        
+        for i in range(m):
+            for j in range(n):
                 if matrix[i][j] == 0:
-                    rowsCeros.append(i)
-                    columnsCeros.append(j)
-                    
-        for i in rowsCeros:
-            for j in range(len(matrix[0])):
-                matrix[i][j] = 0
+                    rows.add(i)    
+                    col.add(j)
+
+        for i in range(m):
+            for j in range(n):
+                if i in rows or j in col:
+                    matrix[i][j] = 0
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # rowsCeros = []
+        # columnsCeros = []
         
-        for i in range(len(matrix)):
-            for j in columnsCeros:
-                matrix[i][j] = 0
+        # for i in range(len(matrix)):
+        #     for j in range(len(matrix[0])):
+        #         if matrix[i][j] == 0:
+        #             rowsCeros.append(i)
+        #             columnsCeros.append(j)
+                    
+        # for i in rowsCeros:
+        #     for j in range(len(matrix[0])):
+        #         matrix[i][j] = 0
+        
+        # for i in range(len(matrix)):
+        #     for j in columnsCeros:
+        #         matrix[i][j] = 0
                 
         

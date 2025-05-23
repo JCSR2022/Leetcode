3a1bb9347da92050bbe84100ea80bf7d1bac199c
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-05-22 20:55:43 -0600

    Time: 37 ms (67.24%), Space: 26.3 MB (32.76%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.py
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.py
@@ -1,5 +1,40 @@
 class Solution:
     def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
+
+        # #brute force, try all cahnges in deges n*edg**2 
+        # N_edg = len(edges)
+
+        # def recursion(i,curr_nums,ans):
+    
+        #     if i == N_edg:
+        #         #print(ans,curr_nums,sum(curr_nums))
+        #         return sum(curr_nums)
+                
+        #     #use
+        #     u = edges[i][0]
+        #     v = edges[i][1]
+        #     previus_u = curr_nums[u]
+        #     previus_v = curr_nums[v]
+        #     curr_nums[u] =  curr_nums[u]^k
+        #     curr_nums[v] =  curr_nums[v]^k
+        #     ans.append(edges[i])
+        #     op1 = recursion(i+1,curr_nums,ans)
+        #     ans.pop()
+
+        #     #not use
+        #     curr_nums[u] =  previus_u
+        #     curr_nums[v] =  previus_v
+        #     op2 = recursion(i+1,curr_nums,ans)
+            
+        #     return max(op1,op2)
+
+        # return recursion(0,nums,[])
+
+#Time Limit Exceeded
+#----------------------------------------------------------------
+
+ 
+        # #https://www.youtube.com/watch?v=bnBp6_b4GCw
         
         delta = [ (n ^ k) - n for n in nums ]
         delta.sort(reverse = True)

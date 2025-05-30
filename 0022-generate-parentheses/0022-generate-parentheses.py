930357b5ac8a6b563ea478f998e639ccce8056e3
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-05-29 18:39:40 -0600

    Time: 0 ms (100%), Space: 18 MB (69.22%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,6 +1,56 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        
+            
+
+            
+            ans = []
+            
+            def recursion(Parentheses = "",cnt_open =0,cnt_close = 0):
+
+                if cnt_open == n and cnt_close == n:
+                    ans.append(Parentheses)
+
+                # use another open
+                if cnt_open < n:
+                    recursion(Parentheses+"(",cnt_open+1,cnt_close)
+
+                # use close
+                if cnt_open > cnt_close:
+                    recursion(Parentheses+")",cnt_open,cnt_close+1)
+
+            recursion()
+            return ans
+                    
+
+
+
+
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         # aproach: dfs
         # count the '(' inserted and the ')' 
         #   push and pop acording to count
@@ -11,22 +61,22 @@ class Solution:
         #   validis  open = close , open and close < n
         # constructtion of tree makes always ..(....)...
         
-        ans = []
-        def dfs(pare,op,cl):
-            #print(pare,op,cl,ans)
+        # ans = []
+        # def dfs(pare,op,cl):
+        #     #print(pare,op,cl,ans)
             
-            if op > n or cl > n :
-                return
+        #     if op > n or cl > n :
+        #         return
             
-            if op == cl and op == n :
-                ans.append(pare)
+        #     if op == cl and op == n :
+        #         ans.append(pare)
             
             
-            if cl<= op: dfs(pare+"(",op+1,cl)
-            dfs(pare+")",op,cl+1)        
+        #     if cl<= op: dfs(pare+"(",op+1,cl)
+        #     dfs(pare+")",op,cl+1)        
                 
-        dfs("",0,0)
+        # dfs("",0,0)
    
-        return ans
+        # return ans
             
             

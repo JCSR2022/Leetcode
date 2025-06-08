649870ab1cb33250b688d1a5b8a07baac2719d18
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-06-07 21:04:17 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.py
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -16,11 +16,11 @@ class Solution:
 
 #--------------------------------------------------
 
-
+        #this is n*log(n) do to sort
              
-        ans = [ str(x) for x in range(1,n+1)]
-        ans.sort()
-        return [int(x) for x in ans]
+        # ans = [ str(x) for x in range(1,n+1)]
+        # ans.sort()
+        # return [int(x) for x in ans]
     
     
     #---------------------------------------------------
@@ -28,18 +28,18 @@ class Solution:
     
         #1 always comes first, and we fill in all the ones that exist,
         #if it gets greater than n, then we finish
-        # ans = []
+        ans = []
         
-        # def recursive(value):
-        #     print(value,ans)
-        #     if len(ans) >= n:
-        #         return
-        #     ans.append(value)
-        #     if (value * 10) <= n:
-        #         recursive(value * 10)
-        #     if value + 1 <= n and int(str(value)[-1]) < 9:
-        #         recursive(value + 1)
+        def recursive(value):
+            print(value,ans)
+            if len(ans) >= n:
+                return
+            ans.append(value)
+            if (value * 10) <= n:
+                recursive(value * 10)
+            if value + 1 <= n and int(str(value)[-1]) < 9:
+                recursive(value + 1)
                 
-        # recursive(1)
+        recursive(1)
 
-        # return ans
+        return ans

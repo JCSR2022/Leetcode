6ae8c65ff0dcad3d3e4365278333ba36ef7e6361
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-06-17 21:03:46 -0600

    Time: 79 ms (71.73%), Space: 33.5 MB (25.74%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.py
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.py
@@ -3,60 +3,29 @@ class Solution:
         
         #brute force:
         #sort  nLog(n)
-        N = len(nums)
-        nums.sort()
-        ans=[[]]
-        for i in range(N):
-            if not ans[-1]:
-                ans[-1].append(nums[i])
-                min_n = nums[i]
-            else:
-                if nums[i]-min_n > k:
-                    return []
-                else:
-                    ans[-1].append(nums[i])
-                    if len(ans[-1]) ==3:
-                        ans.append([])
-        
-        return ans[:-1]
-
-                
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        # N = len(nums)
         # nums.sort()
-        # ans = []
+        # ans=[[]]
+        # for i in range(N):
+        #     if not ans[-1]:
+        #         ans[-1].append(nums[i])
+        #         min_n = nums[i]
+        #     else:
+        #         if nums[i]-min_n > k:
+        #             return []
+        #         else:
+        #             ans[-1].append(nums[i])
+        #             if len(ans[-1]) ==3:
+        #                 ans.append([])
         
-        # for i in range(0,len(nums),3):
-        #     if nums[i+2] - nums[i] > k:
-        #         return []
-        #     ans.append(nums[i:i+3])
-        # return ans
+        # return ans[:-1]
+
+ #--------------------------------------------------------
+
+        nums.sort()
+        ans = []
+        for i in range(0,len(nums),3):
+            if nums[i+2] - nums[i] > k:
+                return []
+            ans.append(nums[i:i+3])
+        return ans

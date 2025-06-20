b8c5d33f4d1cf1ce280ae8e20e060e96f57469b2
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-06-19 21:55:36 -0600

    Time: 2534 ms (73.44%), Space: 18.2 MB (37.5%) - LeetHub

--- 3443-maximum-manhattan-distance-after-k-changes/3443-maximum-manhattan-distance-after-k-changes.py
+++ 3443-maximum-manhattan-distance-after-k-changes/3443-maximum-manhattan-distance-after-k-changes.py
@@ -1,3 +1,6 @@
+fmax = lambda x,y: x if x>y else y
+fmin = lambda x,y: x if x<y else y
+
 class Solution:
     def maxDistance(self, s: str, k: int) -> int:
         #aproach O(n) ?
@@ -6,22 +9,25 @@ class Solution:
 
         #calculate manhattan position for each ch in s and add mim(k,max_contra):
 
-        
         hash_s  = {'N':0,'S':0,'E':0,'W':0}
         max_dist = 0
         for ch in  s:
             hash_s[ch] +=1
-            min_x = min(hash_s['E'],hash_s['W'])
-            kx = min(k,min_x)
-            max_x = max(hash_s['E'],hash_s['W'])+2*kx-min_x
+            min_x = fmin(hash_s['E'],hash_s['W'])
+            kx = fmin(k,min_x)
+            max_x = fmax(hash_s['E'],hash_s['W'])+2*kx-min_x
 
-            min_y = min(hash_s['N'],hash_s['S'])
-            ky = min(k-kx,min_y)
-            max_y = max(hash_s['N'],hash_s['S'])+2*ky-min_y
+            min_y = fmin(hash_s['N'],hash_s['S'])
+            ky = fmin(k-kx,min_y)
+            max_y = fmax(hash_s['N'],hash_s['S'])+2*ky-min_y
 
             curr_max_dist = max_x + max_y
-            max_dist = max(max_dist,curr_max_dist)
+            max_dist = fmax(max_dist,curr_max_dist)
 
             #print(hash_s,(min_x,max_x,kx),(min_y,max_y,ky),curr_max_dist,max_dist )
         return max_dist
+
+#isa pice of SH#$^$^ but it work
+#3991 ms Beats 25.79%
+#------------------------------------------------------------------------
 

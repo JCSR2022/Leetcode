cf754a9a8c1d7992692899d87369ba774b760900
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-06-23 19:53:05 -0600

    Time: 3 ms (83.2%), Space: 17.9 MB (68.6%) - LeetHub

--- 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.py
+++ 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.py
@@ -1,10 +1,11 @@
 class Solution:
     def findKDistantIndices(self, nums: List[int], key: int, k: int) -> List[int]:
 
-        #Aproach: 3*O(n)
-        N = len(nums)
 
-        flags = [0]*N
+
+        #Aproach: 2*O(n)+klog(k)
+        N = len(nums)
+        ans = set()
 
         #Left_right
         cur_k = 0
@@ -13,7 +14,7 @@ class Solution:
                 cur_k = k+1
             if  cur_k > 0:
                 cur_k -=1
-                flags[i] = 1
+                ans.add(i)
            
         #right_left
         cur_k = 0
@@ -22,9 +23,35 @@ class Solution:
                 cur_k = k+1
             if  cur_k > 0:
                 cur_k -=1
-                flags[i] = 1
+                ans.add(i)
         
-        return [ i for i,f in enumerate(flags) if f]
+        return sorted(list(ans))
+
+
+#---------------------------------------------------------------
+        # #Aproach: 3*O(n)
+        # N = len(nums)
+        # flags = [0]*N
+
+        # #Left_right
+        # cur_k = 0
+        # for i in range(N):
+        #     if nums[i] == key:
+        #         cur_k = k+1
+        #     if  cur_k > 0:
+        #         cur_k -=1
+        #         flags[i] = 1
+           
+        # #right_left
+        # cur_k = 0
+        # for i in range(N-1,-1,-1):
+        #     if nums[i] == key:
+        #         cur_k = k+1
+        #     if  cur_k > 0:
+        #         cur_k -=1
+        #         flags[i] = 1
+        
+        # return [ i for i,f in enumerate(flags) if f]
 
 
         

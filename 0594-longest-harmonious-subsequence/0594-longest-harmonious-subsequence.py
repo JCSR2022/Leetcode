4a9288d42731e4fa4e792ad8ac3a97e7ba31be42
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-06-29 20:57:43 -0600

    Time: 23 ms (77.09%), Space: 19 MB (72.21%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
@@ -1,12 +1,24 @@
 class Solution:
     def findLHS(self, nums: List[int]) -> int:
         
-        hash_nums = Counter(nums)
-        values = sorted(hash_nums.keys())
+        #worst case O(nLog(n))
 
-        max_count = 0 
-        for i in range(1,len(values)):
-            if values[i]-values[i-1] == 1:
-                max_count = max(max_count,hash_nums[values[i]]+hash_nums[values[i-1]])
+        # hash_nums = Counter(nums)
+        # values = sorted(hash_nums.keys())
+
+        # max_count = 0 
+        # for i in range(1,len(values)):
+        #     if values[i]-values[i-1] == 1:
+        #         max_count = max(max_count,hash_nums[values[i]]+hash_nums[values[i-1]])
         
+        # return max_count
+#------------------------------------------------------------------
+        
+        #no need to sort
+        
+        hash_nums = Counter(nums)
+        max_count = 0 
+        for n in hash_nums:
+            if n+1 in hash_nums :
+                max_count = max(max_count,hash_nums[n]+hash_nums[n+1])
         return max_count

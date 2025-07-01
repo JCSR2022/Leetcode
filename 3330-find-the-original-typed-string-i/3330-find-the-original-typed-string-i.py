b353c7b80afd8178a9c94ab8a2c5516c10dbf2ea
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-06-30 19:35:36 -0600

    Time: 40 ms (50.14%), Space: 17.5 MB (92.68%) - LeetHub

---
+++ 3330-find-the-original-typed-string-i/3330-find-the-original-typed-string-i.py
@@ -0,0 +1,52 @@
+class Solution:
+    def possibleStringCount(self, word: str) -> int:
+
+        #aproach , O(n), make hash_word
+        # hash_word = Counter(word)
+
+        # ans = 0
+        # for ch,cnt in hash_word.items():
+        #     if cnt > 1:
+        #         ans += cnt-1
+        # return ans + 1 
+
+#noooooooo word = "ere" expected =1
+#----------------------------------------------------------
+        
+        #same idea just will work
+
+        ans = 0
+        prev_ch = ''
+        for ch in word:
+            if ch == prev_ch:
+                ans += 1
+            else:
+                prev_ch = ch
+
+        return ans +1
+
+
+
+
+
+
+
+
+
+
+        #she is aware that she may still have done this at most once.
+        #"abbcccc"
+        #"abcccc"
+        #"abbccc"
+        #"abbccc"
+        #"abbccc"
+
+        #she may still have done this at least once.
+        #"abbcccc"
+        #"abbccc"
+        #"abbcc"
+        #"abbc"
+        #"abcccc"
+        #"abccc"
+        #"abcc"
+        #"abc"

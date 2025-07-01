b6a0e94b9e5baad51ffb66855da6e75dd45d1bee
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-06-30 19:40:34 -0600

    Time: 40 ms (50.14%), Space: 17.6 MB (72.11%) - LeetHub

--- 3330-find-the-original-typed-string-i/3330-find-the-original-typed-string-i.py
+++ 3330-find-the-original-typed-string-i/3330-find-the-original-typed-string-i.py
@@ -15,17 +15,24 @@ class Solution:
         
         #same idea just will work
 
-        ans = 0
-        prev_ch = ''
-        for ch in word:
-            if ch == prev_ch:
+        # ans = 0
+        # prev_ch = ''
+        # for ch in word:
+        #     if ch == prev_ch:
+        #         ans += 1
+        #     else:
+        #         prev_ch = ch
+
+        # return ans +1
+
+#---------------------------------------------------
+        #faster and cleaner
+
+        ans = 1
+        for i in range(1,len(word)):
+            if word[i-1] == word[i]:
                 ans += 1
-            else:
-                prev_ch = ch
-
-        return ans +1
-
-
+        return ans
 
 
 

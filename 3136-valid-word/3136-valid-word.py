01dfd804c84f028f0babb4ae7f10369de830659e
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-07-14 20:05:18 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3136-valid-word/3136-valid-word.py
@@ -0,0 +1,24 @@
+import string
+
+class Solution:
+    def isValid(self, word: str) -> bool:
+
+        #best is use re but i guess is not valid
+
+        if len(word)<3:
+            return False
+
+        if not all([ ch in string.ascii_letters+string.digits for ch in word]):          
+            return False
+
+        consonant = 0
+        vowels = 0
+        for ch in word:
+            vowels += 1 if ch in 'aeiouAEIOU0123456789' else 0
+            consonant += 1 if ch not in 'aeiouAEIOU0123456789' else 0
+        if consonant == 0 or vowels == 0:
+            return False
+        
+        return True 
+        
+        

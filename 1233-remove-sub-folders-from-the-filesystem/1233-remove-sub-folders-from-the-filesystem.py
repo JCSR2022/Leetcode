b2a8b398a6b90ca0b95dea175073f216d8527a16
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-07-18 20:01:31 -0600

    Time: 67 ms (52.05%), Space: 30.5 MB (95.49%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
@@ -1,8 +1,31 @@
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
+
+        # #brute_force:
+
+        # folder.sort()
+        # ans = []
+        # prev_ans = set()
+        # for f in folder:
+        #     split_f = "".join(f.split("/"))
+        #     not_in_ans = True
+        #     for add_f in prev_ans:
+        #         if add_f in split_f:
+        #             not_in_ans = False
+        #     if not_in_ans:
+        #         prev_ans.add(f)
+        #         ans.append(f)    
+
+        # # return ans
+
+#una mierda maldito imbecil!!!!!!
+#---------------------------------------------------------------
+
+
+#------------------------------------------------------------------------
         folder_set = set(folder)
         res = []
-        
+        #print(folder_set)
         for f in folder:
             res.append(f)
             #print(res)

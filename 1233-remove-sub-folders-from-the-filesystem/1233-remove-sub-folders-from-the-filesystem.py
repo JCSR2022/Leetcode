7c53ddf13adbe97deced6cc5ebb56c9799f66599
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-07-18 20:25:08 -0600

    Time: 58 ms (58.11%), Space: 30.7 MB (60.37%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
@@ -1,6 +1,49 @@
+# class trie_folders:
+#     def __init__(self):
+#         curr_folder_level = {}
+#         sub_folder = {}
+
+#     def exist_folder(self,folder):
+#         if folder in self.curr_folder_level:
+#             if self.curr_folder_level[folder]:
+#                 return True
+#         return False
+
+    
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False  # marks if a folder ends here
+
+
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
 
+
+        root = TrieNode()
+        folder.sort()  # important to insert parent folders before children
+        res = []
+
+        for path in folder:
+            parts = path.split("/")[1:]  # skip the empty string before first /
+            node = root
+            skip = False
+
+            for part in parts:
+                if node.end:
+                    skip = True  # already covered by a parent
+                    break
+                if part not in node.children:
+                    node.children[part] = TrieNode()
+                node = node.children[part]
+
+            if not skip:
+                node.end = True
+                res.append(path)
+
+        return res
+
+#------------------------------------------------------------------
         # #brute_force:
 
         # folder.sort()
@@ -20,18 +63,24 @@ class Solution:
 
 #una mierda maldito imbecil!!!!!!
 #---------------------------------------------------------------
-
-
 #------------------------------------------------------------------------
-        folder_set = set(folder)
-        res = []
-        #print(folder_set)
-        for f in folder:
-            res.append(f)
-            #print(res)
-            for i in range(len(f)):
-                if f[i] == "/" and f[:i] in folder_set:
-                    res.pop()
-                    break
+        # folder_set = set(folder)
+        # res = []
+        # #print(folder_set)
+        # for f in folder:
+        #     res.append(f)
+        #     #print(res)
+        #     for i in range(len(f)):
+        #         if f[i] == "/" and f[:i] in folder_set:
+        #             res.pop()
+        #             break
         
-        return res
+        # return res
+#----------------------------------------------------------------
+    #build a trie ????
+
+
+
+
+
+

aac0bdd250d9bf9082ffde7260ecaf5ac13e3d34
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-07-20 19:57:14 -0600

    Time: 194 ms (95.83%), Space: 19.5 MB (66.4%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.py
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.py
@@ -1,21 +1,71 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
+        
+        cnt = 0
+        ans = ""
+        prev = ""
+        for ch in  s:
+            if ch == prev:
+                cnt += 1
+            else:
+                cnt = 0
+            prev = ch
+            if cnt < 2:
+                ans += ch
+            
+        return ans
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  #-------------------------------------------------------------
         #brute force
         
-        ans = ""
-        ch_val = ""
-        ch_count = 0
-        for ch in s:
-            if ch_val == ch:
-                ch_count += 1
-                if ch_count < 2: 
-                    ans +=ch    
-            else:
-                ch_count = 0
-                ch_val = ch
-                ans +=ch
+        # ans = ""
+        # ch_val = ""
+        # ch_count = 0
+        # for ch in s:
+        #     if ch_val == ch:
+        #         ch_count += 1
+        #         if ch_count < 2: 
+        #             ans +=ch    
+        #     else:
+        #         ch_count = 0
+        #         ch_val = ch
+        #         ans +=ch
                 
-        return ans
+        # return ans
     
                 
                     

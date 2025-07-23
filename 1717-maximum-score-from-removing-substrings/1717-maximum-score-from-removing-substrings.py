26bd497971caae9c836d9414da2ccc7a1703f4c4
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-07-22 23:16:55 -0600

    Time: 359 ms (17.42%), Space: 19.2 MB (83.23%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.py
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.py
@@ -1,31 +1,131 @@
 class Solution:
     def maximumGain(self, s: str, x: int, y: int) -> int:
   
-        a_count = 0
-        b_count = 0
-        lesser = min(x, y)
+
+        # use stack,greedy???
+
+        # stack = []
+        # points = 0
+        # for ch in s:   
+        #     stack.append(ch)
+        #     print(stack, points)
+        #     if len(stack) >= 2:
+        #         if stack[-1]+stack[-2] == "ab":
+        #             points += x
+        #             stack.pop()    
+        #             stack.pop() 
+        #         elif stack[-1]+stack[-2] == "ba":
+        #             points += y
+        #             stack.pop()    
+        #             stack.pop() 
+        # return points 
+
+#nooooooooooooooooooooooooooooooooooooooo
+#---------------------------------------------------------------------------
+
+        # def backtrack(i,stack,points):
+        #     print(i,stack,points)
+
+        #     if i == len(s):
+        #         return points
+ 
+        #     op1 = 0
+        #     op2 = 0
+        #     if len(stack) >2:
+        #         #use x if can
+        #         if stack[-1] + stack[-2] == "ab":
+        #             ch1 = stack.pop()
+        #             ch2 = stack.pop()
+        #             op1 = backtrack(i,stack,points + x)
+        #             stack.append(ch2)
+        #             stack.append(ch1)
+        #         #use y if can
+        #         elif stack[-1] + stack[-2] == "ba":     
+        #             ch1 = stack.pop()
+        #             ch2 = stack.pop()
+        #             op2 = backtrack(i,stack,points + y)
+        #             stack.append(ch2)
+        #             stack.append(ch1)
+        #     #no use
+        #     stack.append(s[i])
+        #     op3 = backtrack(i+1,stack,points)
+
+        #     return max(op1,op2,op3)
+
+
+        # return backtrack(0,[],0)   
+
+#Noooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+#---------------------------------------------------------------        
+  
+
+
+  #----------------------------------------------------------------------------
+        #ni entiendo esta @#%#@%#$$# solucion
+        # a_count = 0
+        # b_count = 0
+        # lesser = min(x, y)
+        # result = 0
+
+        # for c in s:
+        #     if c > 'b':
+        #         result += min(a_count, b_count) * lesser
+        #         a_count = 0
+        #         b_count = 0
+        #     elif c == 'a':
+        #         if x < y and b_count > 0:
+        #             b_count -= 1
+        #             result += y
+        #         else:
+        #             a_count += 1
+        #     elif c == 'b':
+        #         if x > y and a_count > 0:
+        #             a_count -= 1
+        #             result += x
+        #         else:
+        #             b_count += 1
+
+        # result += min(a_count, b_count) * lesser
+        # return result
+
+#---------------------------------------------------------------
+        #correct greedy, first make the one with more value
+
+        if x > y:
+            first = x
+            second = y
+            str_first = "ab"
+            str_second = "ba"
+        else:
+            first = y
+            second = x
+            str_first = "ba"
+            str_second = "ab"
+
         result = 0
 
-        for c in s:
-            if c > 'b':
-                result += min(a_count, b_count) * lesser
-                a_count = 0
-                b_count = 0
-            elif c == 'a':
-                if x < y and b_count > 0:
-                    b_count -= 1
-                    result += y
-                else:
-                    a_count += 1
-            elif c == 'b':
-                if x > y and a_count > 0:
-                    a_count -= 1
-                    result += x
-                else:
-                    b_count += 1
+        stack = []
+        for ch in s:
+            stack.append(ch)
+            if len(stack) >= 2 and stack[-2]+stack[-1] == str_first:
+                result += first
+                stack.pop()
+                stack.pop()
+            #print(stack,result)
 
-        result += min(a_count, b_count) * lesser
+        new_s = "".join(stack)
+
+        stack = []
+        for ch in new_s:
+            stack.append(ch)
+            if len(stack) >= 2 and stack[-2]+stack[-1] == str_second:
+                result += second
+                stack.pop()
+                stack.pop()
+            #print(stack,result)
+        
         return result
+        
 
 
 
@@ -35,6 +135,10 @@ class Solution:
 
 
 
+
+
+#----------------------------------------------------------------------------------
+#       Nunca entendi por que no funciona @#%#@%@#$%
 #         #greedy with a pointer and a stack
         
 #         def check_substring(stack,var,substring):

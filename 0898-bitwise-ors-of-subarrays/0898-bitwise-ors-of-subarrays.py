47f477c30280f439c35111195134ace5a28a0791
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-07-30 22:05:53 -0600

    Time: 418 ms (66.07%), Space: 45 MB (20.24%) - LeetHub

--- 0898-bitwise-ors-of-subarrays/0898-bitwise-ors-of-subarrays.py
+++ 0898-bitwise-ors-of-subarrays/0898-bitwise-ors-of-subarrays.py
@@ -1,34 +1,44 @@
 class Solution:
     def subarrayBitwiseORs(self, arr: List[int]) -> int:
-        
-        new_arr = []
-        for num in arr:
-            if num not in new_arr:
-                new_arr.append(num)
-        arr = new_arr
         #brute force n**2
-        n = len(arr)
-        ans = set()
-        for i in range(n):
-            currBitOr = arr[i]
-            for j in range(i,n):
-                currBitOr |= arr[j]
-                ans.add(currBitOr)
-        return len(ans)         
+        # n = len(arr)
+        # ans = set()
+        # for i in range(n):
+        #     currBitOr = arr[i]
+        #     for j in range(i,n):
+        #         currBitOr |= arr[j]
+        #         ans.add(currBitOr)
+        # return len(ans)         
 
 #Time Limit Exceeded
 #--------------------------------------------------------
-     #make bit_mask and update if any bit of new n not in bitmask +1
+     #2d aproach instead of making n**2 will be n*(ans)
 
 
+        # n = len(arr)
+        # prev_ans = set()
+        # for currBitOr in arr:
+        #     curr_ans = set([currBitOr])
+        #     for prev in prev_ans :
+        #         print("#### ",currBitOr,prev, currBitOr | prev )
+        #         curr_ans.add(currBitOr | prev)
+        #     prev_ans = prev_ans | curr_ans
+        #     print(currBitOr,prev_ans)
 
+        # return len(prev_ans)      
 
-
-
-
-
-
-
+#NOOOOOOOOOOOOOOOOOOOOOOOOo
 # ------------------------------------------------        
    
+        result_ors = set()
+        current_ors = set()
+   
+        for x in arr:
+   
+            next_ors = {x | y for y in current_ors}
+            next_ors.add(x) 
+            result_ors.update(next_ors)
+            current_ors = next_ors
+            
+        return len(result_ors)
 

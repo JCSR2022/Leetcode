3feb3b6b90933f1cc28893433e1ab1463219bc34
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-08-03 21:51:30 -0600

    Time: 84 ms (97.31%), Space: 23.6 MB (42.94%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,21 +1,26 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
+
+        # O(n)
+
         par = {-2,-1}
         curr_max = 0
         glob_max = 0
-        prev = {-1:0}
-
+        prev = -1
+        prev_cnt = 0
         for f in fruits:
             if f in par:
                 curr_max += 1
-                if f == list(prev.keys())[0]:
-                    prev[f] +=1 
+                if f == prev:
+                    prev_cnt +=1 
                 else:
-                    prev = {f:1}
+                    prev = f
+                    prev_cnt = 1
             else:
-                par = {list(prev.keys())[0],f}
-                curr_max = list(prev.values())[0]+1
-                prev = {f:1}
+                par = {prev,f}
+                curr_max = prev_cnt +1
+                prev = f
+                prev_cnt = 1
             glob_max = max(glob_max,curr_max)
 
         return glob_max

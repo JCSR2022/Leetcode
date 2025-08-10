866fc9057dfcfd41fc091748b506f83c49cfea67
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-08-09 19:59:23 -0600

    Time: 3 ms (59.16%), Space: 17.7 MB (79.77%) - LeetHub

---
+++ 0869-reordered-power-of-2/0869-reordered-power-of-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+
+        #aproach
+        # find digits in all power of two until 10**9 digits, compare cont digits
+
+        i = 0
+        power2Num = 1
+        dig_count = set()
+        while power2Num < 10**10:
+            curr_count = [0]*10
+            for digit in str(power2Num):
+                curr_count[int(digit)] +=1
+            dig_count.add(tuple(curr_count))
+            i +=1
+            power2Num = 2**i
+
+        curr_count = [0]*10
+        for digit in str(n):
+                curr_count[int(digit)] +=1
+        
+
+        return tuple(curr_count) in dig_count
+
+
+        

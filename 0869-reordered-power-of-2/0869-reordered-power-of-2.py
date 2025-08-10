587c2c2cefd6acc4951096b42ec9aa009c7e532b
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-08-09 20:10:31 -0600

    Time: 0 ms (100%), Space: 17.6 MB (94.27%) - LeetHub

--- 0869-reordered-power-of-2/0869-reordered-power-of-2.py
+++ 0869-reordered-power-of-2/0869-reordered-power-of-2.py
@@ -2,7 +2,35 @@ class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
 
         #aproach
-        # find digits in all power of two until 10**9 digits, compare cont digits
+        # find digits in all power of two until 10**9 digits, 
+        # compare cont digits
+
+        # i = 0
+        # power2Num = 1
+        # dig_count = set()
+        # while power2Num < 10**9:
+        #     curr_count = [0]*10
+        #     for digit in str(power2Num):
+        #         curr_count[int(digit)] +=1
+        #     dig_count.add(tuple(curr_count))
+        #     i +=1
+        #     power2Num = 2**i
+
+        # curr_count = [0]*10
+        # for digit in str(n):
+        #         curr_count[int(digit)] +=1
+        
+
+        # return tuple(curr_count) in dig_count
+
+#-----------------------------------------------
+        
+
+        n_count = [0]*10
+        for digit in str(n):
+                n_count[int(digit)] +=1
+        n_count = tuple(n_count)
+
 
         i = 0
         power2Num = 1
@@ -11,16 +39,16 @@ class Solution:
             curr_count = [0]*10
             for digit in str(power2Num):
                 curr_count[int(digit)] +=1
-            dig_count.add(tuple(curr_count))
+            if tuple(curr_count) == n_count:
+                return True
             i +=1
             power2Num = 2**i
 
-        curr_count = [0]*10
-        for digit in str(n):
-                curr_count[int(digit)] +=1
+        return False
+
         
 
-        return tuple(curr_count) in dig_count
+
 
 
         

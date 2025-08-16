cb4d20cb9754bd2c1ebd133852241f6a736a2761
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-08-15 19:26:44 -0600

    Time: 0 ms (100%), Space: 17.7 MB (76.77%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.py
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -1,19 +1,19 @@
 class Solution:
     def maximum69Number (self, num: int) -> int:
         
-        # str_num = [  digit  for digit in str(num)]
-        # for i,digit in enumerate(str_num):
-        #     if digit == "6":
-        #         str_num[i] = "9"
-        #         break
+        str_num = [  digit  for digit in str(num)]
+        for i,digit in enumerate(str_num):
+            if digit == "6":
+                str_num[i] = "9"
+                break
 
-        # return int("".join(str_num))
+        return int("".join(str_num))
         
         #----------------------------------------------------
 
-        str_num = str(num)
-        indx = str_num.find("6")
-        if indx>=0:
-            return  int(str_num[:indx]+'9'+str_num[indx+1:])
-        else:
-            return num
+        # str_num = str(num)
+        # indx = str_num.find("6")
+        # if indx>=0:
+        #     return  int(str_num[:indx]+'9'+str_num[indx+1:])
+        # else:
+        #     return num

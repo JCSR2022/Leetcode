decfddd21451b8fdca0bbe367bfd6c9dd61a0d61
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-08-28 20:05:57 -0600

    Time: 0 ms (100%), Space: 17.5 MB (98.61%) - LeetHub

---
+++ 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.py
@@ -0,0 +1,9 @@
+class Solution:
+    def flowerGame(self, n: int, m: int) -> int:
+        #esta intersante pero hoy no tengo ganas ni de vivir
+        #gracias daulat_309
+
+         return n * m // 2
+
+
+        

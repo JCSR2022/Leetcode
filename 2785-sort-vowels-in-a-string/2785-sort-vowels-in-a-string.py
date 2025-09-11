98b1cea394af70350f7aed6a1d39b9054e767330
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-09-10 19:34:46 -0600

    Time: 240 ms (11.9%), Space: 31.1 MB (5.41%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
@@ -3,19 +3,34 @@ class Solution:
 
 
         #bruteforce
-        heap = []
-        for letter in s:
-            if letter in "AEIOUaeiou":
-                heapq.heappush(heap,ord(letter))
+        # heap = []
+        # for letter in s:
+        #     if letter in "AEIOUaeiou":
+        #         heapq.heappush(heap,ord(letter))
 
-        new_s = []
-        for letter in s:
-            if letter in "AEIOUaeiou":
-                new_s.append(chr(heapq.heappop(heap)) )
-            else:
-                new_s.append(letter)
+        # new_s = []
+        # for letter in s:
+        #     if letter in "AEIOUaeiou":
+        #         new_s.append(chr(heapq.heappop(heap)) )
+        #     else:
+        #         new_s.append(letter)
 
+        # return "".join(new_s)
+
+# try improve time
+        
+        new_s = list(s)
+        indx = []
+        heap_letter = []
+
+        for i,letter in enumerate(new_s):
+            if letter in "AEIOUaeiou":
+                heapq.heappush(heap_letter,(ord(letter),letter) )
+                indx.append(i)
+                
+        for i in indx:
+            new_s[i] = heapq.heappop(heap_letter)[1]
+        
         return "".join(new_s)
-
 
         

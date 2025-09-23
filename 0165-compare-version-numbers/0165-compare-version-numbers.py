7a50f76fb517af2bac308f8d00f5a0851a7dbcaa
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-09-22 19:16:42 -0600

    Time: 0 ms (100%), Space: 18 MB (17.28%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.py
+++ 0165-compare-version-numbers/0165-compare-version-numbers.py
@@ -1,17 +1,62 @@
+from itertools import zip_longest
+
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        ver1 = [int(i) for i in version1.split('.')]
-        ver2 = [int(i) for i in version2.split('.')]
-        #print(ver1)
-        #print(ver2)
-        #print(list(zip_longest(ver1,ver2, fillvalue=0)))
+
+            ver1 = [ int(n) for n in version1.split(".")]
+            ver2 = [ int(n) for n in version2.split(".")]
+
+            #print(ver1)
+            #print(ver2)
+
+            for v1,v2 in zip_longest(ver1, ver2, fillvalue=0):
+                #print(v1,v2)
+                if v1 < v2:
+                    return -1
+                if v1 > v2:
+                    return 1
+
+            return 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # ver1 = [int(i) for i in version1.split('.')]
+        # ver2 = [int(i) for i in version2.split('.')]
+        # #print(ver1)
+        # #print(ver2)
+        # #print(list(zip_longest(ver1,ver2, fillvalue=0)))
         
         
-        for v1, v2 in zip_longest(ver1,ver2, fillvalue=0):
-            if v1 < v2:
-                return -1
-            elif v1 > v2:
-                return 1
+        # for v1, v2 in zip_longest(ver1,ver2,fillvalue=0):
+        #     if v1 < v2:
+        #         return -1
+        #     elif v1 > v2:
+        #         return 1
         
-        return 0
+        # return 0
         

8da0ddbec6b8bf22f732324ae03e1e20f8d86810
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-10-07 21:36:40 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -0,0 +1,33 @@
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        
+        #sort potions
+        #use bisection to find_potion = ceil( success/ spell_i) 
+
+        n = len(spells)
+        m = len(potions)
+
+        def bisect(x):
+            l, r = 0, m
+            while l < r:
+                mid = (l + r) // 2
+                if potions[mid] < x:
+                    l = mid + 1
+                else:
+                    r = mid
+            return l if l < n else -1
+
+        potions.sort()
+
+        ans = [0]*n
+        for i,spell in enumerate(spells):          
+            curr_cnt = bisect( math.ceil(success/spell) )
+            if curr_cnt > 0:
+                ans[i] = m-curr_cnt
+
+        return ans
+
+        
+
+
+

6746871af422625d74ddfb60bb26ac8157e4cff2
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-10-12 20:17:25 -0600

    Time: 4 ms (67.06%), Space: 18 MB (28.68%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
@@ -1,21 +1,32 @@
 class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
         
+        # ans = []
+        # curr_hash = {}
+        # for w in words:
+
+        #     w_hash = {}
+        #     for ch in w:
+        #         if ch in w_hash:
+        #             w_hash[ch] +=1
+        #         else:
+        #             w_hash[ch] = 1
+
+        #     if curr_hash != w_hash:
+        #         ans.append(w)
+        #         curr_hash = w_hash
+        
+        # return ans
+
+#---------------------------------------------------------       
+#   do to 1 <= words[i].length <= 10 is posible sort is faster
 
         ans = []
-        curr_hash = {}
+        curr_w = []
         for w in words:
-
-            w_hash = {}
-            for ch in w:
-                if ch in w_hash:
-                    w_hash[ch] +=1
-                else:
-                    w_hash[ch] = 1
-                    
-            if curr_hash != w_hash:
+            new_w = sorted(list(w))
+            if curr_w != new_w :
                 ans.append(w)
-                curr_hash = w_hash
+                curr_w = new_w
         
         return ans
-        

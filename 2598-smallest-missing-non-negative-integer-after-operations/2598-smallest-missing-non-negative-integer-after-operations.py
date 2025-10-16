9faa58e1efe160b2121c9b478db697fab9400d1c
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-10-15 19:53:04 -0600

    Time: 47 ms (91.11%), Space: 32.2 MB (79.64%) - LeetHub

--- 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.py
+++ 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.py
@@ -3,21 +3,41 @@ class Solution:
         
         #sol O(n), to be precise 2*n
 
-        dict_div_nums = {}
+        # dict_div_nums = {}
+        # for n in nums:
+        #     curr_div = n%value 
+        #     if curr_div not in dict_div_nums:
+        #         dict_div_nums[curr_div] = 1
+        #     else:
+        #         dict_div_nums[curr_div] += 1
+
+
+        # for i in range(len(nums)):
+        #     curr_div = i%value
+        #     if curr_div in dict_div_nums and dict_div_nums[curr_div] != 0 :
+        #         dict_div_nums[curr_div] -=1
+        #     else:
+        #         return i
+
+        # return len(nums)
+
+#-----------------------------------------------------------
+
+#Vayase a la verga!! claro que es mas rapido pendejo ajajajaajaj
+
+
+        DP = [0] * value
+
         for n in nums:
-            curr_div = n%value 
-            if curr_div not in dict_div_nums:
-                dict_div_nums[curr_div] = 1
-            else:
-                dict_div_nums[curr_div] += 1
+            DP[n % value] += 1
+        
 
+        seed = 0
 
-        for i in range(len(nums)):
-            curr_div = i%value
-            if curr_div in dict_div_nums and dict_div_nums[curr_div] != 0 :
-                dict_div_nums[curr_div] -=1
-            else:
-                return i
+        rounds = min(DP)
 
-        return len(nums)
+        #This approach is faster than the one below.
+        for i in range(value):
+            if DP[i] - rounds == 0:
+                return (rounds * value) + i
 

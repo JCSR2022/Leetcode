8f0395b25b802abbee7844f82aa31f30961653f4
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-10-19 19:16:22 -0600

    Time: 0 ms (100%), Space: 17.9 MB (37.61%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
@@ -1,8 +1,19 @@
 class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
 
-        op_val = {"++X":1,"X++":1,"--X":-1,"X--":-1}
-        x = 0
-        for op in operations:
-            x += op_val[op]
-        return x
+        # op_val = {"++X":1,"X++":1,"--X":-1,"X--":-1}
+        # x = 0
+        # for op in operations:
+        #     x += op_val[op]
+        # return x
+#-----------------------------------------------
+
+        #this is faster!!!!??? with 2 if?
+
+        ans = 0
+        for o in operations:
+            if '+' in o:
+                ans = ans + 1
+            elif '-' in o:
+                ans = ans - 1
+        return ans

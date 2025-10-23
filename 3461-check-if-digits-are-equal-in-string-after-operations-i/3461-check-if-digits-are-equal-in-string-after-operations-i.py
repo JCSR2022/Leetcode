9cec9ff4005739af1a36f4d66d57b86f8b050aad
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-10-22 19:40:52 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.py
+++ 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.py
@@ -1,17 +1,30 @@
 class Solution:
     def hasSameDigits(self, s: str) -> bool:
         
-        curr_s = [ int(ch) for ch in s]
-        curr_size = len(curr_s) 
+        # curr_s = [int(ch) for ch in s]
+        # curr_size = len(curr_s) 
         
+        # while curr_size > 2:
+        #     new_curr = []
+        #     for i in range(1,curr_size):
+        #         new_curr.append((curr_s[i-1]+curr_s[i])%10 )
+
+        #     curr_size -=1
+        #     curr_s = new_curr
+
+        # return curr_s[0] == curr_s[1]
+
+#------------------------------------------------------
+
+        curr_s = [int(ch) for ch in s]
+        curr_size = len(curr_s) 
+        count = 1
         while curr_size > 2:
-            new_curr = []
-            for i in range(1,curr_size):
-                new_curr.append((curr_s[i-1]+curr_s[i])%10 )
+
+            for i in range(count,curr_size):
+                curr_s[i] = (curr_s[i-1]+curr_s[i])%10 
 
             curr_size -=1
-            curr_s = new_curr
+            count += 1
 
-        return curr_s[0] == curr_s[1]
-
-        
+        return curr_s[-1] == curr_s[-2]

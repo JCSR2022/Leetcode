7788175e342e9d047dd2b5fc72794b0a58a9fb7c
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-10-27 20:17:09 -0600

    Time: 49 ms (54.26%), Space: 17.6 MB (79.79%) - LeetHub

--- 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.py
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.py
@@ -2,18 +2,38 @@ class Solution:
     def countValidSelections(self, nums: List[int]) -> int:
 
         #valid select if 0 and left = right  +-1
+
+        # size = len(nums)
+        # left = [0]*(size+1)
+        # right = [0]*(size+1)
+
+        # for i in range(size):
+        #     left[i+1] = left[i]+nums[i]
+
+        # for i in range(size-1,-1,-1):
+        #     right[i] = right[i+1]+nums[i]
+
+        # ans = 0
+        # for i in range(size):
+        #     if nums[i] == 0:
+        #         if left[i+1] == right[i]:
+        #             ans +=2
+        #         elif abs(left[i+1] - right[i]) == 1:
+        #             ans +=1
+        # return ans
+
+
+#------------------------------------------
+
+
+
         size = len(nums)
         left = [0]*(size+1)
         right = [0]*(size+1)
 
         for i in range(size):
             left[i+1] = left[i]+nums[i]
-
-        for i in range(size-1,-1,-1):
-            right[i] = right[i+1]+nums[i]
-
-        # print(left)
-        # print(right)
+            right[size-i-1] = right[size-i]+nums[size-i-1]
 
         ans = 0
         for i in range(size):
@@ -23,7 +43,6 @@ class Solution:
                 elif abs(left[i+1] - right[i]) == 1:
                     ans +=1
         return ans
-
 
 
 

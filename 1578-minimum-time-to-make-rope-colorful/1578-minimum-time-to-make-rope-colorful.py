575554f22e998d73bdf3804af8478d4783f8ca1f
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-11-02 19:35:25 -0600

    Time: 191 ms (10.4%), Space: 26.6 MB (15.19%) - LeetHub

---
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -0,0 +1,77 @@
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+
+            ans = 0
+            heap = []
+            for i in range(1,len(colors)):
+                if colors[i] == colors[i-1]:
+                    if len(heap) == 0:
+                        heapq.heappush(heap, neededTime[i-1])
+                    heapq.heappush(heap, neededTime[i])
+                else:
+                    if len(heap) != 0:
+                        while len(heap) > 1:
+                            ans += heapq.heappop(heap)
+                        heapq.heappop(heap)
+
+            while len(heap) > 1:
+                ans += heapq.heappop(heap)
+                
+            return ans
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # num_ballons = len(neededTime)
+        # if num_ballons==1:
+        #     return 0
+
+        # list_colors = list(colors)
+        # j = 1 
+        # group = []
+        # sum_time = 0
+        # while j < num_ballons:
+        #     i = j-1
+        #     if list_colors[i] == list_colors[j]:
+        #         if group == []:
+        #             group = [neededTime[i],neededTime[j]]
+        #             #print(i,j,list_colors[i],list_colors[j],neededTime[i],neededTime[j],sum_time,group)
+        #             sum_time += min(group)
+        #             group.pop(group.index(min(group)))
+        #         else:
+        #             group.append(neededTime[j])
+        
+        #             sum_time += min(group)
+        #             group.pop(group.index(min(group)))    
+        #     else:
+        #         group = []
+        
+        #     j +=1
+
+        # return sum_time

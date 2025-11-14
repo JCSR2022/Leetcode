a8de21c90cd19a4fbfae82209cd093b25a413354
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-11-13 20:16:44 -0600

    Time: 751 ms (34.27%), Space: 39.7 MB (49.77%) - LeetHub

--- 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.py
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.py
@@ -2,14 +2,55 @@ class Solution:
     def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
         #brute force
 
-        ans = [ [0]*n for _ in range(n)]
+        # ans = [ [0]*n for _ in range(n)]
+
+        # for row1i, col1i, row2i, col2i in queries :
+        #     for j in range(col1i,col2i+1):
+        #         for i in range(row1i,row2i+1):
+        #             ans[i][j] +=1
+        #     # print(row1i, col1i, row2i, col2i)
+        #     # for row in ans:
+        #     #     print(row)
+        #     # print()
+        # return ans
+
+#Time Limit Exceeded
+#-------------------------------------------------------------------
+
+        ans = [ [0]*(n+1) for _ in range(n)]
 
         for row1i, col1i, row2i, col2i in queries :
-            for j in range(col1i,col2i+1):
-                for i in range(row1i,row2i+1):
-                    ans[i][j] +=1
+            for i in range(row1i,row2i+1):
+                ans[i][col1i] +=1
+                ans[i][col2i+1] -=1
+                
             # print(row1i, col1i, row2i, col2i)
             # for row in ans:
             #     print(row)
             # print()
-        return ans
+
+        for row in ans:
+            for j in range(n):
+                row[j+1] = row[j+1]+row[j]     
+
+        return [ row[:-1] for row in ans]
+
+#--------------------------------------------------0
+        #This will not work logic is bad
+
+        # pre_ans = [0]*((n**2)+1)
+        # for row1i, col1i, row2i, col2i in queries :
+        #     pre_ans[row1i*n+ col1i ] +=1
+        #     pre_ans[row2i*n+ col2i+1 ] -=1
+
+        # print(pre_ans)
+
+
+        # ans = [ [0]*n for _ in range(n)]
+        # return ans
+
+
+
+
+
+

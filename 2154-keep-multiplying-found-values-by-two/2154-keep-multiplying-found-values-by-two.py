f322871cdf9a76e5e9cd5cac58c3fcec52f0bfb7
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-11-18 21:50:37 -0600

    Time: 0 ms (100%), Space: 18 MB (12.78%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.py
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.py
@@ -2,10 +2,31 @@ class Solution:
     def findFinalValue(self, nums: List[int], original: int) -> int:
 
         #do the size of the arr i think a brute force will work
-        nums = set(nums)
+        # nums = set(nums)
 
-        while original in nums:
-            original *=2
+        # while original in nums:
+        #     original *=2
+
+        # return original
+#------------------------------------------------------------------
+
+        nums.sort()
+        high = len(nums)
+        i = bisect.bisect_left(nums,original)
+        
+        while i < high:
+            print(i,nums[i],original == nums[i])
+            if original == nums[i]:
+                original *= 2
+            else:
+                return original
+            i = bisect.bisect_left(nums,original,i,high)
 
         return original
-        
+
+
+#----------------------------------------------------------------
+
+
+
+

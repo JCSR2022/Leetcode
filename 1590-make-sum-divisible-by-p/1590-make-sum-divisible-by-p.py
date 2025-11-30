fd9eb7571bcbe03b2cfa74c488e4439573cedb36
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-11-30 15:11:03 -0600

    Time: 71 ms (83.08%), Space: 38.2 MB (59.21%) - LeetHub

--- 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.py
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.py
@@ -1,26 +1,58 @@
 class Solution:
     def minSubarray(self, nums: List[int], p: int) -> int:
-        
-        size = len(nums)
-        target = sum(nums)%p
 
-        if target == 0:
+        #https://www.youtube.com/watch?v=7FJrMTpadRI
+
+        total =sum(nums)
+        remain = total % p
+
+        if remain == 0 :
             return 0
 
-        prefix = [0]*size
-        prefix[0] = nums[0]
-        for i in range(1,size):
-            prefix[i] = nums[i]+prefix[i-1]
+        res = len(nums)
+        cur_sum = 0
+        remain_to_idx = {0:-1}
+
+        for i,n in enumerate(nums):
+            cur_sum = (cur_sum + n)%p
+            prefix = (cur_sum - remain + p) % p
+            if prefix in remain_to_idx:
+                cur_len = i - remain_to_idx[prefix]
+                res = min(res,cur_len)
+            remain_to_idx[cur_sum] = i
+
+        return -1 if res == len(nums) else res
+
+
+
+
+
+#--------------------------------------------------------------------
+#no funciona, como tu 
+
+        # size = len(nums)
+        # target = sum(nums)%p
+
+        # if target == 0:
+        #     return 0
+
+        # prefix = [0]*size
+        # prefix[0] = nums[0]
+        # for i in range(1,size):
+        #     prefix[i] = nums[i]+prefix[i-1]
         
-        min_len = float("inf")
-        for l in range(size):
-            for r in range(l+1,size):
-                if (prefix[r]-prefix[l])%p == target:
-                    min_len = min(min_len,r-l)
+        # print(prefix)
+        
+        # min_len = float("inf")
+        # for l in range(size):
+        #     for r in range(l+1,size):
+        #         print((l,r),prefix[r]-prefix[l],(prefix[r]-prefix[l])%p)
+        #         if (prefix[r]-prefix[l])%p == target:
+        #             min_len = min(min_len,r-l)
 
      
 
-        return min_len if min_len < float("inf") else -1
+        # return min_len if min_len < float("inf") else -1
 
 
 

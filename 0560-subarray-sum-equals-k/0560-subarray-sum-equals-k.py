02daf9ff39c2c59c2aee3dc6a6025dcb66357b1e
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-11-30 17:29:43 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,20 +1,123 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+
+
+        size = len(nums)  
+        prefix = [0]*size
+        prefix[0] = nums[0]
+        for i in range(1,size):
+            prefix[i] = prefix[i-1]+nums[i]
         
-        #https://www.youtube.com/watch?v=fFVZt-6sgyo
+        count = 0
+        for n in prefix:
+            if n == k:
+                count +=1 
+
+        for i in range(size):
+            for j in range(i+1,size):
+                curr =  prefix[j] - prefix[i]
+                if curr == k:
+                    count +=1 
+
+        return count
+
+#-------------------------------------------------------------------
+        # size = len(nums)
+        # count  = 0
+        # curr_sum = 0
+        # previos_sum_record = defaultdict(int)
         
-        res = 0
-        curSum = 0
-        prefixSums = {0:1}
-        
-        for n in nums:
-            curSum += n
-            diff = curSum - k
+        # for i in range(size):
+        #     curr_sum += nums[i]
+        #     previos_sum_record[curr_sum] += 1
+        #     previos_valid =  curr_sum - k 
+        #     if previos_valid in previos_sum_record:
+        #          count  = previos_sum_record[previos_valid]
+        #     print(i,curr_sum,dict(previos_sum_record),previos_valid,count)     
+        # return count
+
+
+#noooooooo
+#----------------------------------------------------------------
+        #brute force as always, the only thing you can do
+
+        # size = len(nums)
+        # count  = 0
+        # for i in range(size):
+        #     for j in range(i+1,size+1):
+        #         print(nums[i:j],sum(nums[i:j]))
+        #         if sum(nums[i:j]) == k:
+        #             count +=1
+        # return count
+
+
+
+        # size = len(nums)
+        # count  = 0
+        # for i in range(size):
+        #     curr_sum = 0
+        #     for j in range(i,size):
+        #         curr_sum +=nums[j]
+        #         if curr_sum == k:
+        #             count +=1
+        # return count
+
+
+
+
+
+#--------------------------------------------------------------
+        # record_sums = defaultdict(int)        
+
+        # for n in nums:
+
+        #     new_records = defaultdict(int)
+        #     for record in record_sums.keys():
+        #         new_records[record+n] +=1
+        #     for record in new_records:
+        #         record_sums[record] += new_records[record]
             
-            res += prefixSums.get(diff,0)
-            prefixSums[curSum] = 1 + prefixSums.get(curSum,0)
+        #     record_sums[n] += 1
+
+        #     print(dict(record_sums)) 
+
+        # return record_sums[k]
+
+#no imbecil!!!!!!!!!
+#------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------------------------
+
+        # #https://www.youtube.com/watch?v=fFVZt-6sgyo
+        
+        # res = 0
+        # curSum = 0
+        # prefixSums = {0:1}
+        
+        # for n in nums:
+        #     curSum += n
+        #     diff = curSum - k
             
-        return res
+        #     res += prefixSums.get(diff,0)
+        #     prefixSums[curSum] = 1 + prefixSums.get(curSum,0)
+            
+        # return res
         
         
         

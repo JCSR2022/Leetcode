458efd6aa8d69e19d97de97250d60572531ef8d7
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-12-12 21:20:21 -0600

    Time: 15 ms (27.33%), Space: 17.8 MB (96.78%) - LeetHub

---
+++ 3606-coupon-code-validator/3606-coupon-code-validator.py
@@ -0,0 +1,24 @@
+class Solution:
+    def validateCoupons(self, code: List[str], businessLine: List[str], isActive: List[bool]) -> List[str]:
+        
+        def is_valid_code(s: str) -> bool:
+            return bool(re.fullmatch(r'\w+', s))
+
+        def is_valid_business(s:str) ->bool:
+            return s in {"electronics", "grocery", "pharmacy", "restaurant"}
+
+
+        ans = {"electronics":[],"grocery":[],"pharmacy":[],"restaurant":[]}
+        for i in range(len(code)):
+            if (  is_valid_code(code[i]) and 
+                  is_valid_business(businessLine[i]) and
+                  isActive[i] ):
+                
+                ans[businessLine[i]].append(code[i])
+
+        final_ans = []
+        for businessLine in ["electronics", "grocery", "pharmacy", "restaurant"]:
+            ans[businessLine].sort(reverse=False)
+            final_ans += ans[businessLine]
+
+        return final_ans

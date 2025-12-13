1277d46211665549f498a64232223e80ea644f3e
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-12-12 21:36:47 -0600

    Time: 26 ms (7.4%), Space: 18.2 MB (13.18%) - LeetHub

--- 3606-coupon-code-validator/3606-coupon-code-validator.py
+++ 3606-coupon-code-validator/3606-coupon-code-validator.py
@@ -1,55 +1,83 @@
 class Solution:
     def validateCoupons(self, code: List[str], businessLine: List[str], isActive: List[bool]) -> List[str]:
-        
-        # def is_valid_code(s: str) -> bool:
-        #     return bool(re.fullmatch(r'\w+', s))
- 
+        #final ver
 
-        def is_valid_code(s: str) -> bool:
-            #string.ascii_letters
-            if len(s) == 0: return False
-            #valids ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
-            for ch in s:
-                if ch not in valids:
-                    return False
-            return True
+        val_ch ={'F', 'p', 'N', 'P', 'h', 'E', 'g', 'm', 'H',
+            'X', 'o', 'K', 'G', 'v', 's', 'c', 'A', 'I', 'r', 'C',
+            'n', 'd', 'z', '9', 'a', 'T', 'W', 'i', 'V', 'x', 'q', 
+            '1', 'f', 'Y', 'y', '0', '2', 'M', '3', 'e', 'j', 'b',
+            'u', 'J', '5', 'l', 'O', 'w', '6', '7', 'U', 'R', 'k', 
+            'L', 'Z', '8', '4', '_', 'D', 'Q', 'B', 't', 'S'}
 
-
-        def is_valid_code(s: str) -> bool:
-            #string.ascii_letters
-            if not s: return False
-            
-            valids ={'F', 'p', 'N', 'P', 'h', 'E', 'g', 'm', 'H',
-             'X', 'o', 'K', 'G', 'v', 's', 'c', 'A', 'I', 'r', 'C',
-              'n', 'd', 'z', '9', 'a', 'T', 'W', 'i', 'V', 'x', 'q', 
-              '1', 'f', 'Y', 'y', '0', '2', 'M', '3', 'e', 'j', 'b',
-               'u', 'J', '5', 'l', 'O', 'w', '6', '7', 'U', 'R', 'k', 
-               'L', 'Z', '8', '4', '_', 'D', 'Q', 'B', 't', 'S'}
-
-            return all(ch in valids for ch in s)
-
-
-
-        # def is_valid_code(s: str) -> bool:
-        #     if not s:
-        #         return False
-        #     return all(ch.isalnum() or ch == '_' for ch in s)
-
-        def is_valid_business(s:str) ->bool:
-            return s in {"electronics", "grocery", "pharmacy", "restaurant"}
+        val_business = {"electronics", "grocery", "pharmacy", "restaurant"}
 
 
         ans = {"electronics":[],"grocery":[],"pharmacy":[],"restaurant":[]}
         for i in range(len(code)):
-            if (  is_valid_code(code[i]) and 
-                  is_valid_business(businessLine[i]) and
-                  isActive[i] ):
+            if (    code[i] and 
+                    all(ch in val_ch for ch in code[i])  and 
+                    (businessLine[i] in val_business ) and
+                    isActive[i] ):
                 
                 ans[businessLine[i]].append(code[i])
 
         final_ans = []
         for businessLine in ["electronics", "grocery", "pharmacy", "restaurant"]:
-            ans[businessLine].sort(reverse=False)
-            final_ans += ans[businessLine]
+            if ans[businessLine]:
+                ans[businessLine].sort(reverse=False)
+                final_ans += ans[businessLine]
 
         return final_ans
+
+
+#---------------------------------------------------
+        # # def is_valid_code(s: str) -> bool:
+        # #     return bool(re.fullmatch(r'\w+', s))
+ 
+
+        # def is_valid_code(s: str) -> bool:
+        #     #string.ascii_letters
+        #     if len(s) == 0: return False
+        #     #valids ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
+        #     for ch in s:
+        #         if ch not in valids:
+        #             return False
+        #     return True
+
+
+        # def is_valid_code(s: str) -> bool:
+        #     if not s: return False
+        #     valids ={'F', 'p', 'N', 'P', 'h', 'E', 'g', 'm', 'H',
+        #      'X', 'o', 'K', 'G', 'v', 's', 'c', 'A', 'I', 'r', 'C',
+        #       'n', 'd', 'z', '9', 'a', 'T', 'W', 'i', 'V', 'x', 'q', 
+        #       '1', 'f', 'Y', 'y', '0', '2', 'M', '3', 'e', 'j', 'b',
+        #        'u', 'J', '5', 'l', 'O', 'w', '6', '7', 'U', 'R', 'k', 
+        #        'L', 'Z', '8', '4', '_', 'D', 'Q', 'B', 't', 'S'}
+
+        #     return all(ch in valids for ch in s)
+
+
+
+        # # def is_valid_code(s: str) -> bool:
+        # #     if not s:
+        # #         return False
+        # #     return all(ch.isalnum() or ch == '_' for ch in s)
+
+        # def is_valid_business(s:str) ->bool:
+        #     return s in {"electronics", "grocery", "pharmacy", "restaurant"}
+
+
+        # ans = {"electronics":[],"grocery":[],"pharmacy":[],"restaurant":[]}
+        # for i in range(len(code)):
+        #     if (  is_valid_code(code[i]) and 
+        #           is_valid_business(businessLine[i]) and
+        #           isActive[i] ):
+                
+        #         ans[businessLine[i]].append(code[i])
+
+        # final_ans = []
+        # for businessLine in ["electronics", "grocery", "pharmacy", "restaurant"]:
+        #     ans[businessLine].sort(reverse=False)
+        #     final_ans += ans[businessLine]
+
+        # return final_ans

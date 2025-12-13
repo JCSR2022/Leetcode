37fa27dfed08cc872600eeeaa0d6c0fab34062e2
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-12-12 21:32:35 -0600

    Time: 19 ms (15.76%), Space: 18.1 MB (31.19%) - LeetHub

--- 3606-coupon-code-validator/3606-coupon-code-validator.py
+++ 3606-coupon-code-validator/3606-coupon-code-validator.py
@@ -5,20 +5,35 @@ class Solution:
         #     return bool(re.fullmatch(r'\w+', s))
  
 
-        # def is_valid_code(s: str) -> bool:
-        #     #string.ascii_letters
-        #     if len(s) == 0: return False
-        #     valids ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
-        #     for ch in s:
-        #         if ch not in valids:
-        #             return False
-        #     return True
+        def is_valid_code(s: str) -> bool:
+            #string.ascii_letters
+            if len(s) == 0: return False
+            #valids ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
+            for ch in s:
+                if ch not in valids:
+                    return False
+            return True
 
 
         def is_valid_code(s: str) -> bool:
-            if not s:
-                return False
-            return all(ch.isalnum() or ch == '_' for ch in s)
+            #string.ascii_letters
+            if not s: return False
+            
+            valids ={'F', 'p', 'N', 'P', 'h', 'E', 'g', 'm', 'H',
+             'X', 'o', 'K', 'G', 'v', 's', 'c', 'A', 'I', 'r', 'C',
+              'n', 'd', 'z', '9', 'a', 'T', 'W', 'i', 'V', 'x', 'q', 
+              '1', 'f', 'Y', 'y', '0', '2', 'M', '3', 'e', 'j', 'b',
+               'u', 'J', '5', 'l', 'O', 'w', '6', '7', 'U', 'R', 'k', 
+               'L', 'Z', '8', '4', '_', 'D', 'Q', 'B', 't', 'S'}
+
+            return all(ch in valids for ch in s)
+
+
+
+        # def is_valid_code(s: str) -> bool:
+        #     if not s:
+        #         return False
+        #     return all(ch.isalnum() or ch == '_' for ch in s)
 
         def is_valid_business(s:str) ->bool:
             return s in {"electronics", "grocery", "pharmacy", "restaurant"}

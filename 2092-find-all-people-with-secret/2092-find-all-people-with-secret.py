a9487626bd3f069fb065a61df3df58261f24363d
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2025-12-18 21:42:52 -0600

    Time: 376 ms (38.22%), Space: 80.9 MB (6.37%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
@@ -1,5 +1,58 @@
+
+# class myUnionFind1:
+#     def __init__(self,n):
+#         self.arr = [i for i in range(n)]
+
+#     def find(self,val):
+#         while self.arr[val] != val:
+#                 val = self.arr[val]
+#         return val
+
+#     def union(self,x,y):
+#         root_x = self.find(x)
+#         root_y = self.find(y)
+
+#         if  root_x == root_y:
+#             return 
+#         self.arr[x] = root_y
+
+
 class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]: 
+
+        # beging_time = float("inf")
+        # dict_meetings = defaultdict(set)
+        # for x,y,time in meetings:
+        #     dict_meetings[time].add((x,y))
+        #     if firstPerson in {x,y}:
+        #         beging_time = min(beging_time,time)
+        # print(beging_time)
+        # print(dict(dict_meetings ))
+
+        # my_union = myUnionFind1(n)
+        # # print("my_union:",my_union.arr)
+        # # print("my_union.find(2):",my_union.find(2))
+        # # my_union.union(2,3)
+        # # print("my_union.union(2,3), my_union:",my_union.arr)
+        # # print("my_union.find(2):",my_union.find(2))
+
+
+        # for t in sorted(dict_meetings.keys()):
+        #     if t >= beging_time:
+        #         print(t,dict_meetings[t])
+
+
+        
+
+
+        # #Noooooooooooooooooooooooooooooooo
+        # return [0,1,2,3]
+
+
+
+       
+       
+    
         secrets = set([0,firstPerson])
         
         time_map = {} # time -. adj list meetings

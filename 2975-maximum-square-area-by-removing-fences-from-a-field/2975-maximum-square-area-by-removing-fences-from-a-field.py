171c5cbaa0c11e647a8b03d170494eec16a8bdda
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-01-15 21:25:58 -0600

    Time: 1578 ms (21.95%), Space: 54.1 MB (12.8%) - LeetHub

--- 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.py
+++ 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.py
@@ -1,16 +1,44 @@
 class Solution:
     def maximizeSquareArea(self, m: int, n: int, hFences: List[int], vFences: List[int]) -> int:
         
+        # mod = 10**9+7
+
+        # hFences = [1]+sorted(hFences)+[m]
+        # h_gaps = set()
+        # for i in range(len(hFences)):
+        #     for j in range(i+1,len(hFences)):
+        #         h_gaps.add(hFences[j]-hFences[i])
+        # print(hFences)
+        # print(h_gaps)
+
+        # vFences = [1]+sorted(vFences)+[n]
+        # v_gaps = set()
+        # for i in range(len(vFences)):
+        #     for j in range(i+1,len(vFences)):
+        #         v_gaps.add(vFences[j]-vFences[i])
+        # print(vFences)
+        # print(v_gaps)
+
+        # squares = [ gap for gap in h_gaps if gap in v_gaps]
+
+        # return (max(squares)**2)%mod  if squares else -1
+
+
+#-----------------------------------------------------
+#to make it faster:
+
         mod = 10**9+7
 
-        hFences = [1]+sorted(hFences)+[m]
+        hFences.extend([1,m])       #faster than [1]+sorted(hFences)+[m]
+        hFences.sort()              #faster because do not create new arr
         h_gaps = set()
         for i in range(len(hFences)):
             for j in range(i+1,len(hFences)):
                 h_gaps.add(hFences[j]-hFences[i])
         
 
-        vFences = [1]+sorted(vFences)+[n]
+        vFences.extend([1,n])       
+        vFences.sort()
         v_gaps = set()
         for i in range(len(vFences)):
             for j in range(i+1,len(vFences)):
@@ -19,4 +47,3 @@ class Solution:
         squares = [ gap for gap in h_gaps if gap in v_gaps]
 
         return (max(squares)**2)%mod  if squares else -1
-        

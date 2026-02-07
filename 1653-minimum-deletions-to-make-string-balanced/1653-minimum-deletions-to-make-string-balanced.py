1054f6b2ab2dd58663749030e967b6dc1119a508
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-02-07 16:04:40 -0600

    Time: 487 ms (32.97%), Space: 21.9 MB (28.38%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -1,7 +1,33 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
         
+
+        #only 2 pass
+
+        sum_a = [0]*(len(s)+1)
+
+        for i in range(len(s)-1,-1,-1):
+            sum_a[i] = sum_a[i+1]
+            if s[i] =='a':
+                sum_a[i] += 1
+
+        ans = len(s)
+        count_b = 0
+        for i in range(len(s)):
+            if s[i] == 'b':
+                count_b += 1
         
+            ans = min(ans,sum_a[i]+count_b)
+        
+        return ans-1
+
+
+
+
+
+
+
+#-----------------------------------------------------------    
         #brute force:
         #two pointers l, r:
         # pointer l move from left to rigth, counts "b" to the left
@@ -12,6 +38,7 @@ class Solution:
         count_a = 0
         count_b = 0
         for l in range(len(s)):
+            
             r = len(s)-l-1
             
             if s[l] == 'b': count_b +=1
@@ -19,10 +46,35 @@ class Solution:
     
             sum_ab[l] += count_b
             sum_ab[r] += count_a
+            print(sum_ab,l,count_a,count_b)
             #print(l,r,s[l],s[r],count_b,count_a)
         #print(sum_ab)
             
         return min(sum_ab)-1
+        
+#-------------------------------------------------------
+# #just to visualize:
+
+        
+#         sum_a = [0]*len(s)
+#         sum_b = [0]*len(s)
+#         count_a = 0
+#         count_b = 0
+#         for l in range(len(s)):
+#             r = len(s)-l-1
+            
+#             if s[l] == 'b': count_b +=1
+#             if s[r] == 'a': count_a +=1
+    
+#             sum_a[r] += count_a
+#             sum_b[l] += count_b
+#             print(sum_b,sum_a)
+            
+#         sum_ab = [a+b  for a,b in zip(sum_a,sum_b)]
+#         print(sum_b)
+#         print(sum_a)
+#         print(sum_ab)
+#         return min(sum_ab)-1
         
         
         

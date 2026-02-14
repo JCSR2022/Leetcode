c4099210472b68d5e6b4e06a466b96dd0a52cef1
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-02-13 20:13:27 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0799-champagne-tower/0799-champagne-tower.py
@@ -0,0 +1,23 @@
+class Solution:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        
+
+#                           1                       1
+#                       1       1                   2
+#                   1       2       1               3   4>1/4,2/4,1/4; 5>2/4,3/4,2/4; 6>3/4,4/4,3/4, 7>4/4,5/4????,4/4 
+#               1       3       3       1           4
+#           1       4       6       4       1       5
+
+
+#------------------------------------------------
+#                                          1                                    1
+#                               1/2                 1/2                         2,3
+#                       1/4                1/2              1/4                 4,5,6,7   
+#               1/8             3/4                 3/4             1/8         4
+
+
+
+#maldita basura, nunca vas a servir para nada, eres una mierda inutil!!!!!!!
+
+
+

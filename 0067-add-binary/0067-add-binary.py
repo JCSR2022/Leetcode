f6e90ff96c0f48cdbf14f1ce3c1309de2185e729
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-02-14 20:31:06 -0600

    Time: 3 ms (44.47%), Space: 19.4 MB (57.25%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,8 +1,41 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
+
+        ans = []
+        carry = 0
+
+        max_len = max(len(a),len(b))
+        a = a.zfill(max_len)
+        b = b.zfill(max_len)
+
+        for i in range(max_len-1,-1,-1):
+            curr = int(a[i])+int(b[i])+carry
+            carry = 0
+            if curr == 0:
+                ans.append('0')
+            elif curr == 1:
+                ans.append('1')
+            elif curr == 2:
+                ans.append('0')
+                carry = 1
+            elif curr == 3:
+                ans.append('1')
+                carry = 1
+                
+        if carry ==1:
+            ans.append('1')
+
+        return "".join(ans[::-1])
+
+            
+
+
+
+
+
+#-------------------------------------------------------------
+        # dec_a = int(a, 2)
+        # dec_b = int(b, 2)
         
-        dec_a = int(a, 2)
-        dec_b = int(b, 2)
-        
-        return bin(dec_a+dec_b)[2:]
+        # return bin(dec_a+dec_b)[2:]
         

98f3d9cbe33d49c54fbf7b9c963870f02f5a87c3
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-03-15 21:51:27 -0600

    Time: 190 ms (78.79%), Space: 54.7 MB (84.85%) - LeetHub

--- 1622-fancy-sequence/1622-fancy-sequence.py
+++ 1622-fancy-sequence/1622-fancy-sequence.py
@@ -1,25 +1,32 @@
 class Fancy:
+    #  y = m*x+c
+    #  (y-c)/m = x
+    
 
     def __init__(self):
         self.arr = []
-        self.mod = 10**9+7 
+        self.mult = 1
+        self.const = 0
+        self.mod = 10**9+7
 
     def append(self, val: int) -> None:
-        self.arr.append(val)
-        
+        #self.arr.append(  ((val-self.const)/self.mult)% self.mod )        
+        corect_val =  ((val-self.const)*pow(self.mult,-1,self.mod))%self.mod
+        self.arr.append(corect_val)
     def addAll(self, inc: int) -> None:
-        for i in range(len(self.arr)):
-            self.arr[i] +=inc
-            self.arr[i] %=self.mod
+        self.const = (self.const+inc)%self.mod 
+        
 
     def multAll(self, m: int) -> None:
-        for i in range(len(self.arr)):
-            self.arr[i] *=m
-            self.arr[i] %=self.mod
         
+        self.const = (self.const*m)%self.mod
+        self.mult = (self.mult*m)%self.mod
+        
+
     def getIndex(self, idx: int) -> int:
-        
-        return self.arr[idx] if len(self.arr)>idx else -1
+        if idx >= len(self.arr): return -1
+        return int(self.arr[idx]*self.mult+self.const)%self.mod
+
 
 # Your Fancy object will be instantiated and called as such:
 # obj = Fancy()

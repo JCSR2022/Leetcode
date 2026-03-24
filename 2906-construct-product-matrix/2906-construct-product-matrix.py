71daf70cfbadbc892b5bf56b98b43d23887c850a
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-03-23 20:56:59 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.py
+++ 2906-construct-product-matrix/2906-construct-product-matrix.py
@@ -4,26 +4,34 @@ class Solution:
         #this should works for python because no need for mod only at the end
         #  O(2*n*m)
 
-        # mod = 12345
+        mod = 12345
 
-        # n = len(grid)
-        # m = len(grid[0])
+        n = len(grid)
+        m = len(grid[0])
 
-        # mult = 1
-        # for row in grid:
-        #     for val in row:
-        #         mult *=val
+        mult = 1
+        for row in grid:
+            for val in row:
+                mult *=val
 
 
-        # p = [ [0]*m for _ in range(n)]
-        # for i in range(n):
-        #     for j in range(m): 
-        #         p[i][j] += (mult//grid[i][j])%mod
+        p = [ [0]*m for _ in range(n)]
+        for i in range(n):
+            for j in range(m): 
+                p[i][j] += (mult//grid[i][j])%mod
         
-        # return p
+        return p
                 
 #Time Limit Exceeded?
 #---------------------------------------------------------------------
+
+        #Mantener números pequeños con % mod es la clave real de velocidad
+        #mult *= val, hace que mult crezca enormemente.Después de multiplicar muchos números:
+        # mult puede tener cientos o miles de bits
+        # Cada nueva multiplicación es más cara
+        # La división mult // grid[i][j] es muy costosa
+
+
 
 
         mod = 12345
@@ -37,17 +45,18 @@ class Solution:
         # Prefix
         prefix = [1] * size
         for i in range(1, size):
+            # Después de cada operación se hace % mod
             prefix[i] = (prefix[i-1] * flat[i-1]) % mod
 
         # Suffix
         suffix = [1] * size
         for i in range(size-2, -1, -1):
+            #Entonces los números nunca crecen más allá de mod (~12345)
             suffix[i] = (suffix[i+1] * flat[i+1]) % mod
 
-        print(flat)
-        print(prefix)
-        print(suffix)
-        
+        # print(flat)
+        # print(prefix)
+        # print(suffix)
 
         # Build result
         p = [[0]*m for _ in range(n)]

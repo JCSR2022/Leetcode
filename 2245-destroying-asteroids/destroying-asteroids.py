4a1125231819d4b6e2986b6353188e2c140bf853
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-05-30 20:10:59 -0600

    Time: 95 ms (21.30%) | Memory: 33.8 MB (86.18%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.py
+++ 2245-destroying-asteroids/destroying-asteroids.py
@@ -1,11 +1,12 @@
 class Solution:
     def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
-
+        max_ast = max(asteroids)
         heap_mass = []
         for ast in asteroids:
             heapq.heappush(heap_mass,ast)
             while heap_mass and heap_mass[0] <= mass:
                 mass += heapq.heappop(heap_mass)
-
-        return len(heap_mass) == 0     
+            if mass >= max_ast:
+                return True
+        return False    
         

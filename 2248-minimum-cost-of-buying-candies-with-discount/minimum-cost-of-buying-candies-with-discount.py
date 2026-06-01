fae0e82c4e13ddc6a4a43b6a31680cad9cf30519
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-05-31 19:50:22 -0600

    Time: 2 ms (31.45%) | Memory: 19.4 MB (15.37%) - LeetSync

--- 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.py
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.py
@@ -1,9 +1,15 @@
 class Solution:
     def minimumCost(self, cost: List[int]) -> int:
 
-        cost.sort(reverse = True)
-
-        return sum( cost[i]  for i in range(len(cost)) if (i+1)%3 )
+        # cost.sort(reverse = True)
+        # return sum( cost[i]  for i in range(len(cost)) if (i+1)%3 )
         
+
+        cost.sort(reverse = True)
+        ans = 0
+        for i,n in enumerate(cost):
+            if (i+1)%3:
+                ans += n
+        return ans
 
         

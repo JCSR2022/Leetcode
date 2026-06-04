591a5891d8c82e1a6b8786f338382a61e160faba
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-06-04 08:06:15 -0600

    Time: 1165 ms (17.47%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -6,24 +6,62 @@
 -- JOIN Subjects AS su , Examinations AS ex
 -- ON  st.student_id = ex.student_id and su.subject_name = ex.subject_name
 -- GROUP BY st.student_id, st.student_name,  su.Subjects;
+-- NOOOOOOOOOOOOOOOOOOOOOO
+--     --------------------------------------------------------------------------
+
+
+-- SELECT
+--     st.student_id,
+--     st.student_name,
+--     su.subject_name,
+--     COUNT(ex.subject_name) AS attended_exams
+-- FROM Students AS st
+-- CROSS JOIN Subjects AS su
+-- LEFT JOIN Examinations AS ex
+--     ON st.student_id = ex.student_id
+--    AND su.subject_name = ex.subject_name
+-- GROUP BY
+--     st.student_id,
+--     st.student_name,
+--     su.subject_name
+-- ORDER BY
+--     st.student_id,
+--     su.subject_name;
+
+-- SIIIIIIIIIIIIIIIIIIIIIII
+-- --------------------------------------------------------------------
+
+
+
+-- SELECT st.student_id, st.student_name, su.subject_name, ex.attended_exams
+-- FROM(   SELECT student_id,subject_name,COUNT(*) as attended_exams
+--         FROM Examinations
+--         GROUP BY student_id, subject_name
+-- )   AS ex
+-- LEFT JOIN Students st ON  st.student_id = ex.student_id 
+-- LEFT JOIN Subjects su ON su.subject_name = ex.subject_name
+-- ORDER BY st.student_id, su.subject_name;
+
+-- NOoooooooooooooooooooooo
 
 
 SELECT
     st.student_id,
     st.student_name,
     su.subject_name,
-    COUNT(ex.subject_name) AS attended_exams
-FROM Students AS st
-CROSS JOIN Subjects AS su
-LEFT JOIN Examinations AS ex
+    COALESCE(ex.attended_exams, 0) AS attended_exams
+FROM Students st
+CROSS JOIN Subjects su
+LEFT JOIN (
+    SELECT
+        student_id,
+        subject_name,
+        COUNT(*) AS attended_exams
+    FROM Examinations
+    GROUP BY student_id, subject_name
+) AS ex
     ON st.student_id = ex.student_id
    AND su.subject_name = ex.subject_name
-GROUP BY
-    st.student_id,
-    st.student_name,
-    su.subject_name
 ORDER BY
     st.student_id,
     su.subject_name;
-
-    

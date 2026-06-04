096d9f9a17698047f8f7e494b050328c67b7ce72
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-06-04 11:34:36 -0600

    Time: 631 ms (87.98%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,84 @@
+# Write your MySQL query statement below
+
+
+-- COALESCE(ex.attended_exams, 0)
+
+
+
+-- SELECT *
+-- FROM (SELECT user_id , count(action) AS request
+--     FROM Confirmations
+--     GROUP BY user_id ) AS req
+-- LEFT JOIN (SELECT user_id , count(action) AS confirm
+--     FROM Confirmations 
+--     WHERE action = 'confirmed'
+--     GROUP BY user_id) AS conf
+-- ON req.user_id = conf.user_id;
+
+-- IF conf.confirm IS NULL THEN 0 ELSE conf.confirm  
+-- ELECT user_id , CASE IS NULL THEN 0 ELSE COUNT(action)   AS confirm
+
+
+
+
+-- SELECT req.user_id, req.request, CASE WHEN conf.confirm IS NULL THEN 0 ELSE conf.confirm END AS confirm
+-- FROM (SELECT user_id , COUNT(action) AS request
+--     FROM Confirmations
+--     GROUP BY user_id ) AS req
+-- LEFT JOIN (SELECT user_id , COUNT(action) AS confirm
+--     FROM Confirmations 
+--     WHERE action = 'confirmed'
+--     GROUP BY user_id) AS conf
+-- ON req.user_id = conf.user_id;
+
+
+-- ------------------------------------------------------------------
+
+-- SELECT sig.user_id, ROUND(conf_req.confirm/conf_req.request,2) as confirmation_rate
+-- FROM (
+--     SELECT req.user_id, req.request AS request , CASE WHEN conf.confirm IS NULL THEN 0 ELSE conf.confirm END AS confirm
+--     FROM (SELECT user_id , COUNT(action) AS request
+--         FROM Confirmations
+--         GROUP BY user_id ) AS req
+--     LEFT JOIN (SELECT user_id , COUNT(action) AS confirm
+--         FROM Confirmations 
+--         WHERE action = 'confirmed'
+--         GROUP BY user_id) AS conf
+--     ON req.user_id = conf.user_id) AS conf_req ;
+
+
+--  --------------------------------------------------------------------------
+
+
+-- SELECT sig.user_id, conf_req.confirm/conf_req.request as confirmation_rate
+-- FROM Signups AS sig 
+-- LEFT JOIN (
+--     SELECT req.user_id, req.request AS request , CASE WHEN conf.confirm IS NULL THEN 0 ELSE conf.confirm END AS confirm
+--     FROM (SELECT user_id , COUNT(action) AS request
+--         FROM Confirmations
+--         GROUP BY user_id ) AS req
+--     LEFT JOIN (SELECT user_id , COUNT(action) AS confirm
+--         FROM Confirmations 
+--         WHERE action = 'confirmed'
+--         GROUP BY user_id) AS conf
+--     ON req.user_id = conf.user_id) AS conf_req 
+-- ON sig.user_id  = conf_req.user_id ;
+
+
+--  --------------------------------------------------------------------
+
+SELECT ans.user_id , ROUND( CASE WHEN ans.confirmation_rate IS NULL THEN 0 ELSE ans.confirmation_rate END   , 2) AS confirmation_rate
+FROM (
+SELECT sig.user_id, conf_req.confirm/conf_req.request as confirmation_rate
+FROM Signups AS sig 
+LEFT JOIN (
+    SELECT req.user_id, req.request AS request , CASE WHEN conf.confirm IS NULL THEN 0 ELSE conf.confirm END AS confirm
+    FROM (SELECT user_id , COUNT(action) AS request
+        FROM Confirmations
+        GROUP BY user_id ) AS req
+    LEFT JOIN (SELECT user_id , COUNT(action) AS confirm
+        FROM Confirmations 
+        WHERE action = 'confirmed'
+        GROUP BY user_id) AS conf
+    ON req.user_id = conf.user_id) AS conf_req 
+ON sig.user_id  = conf_req.user_id ) AS ans ;

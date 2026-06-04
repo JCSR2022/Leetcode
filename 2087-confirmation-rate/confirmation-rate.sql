8413a97d1ac5a04af67bea81417bf9f8e11007fe
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-06-04 11:39:16 -0600

    Time: 709 ms (33.35%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -67,18 +67,32 @@
 
 --  --------------------------------------------------------------------
 
-SELECT ans.user_id , ROUND( CASE WHEN ans.confirmation_rate IS NULL THEN 0 ELSE ans.confirmation_rate END   , 2) AS confirmation_rate
-FROM (
-SELECT sig.user_id, conf_req.confirm/conf_req.request as confirmation_rate
-FROM Signups AS sig 
-LEFT JOIN (
-    SELECT req.user_id, req.request AS request , CASE WHEN conf.confirm IS NULL THEN 0 ELSE conf.confirm END AS confirm
-    FROM (SELECT user_id , COUNT(action) AS request
-        FROM Confirmations
-        GROUP BY user_id ) AS req
-    LEFT JOIN (SELECT user_id , COUNT(action) AS confirm
-        FROM Confirmations 
-        WHERE action = 'confirmed'
-        GROUP BY user_id) AS conf
-    ON req.user_id = conf.user_id) AS conf_req 
-ON sig.user_id  = conf_req.user_id ) AS ans ;
+-- SELECT ans.user_id , ROUND( CASE WHEN ans.confirmation_rate IS NULL THEN 0 ELSE ans.confirmation_rate END   , 2) AS confirmation_rate
+-- FROM (
+-- SELECT sig.user_id, conf_req.confirm/conf_req.request as confirmation_rate
+-- FROM Signups AS sig 
+-- LEFT JOIN (
+--     SELECT req.user_id, req.request AS request , CASE WHEN conf.confirm IS NULL THEN 0 ELSE conf.confirm END AS confirm
+--     FROM (SELECT user_id , COUNT(action) AS request
+--         FROM Confirmations
+--         GROUP BY user_id ) AS req
+--     LEFT JOIN (SELECT user_id , COUNT(action) AS confirm
+--         FROM Confirmations 
+--         WHERE action = 'confirmed'
+--         GROUP BY user_id) AS conf
+--     ON req.user_id = conf.user_id) AS conf_req 
+-- ON sig.user_id  = conf_req.user_id ) AS ans ;
+
+-- MI SOLUCION DE MIERDA, FUNCIONA PERO ES UNA MIERDA!!!!
+-- ----------------------------------------------------------------
+
+
+
+SELECT s.user_id,
+    ROUND( COALESCE( AVG(c.action = 'confirmed'),0 ), 2 ) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+    ON s.user_id = c.user_id
+GROUP BY s.user_id;
+
+

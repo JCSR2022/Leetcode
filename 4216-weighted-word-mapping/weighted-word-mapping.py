3ced85dfc1713938aaea66589a98af89af2100ec
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-06-12 22:37:42 -0600

    Time: 12 ms (25.27%) | Memory: 19.3 MB (33.64%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.py
+++ 4216-weighted-word-mapping/weighted-word-mapping.py
@@ -1,18 +1,26 @@
 class Solution:
+
+    hash_letters_normal = { i:ch for i,ch in enumerate(ascii_lowercase[::-1]) }
+
     def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
 
-        hash_letters_normal = { i:ch for i,ch in enumerate(ascii_lowercase[::-1])  }
-        hash_letters_weights = { ch:w for ch,w in  zip(ascii_lowercase,weights) }
+        # hash_letters_weights = { ch:w for ch,w in  zip(ascii_lowercase,weights) }
 
-        def findVal(word):
-            ans = 0
-            for ch in word:
-                ans+= hash_letters_weights[ch]
-            return ans%26
+        # def findVal(word):
+        #     ans = 0
+        #     for ch in word:
+        #         ans+= hash_letters_weights[ch]
+        #     return ans%26
         
 
-        ans = []
-        for word in words:
-            ans.append(  hash_letters_normal[findVal(word)]   )
+        # ans = []
+        # for word in words:
+        #     ans.append(  self.hash_letters_normal[findVal(word)]   )
 
-        return "".join(ans)
+        # return "".join(ans)
+
+#-------------------------------------------------------------------------------------
+
+        hash_letters_weights = { ch:w for ch,w in  zip(ascii_lowercase,weights) }
+
+        return "".join([self.hash_letters_normal[ sum(hash_letters_weights[ch] for ch in word )%26] for word in words])

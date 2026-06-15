b447a90d124c2e47fc2c86056746475495c72d4b
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-06-14 19:29:50 -0600

    Time: 74 ms (85.66%) | Memory: 62.4 MB (49.97%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
@@ -33,35 +33,49 @@ class Solution:
 
 #----------------------------------------------------
 
-        jumps = 0
-        node = head
-        while node.next:
-            jumps +=1
-            node = node.next
+        # jumps = 0
+        # node = head
+        # while node.next:
+        #     jumps +=1
+        #     node = node.next
 
-        if jumps == 0:
-            head = None
-            return head    
+        # if jumps == 0:
+        #     head = None
+        #     return head    
 
-        mid =  math.ceil(jumps/2)
+        # mid =  math.ceil(jumps/2)
 
-        node = head
-        jump = 0
-        while jump < mid:
-            jump +=1
-            prev = node
-            node = node.next
+        # node = head
+        # jump = 0
+        # while jump < mid:
+        #     jump +=1
+        #     prev = node
+        #     node = node.next
         
-        prev.next = node.next
+        # prev.next = node.next
 
+        # return head
+
+
+#-----------------------------------------------------
+
+
+#aqui esta como se hace con dos punteros imbecil:
+  # # 核心技巧：快指针提前走两步，这样循环结束时，slow 刚好在中点的前一个节点
+
+
+        if not head.next:
+            return None
+        
+        slow = head
+        fast = head.next.next
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        slow.next = slow.next.next
         return head
-
-
-
-
-
-
-
 
         
 

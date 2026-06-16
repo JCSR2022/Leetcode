32bbbc843891b6b0143dcac4c4da67446ddd32ef
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-06-15 20:18:57 -0600

    Time: 43 ms (40.13%) | Memory: 28.5 MB (21.71%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.py
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.py
@@ -2,15 +2,46 @@ class Solution:
     def processStr(self, s: str) -> str:
         #brute force
 
-        ans = []        
-        for ch in s:
-            if ch == '*':
-                if ans: ans.pop()
-            elif ch == "#":
-                ans += ans
-            elif ch == '%':
-                ans = ans[::-1]
-            else:
-                ans.append(ch)
 
-        return  "".join(ans)
+        # ans = []        
+        # for ch in s:
+        #     if ch == '*':
+        #         ans = ans[:-1]
+        #     elif ch == "#":
+        #         ans += ans
+        #     elif ch == '%':
+        #         ans = ans[::-1]
+        #     else:
+        #         ans.append(ch)
+
+        # return  "".join(ans)
+
+
+#---------------------------------------------
+
+
+
+        def append_char(ch):
+            return lambda arr: arr + [ch]
+
+        def remove(arr):
+            return arr[:-1]
+
+        def duplicate(arr):
+            return arr + arr
+
+        def reverse(arr):
+            return arr[::-1]
+
+        # Build dispatch table
+        hash_func = {ch: append_char(ch) for ch in string.ascii_lowercase}
+
+        hash_func['*'] = remove
+        hash_func['#'] = duplicate
+        hash_func['%'] = reverse
+
+
+        ans = []
+        for ch in s:
+            ans = hash_func[ch](ans)
+        return "".join(ans)

64cbda0de3df6ab68aa280724629bf16b199a23e
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-06-24 20:23:33 -0600

    Time: 7 ms (100.00%) | Memory: 19.6 MB (30.22%) - LeetSync

--- 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.py
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.py
@@ -7,11 +7,9 @@ class Solution:
         # size = len(nums)
         # for i in range(size):
         #     cnt_target = 0
-        #     for cnt,j in enumerate(range(i,size)):
+        #     for j in range(i,size):
         #         if nums[j] == target: cnt_target += 1
-        #         if cnt_target > (cnt+1)//2: ans += 1
-
-
+        #         if cnt_target > (j-i+1)//2: ans += 1
         # return ans
 
 
@@ -25,15 +23,27 @@ class Solution:
 
 
 
-        ans = 0
-        size = len(nums)
-        for i in range(size):
-            cnt_target = 0
-            for j in range(i,size):
-                if nums[j] == target: cnt_target += 1
-                if cnt_target > (j-i+1)//2: ans += 1
 
 
-        return ans
+#-------------------------------------------------------------------------------------------------
+#this is more eficcient but i dont understand
+
+        n = len(nums)
+        t = target
+        cnt = [0] * (n*2+2)
+        acc = [0] * (n*2+2)
+        pre = n+1
+        cnt[pre]=1
+        acc[pre]=1
+        res =0
+        for num in nums:
+            if t==num:
+                pre+=1
+            else:
+                pre-=1
+            cnt[pre]+=1
+            acc[pre] = acc[pre-1] + cnt[pre]
+            res+=acc[pre-1]
+        return res
 
 

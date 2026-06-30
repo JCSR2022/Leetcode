ae1fdab62e0690a6299c305eb9661346d2d7cecf
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-06-29 22:31:07 -0600

    Time: 61 ms (91.03%) | Memory: 19.5 MB (48.97%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
@@ -1,26 +1,35 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        # brute force
+        # brute force as you
 
-        size = len(s)
-        window = {'a':0,'b':0,'c':0}
-        i = 0
-        j = 0
-        ans = 0
-        while j < size:
-            window[s[j]] += 1
-            j +=1
-            allpresent = True
-            while allpresent:
-                for k,v in window.items():
-                    if v == 0: 
-                        allpresent = False
-                        break
-                if allpresent:
-                    ans += size-j+1
-                    window[s[i]] -=1
-                    i +=1
+        # size = len(s)
+        # window = {'a':0,'b':0,'c':0}
+        # i = 0
+        # j = 0
+        # ans = 0
+        # while j < size:
+        #     window[s[j]] += 1
+        #     j +=1
+        #     allpresent = True
+        #     while allpresent:
+        #         for k,v in window.items():
+        #             if v == 0: 
+        #                 allpresent = False
+        #                 break
+        #         if allpresent:
+        #             ans += size-j+1
+        #             window[s[i]] -=1
+        #             i +=1
                 
-        return ans
+        # return ans
 
-
+#no mames!!!!! que belleza
+#-------------------------------------------------------------------------
+        last = {'a': -1, 'b': -1, 'c': -1}
+        result = 0
+        
+        for i, ch in enumerate(s):
+            last[ch] = i
+            result += 1 + min(last['a'], last['b'], last['c'])
+        
+        return result

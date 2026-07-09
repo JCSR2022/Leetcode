fc3d330928c892ebbfb7f5ec3902e7b6b3ed58e3
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-07-08 20:32:49 -0600

    Time: 99 ms (58.73%) | Memory: 50.1 MB (52.38%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.py
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.py
@@ -3,8 +3,7 @@ class Solution:
 
         unionFind = [ i for i in range(n)]
         for i in range(1,n):
-            if abs(nums[i] - nums[i-1]) <= maxDiff:
+            if nums[i] - nums[i-1]  <= maxDiff:
                 unionFind[i] = unionFind[i-1]
                 
-        
         return [ unionFind[qi]==unionFind[qv] for qi,qv in queries ]

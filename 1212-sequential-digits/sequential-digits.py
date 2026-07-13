650c90d8cdc5aae78ea1503d50f807b2fda01535
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-07-12 21:51:26 -0600

    Time: 0 ms (100.00%) | Memory: 19.2 MB (64.31%) - LeetSync

---
+++ 1212-sequential-digits/sequential-digits.py
@@ -0,0 +1,42 @@
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        #create all solutions:
+        #342   78540
+        #max size = 5
+        #3 size 3   345 if > low and <high
+        #4 size 3   456 if > low and <high
+        #5 size 3   567
+        #6          678
+        #7          789
+        #8 not valid  n+size >10
+        #1 size 4   1234  
+        #2......... 2345
+        #...
+        #6          6789
+        #7 not valid 
+
+
+        def create(inc_val,size):
+            return sum([ (inc_val+i)*10**(size-i-1) for i in range(size) ])
+
+        size = len(str(low))
+        ans = []
+        while True:
+            inc_val = 1
+            while inc_val + size <= 10:
+                curr_ans =  create(inc_val,size)
+                print(curr_ans)
+                if curr_ans > high:  
+                    return ans
+                if curr_ans >= low:
+                    ans.append(curr_ans)
+                inc_val +=1
+
+            if curr_ans == 123456789: 
+                return ans
+            size +=1
+
+
+        
+
+

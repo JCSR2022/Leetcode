6ce84c2d0e518dd2f1e081fd52ffb50f715f8b4d
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-07-19 20:19:06 -0600

    Time: 3 ms (83.13%) | Memory: 19.5 MB (81.69%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.py
+++ 1386-shift-2d-grid/shift-2d-grid.py
@@ -1,28 +1,36 @@
 class Solution:
     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
 
-        n = len(grid)
-        m = len(grid[0])
-        size = m*n 
-        k %= size 
+        # n = len(grid)
+        # m = len(grid[0])
+        # size = m*n 
+        # k %= size 
+        # if k == 0:
+        #     return grid
+
+        # grid_1D = [ grid[i][j] for i in range(n) for j in range(m) ]
+        # grid_1D += grid_1D 
+        
+        # indx = size-k
+        # for i in range(n):
+        #     for j in range(m):
+        #         grid[i][j] = grid_1D[indx]
+        #         indx +=1
+
+        # return grid 
+
+#------------------------------------------------------------------------
+
+        m = len(grid)
+        n = len(grid[0])
+        k %= m*n
         if k == 0:
             return grid
 
+        grid_1D = [ grid[i][j] for i in range(m) for j in range(n) ]
+        grid_1D = grid_1D[-k:]+grid_1D[:-k]
 
-        grid_1D = [ grid[i][j] for i in range(n) for j in range(m) ]
-        grid_1D += grid_1D 
-        
-        # print(n,m)
-        # print(grid_1D)
-
-        indx = size-k
-        for i in range(n):
-            for j in range(m):
-                grid[i][j] = grid_1D[indx]
-                indx +=1
-
-        return grid 
-
+        return [ grid_1D[i*n:i*n+n] for i in range(m) ]
 
 
 

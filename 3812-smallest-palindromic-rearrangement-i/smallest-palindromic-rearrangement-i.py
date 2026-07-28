4a024021269ed7ca31ea180698de1d9db58095a5
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-07-27 23:10:56 -0600

    Time: 175 ms (73.73%) | Memory: 20.9 MB (65.44%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
@@ -1,23 +1,28 @@
 class Solution:
     def smallestPalindrome(self, s: str) -> str:
         
-        base = ord('a')
+        # base = ord('a')
 
-        hash_ch = [0]*26
-        for ch in s:
-            hash_ch[ord(ch) - base] +=1
+        # hash_ch = [0]*26
+        # for ch in s:
+        #     hash_ch[ord(ch) - base] +=1
         
-        ans = "" 
-        is_odd = ""
-        for i in range(26):
+        # ans = "" 
+        # is_odd = ""
+        # for i in range(26):
         
-            if hash_ch[i]%2 != 0:
-                is_odd = chr(i + base)
-                hash_ch[i] -= 1
+        #     if hash_ch[i]%2 != 0:
+        #         is_odd = chr(i + base)
+        #         hash_ch[i] -= 1
 
-            ans += chr(i + base)*(hash_ch[i]//2)
+        #     ans += chr(i + base)*(hash_ch[i]//2)
 
-        ans += is_odd + ans[::-1]
+        # ans += is_odd + ans[::-1]
 
-        return ans
-                
+        # return ans
+#---------------------------------------------------
+
+        counts = sorted(Counter(s).items())
+        half = "".join(c * (k // 2) for c, k in counts)
+        mid  = "".join(c * (k % 2)  for c, k in counts)
+        return half + mid + half[::-1]

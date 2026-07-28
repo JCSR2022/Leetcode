88c28f58d659534c0f6d5fd88fd8e056831600a8
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-07-27 23:07:48 -0600

    Time: 263 ms (53.00%) | Memory: 20.9 MB (51.15%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
@@ -0,0 +1,23 @@
+class Solution:
+    def smallestPalindrome(self, s: str) -> str:
+        
+        base = ord('a')
+
+        hash_ch = [0]*26
+        for ch in s:
+            hash_ch[ord(ch) - base] +=1
+        
+        ans = "" 
+        is_odd = ""
+        for i in range(26):
+        
+            if hash_ch[i]%2 != 0:
+                is_odd = chr(i + base)
+                hash_ch[i] -= 1
+
+            ans += chr(i + base)*(hash_ch[i]//2)
+
+        ans += is_odd + ans[::-1]
+
+        return ans
+                

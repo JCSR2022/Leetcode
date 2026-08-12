3f62a4c4d7bf0e19381bafb91b69f137e43df0bf
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-08-11 23:08:57 -0600

    Time: 263 ms (43.41%) | Memory: 35.3 MB (72.85%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.py
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.py
@@ -3,23 +3,55 @@ class Solution:
 
         #sliding window
 
-        size = len(nums)
         frec = defaultdict(int)
-        length = 0
         max_length = 0
         i = 0
         j = 0
-        while j < size:
+        while j < len(nums):
             frec[nums[j]] +=1
-            length +=1
             while frec[nums[j]] > k:
                 frec[nums[i]] -=1
                 i +=1
-                length -=1
             j +=1
-            max_length = max(max_length,length)
+            max_length = max(max_length,j-i)
 
         return max_length
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # frec = defaultdict(int)
+        # length = 0
+        # max_length = 0
+        # i = 0
+        # j = 0
+        # while j < len(nums):
+        #     frec[nums[j]] +=1
+        #     length +=1
+        #     while frec[nums[j]] > k:
+        #         frec[nums[i]] -=1
+        #         i +=1
+        #         length -=1
+        #     j +=1
+        #     max_length = max(max_length,length)
+
+        # return max_length
         
 
 

ee7d28d715f23c9b457a5efc708497ec363c9062
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-09-04 21:31:02 -0600

    Time: 159 ms (65.46%) | Memory: 33.3 MB (23.64%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.py
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.py
@@ -1,20 +1,39 @@
 class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
 
+        # size = len(nums)
+
+        # pre_max = [nums[0]]*size
+        # for i in range(1,size):
+        #     pre_max[i] = max(pre_max[i-1],nums[i])
+        
+        # pre_min = [nums[-1]]*size
+        # for i in range(size-2,-1,-1):
+        #     pre_min[i] = min(pre_min[i+1],nums[i])
+
+        # for i in range(size):
+        #     if pre_max[i]-pre_min[i] <= k :
+        #         return i
+
+        # return -1    
+
+
+#---------------------------------------------------------------
+
+
         size = len(nums)
 
-        pre_max = [nums[0]]*size
-        for i in range(1,size):
-            pre_max[i] = max(pre_max[i-1],nums[i])
-        
         pre_min = [nums[-1]]*size
         for i in range(size-2,-1,-1):
             pre_min[i] = min(pre_min[i+1],nums[i])
 
+        max_val = 0
         for i in range(size):
-            if pre_max[i]-pre_min[i] <= k :
+            max_val = max(max_val,nums[i])
+            if max_val-pre_min[i] <= k :
                 return i
 
         return -1    
+
 
         

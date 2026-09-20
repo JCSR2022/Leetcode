ed52b4db552afecd363e06c1ea1eea07283c1ba2
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-09-20 11:03:47 -0600

    Time: 7 ms (70.00%) | Memory: 19.2 MB (55.57%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
@@ -6,11 +6,20 @@ class Solution:
 
 
 
-        value = { ch:(26-i)  for i,ch in enumerate(ascii_lowercase) }
+        # value = { ch:(26-i)  for i,ch in enumerate(ascii_lowercase) }
+
+        # ans = 0
+        # for i,ch in enumerate(s):
+        #     ans += value[ch]*(i+1)
+
+        # return ans       
+        
+#------------------------------------------------------
+
+        value = [ 26-i for i in range(27)]
 
         ans = 0
         for i,ch in enumerate(s):
-            ans += value[ch]*(i+1)
+            ans += value[ord(ch)-97] * (i+1)
 
-        return ans       
-        
+        return ans

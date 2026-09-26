a9254031dddd5b45f18be37f3142d41c04eafb00
Jhonathan <105757800+JCSR2022@users.noreply.github.com>
2026-09-25 22:28:07 -0600

    Time: 46 ms (71.65%) | Memory: 51.8 MB (34.58%) - LeetSync

--- 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.py
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.py
@@ -1,25 +1,39 @@
 class Solution:
     def evaluate(self, s: str, knowledge: list[list[str]]) -> str:
 
-        knowledge_dict = { k:v for k,v in knowledge}
+        # knowledge_dict = { k:v for k,v in knowledge}
 
+        # ans = ""
+        # copy = True
+        # word = ""
+        # for ch in s:
+        #     if ch == "(":
+        #         copy = False
+        #         continue
+        #     elif ch == ")":
+        #         copy = True
+        #         ch = knowledge_dict[word] if word in knowledge_dict else "?"
+        #         word = ""
 
-        ans = ""
-        copy = True
-        word = ""
-        for ch in s:
-            if ch == "(":
-                copy = False
-                continue
-            elif ch == ")":
-                copy = True
-                ch = knowledge_dict[word] if word in knowledge_dict else "?"
-                word = ""
+        #     if copy:
+        #         ans += ch
+        #     else:
+        #         word += ch
 
-            if copy:
-                ans += ch
-            else:
-                word += ch
+        # return ans 
 
-        return ans 
-                
+#-------------------------------------------------------------------------------------
+
+        # ans1 = s.replace('(','{knowledge[').replace(')',']}')
+        # print(ans1)
+        # ans2 = defaultdict(lambda:'?',knowledge)
+        # print(dict(ans2), (ans2['age'],ans2['jho']))
+
+        # mydict = {"a":"1","b":"2"}
+        # test = "L{this_dict[a]} es {this_dict[b]}"
+        # ans3 = test.format(this_dict = mydict)
+        # print(ans3)
+
+        return s.replace('(','{knowledge[').replace(')',']}').format(knowledge = defaultdict(lambda:'?',knowledge))
+
+        
